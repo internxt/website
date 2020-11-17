befbd26dd517cd8ebaf04b33efeb4f546daa86a4
unknown <aldimirprincipal@gmail.com>
2020-11-17 12:18:03 +0100

    token finished, needs responsiveness

--- components/token/Container7.js
+++ components/token/Container7.js
@@ -1,7 +1,7 @@
 import styles from './Container7.module.css'
 import descriptions from '../../assets/token-descriptions.json'
 
-const Container7 = ({ id }) => {
+const Container7 = ({ id, data }) => {
 
     // Filter container specific descriptions
     const description = descriptions.filter( desc => desc.id === id)
@@ -13,6 +13,21 @@ const Container7 = ({ id }) => {
 
     // Set the background color of the container depending on its id
     const background = isOdd(id) ? 'container grey' : 'container'
+
+    const formatNumber = (value) => {
+        let newNum = value;
+        const suffixes = ["", "K", "M", "B","T"];
+        let suffixNum = 0;
+
+        while (newNum >= 1000) {
+          newNum /= 1000;
+          suffixNum++;
+        }
+        newNum = newNum.toPrecision(3);
+      
+        newNum += suffixes[suffixNum];
+        return newNum;
+      }
 
     return ( 
         <div className={background}>
@@ -26,7 +41,9 @@ const Container7 = ({ id }) => {
                         {description[0].label}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={styles.data}>
+                        ${Math.round((data.quote.EUR.price + Number.EPSILON) * 100) / 100}
+                    </p>
                 </div>
                 
                 <div className={styles.card}>
@@ -34,7 +51,9 @@ const Container7 = ({ id }) => {
                         {description[0].label2}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={data.quote.EUR.percent_change_24h > 0 ? `${styles.data} ${styles.green}` : `${styles.data} ${styles.red}`}>
+                        {Math.round((data.quote.EUR.percent_change_24h + Number.EPSILON) * 100) / 100}%
+                    </p>
                 </div>
                 
                 <div className={styles.card}>
@@ -42,7 +61,9 @@ const Container7 = ({ id }) => {
                         {description[0].label3}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={data.quote.EUR.percent_change_7d > 0 ? `${styles.data} ${styles.green}` : `${styles.data} ${styles.red}`}>
+                        {Math.round((data.quote.EUR.percent_change_7d + Number.EPSILON) * 100) / 100}%
+                    </p>
                 </div>
                 
                 <div className={styles.card}>
@@ -50,7 +71,9 @@ const Container7 = ({ id }) => {
                         {description[0].label4}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={styles.data}>
+                        ${formatNumber(data.quote.EUR.market_cap)}
+                    </p>
                 </div>
                 
                 <div className={styles.card}>
@@ -58,7 +81,7 @@ const Container7 = ({ id }) => {
                         {description[0].label5}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={`${styles.data} ${styles.blue}`}>$18.2M</p>
                 </div>
                 
                 <div className={styles.card}>
@@ -66,7 +89,7 @@ const Container7 = ({ id }) => {
                         {description[0].label6}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={`${styles.data} ${styles.blue}`}>$33.71</p>
                 </div>
                 
                 <div className={styles.card}>
@@ -74,7 +97,9 @@ const Container7 = ({ id }) => {
                         {description[0].label7}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={styles.data}>
+                        ${formatNumber(data.quote.EUR.volume_24h)}
+                    </p>
                 </div>
                 
                 <div className={styles.card}>
@@ -82,7 +107,9 @@ const Container7 = ({ id }) => {
                         {description[0].label8}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={styles.data}>
+                        {Intl.NumberFormat("en-EN").format(Math.round(data.circulating_supply))}
+                    </p>
                 </div>
                 
                 <div className={styles.card}>
@@ -90,7 +117,9 @@ const Container7 = ({ id }) => {
                         {description[0].label9}
                     </p>
 
-                    <p className={styles.data}>20.88%</p>
+                    <p className={styles.data}>
+                        {Intl.NumberFormat("en-EN").format(Math.round(data.total_supply))}
+                    </p>
                 </div>
             </div>
         </div>
--- pages/token.js
+++ pages/token.js
@@ -9,7 +9,7 @@ import Container6 from '../components/token/Container6'
 import Container7 from '../components/token/Container7'
 import Container8 from '../components/token/Container8'
 
-const Token = () => {
+const Token = (props) => {
     return ( 
         <>
             <TopBar />
@@ -19,13 +19,25 @@ const Token = () => {
             <Container4 id='4' />
             <Container5 id='5' />
             <Container6 id='6' />
-            <Container7 id='7' />
+            <Container7 id='7' data={props.data} />
             <Container8 id='8' />
             <Footer />
         </>
      );
 }
 
+export async function getStaticProps() {
+    const URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=ad054d41-e820-4ebb-805b-dabf899a49ff&symbol=INXT&convert=EUR'
+    const res = await fetch(URL)
+    const data = await res.json()
 
- 
+    return {
+        props: {
+            data: data.data.INXT
+        }
+    }
+}
+
+
+
 export default Token;
