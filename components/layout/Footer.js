dae02c54d0039ece77c25e78ed7979187fe4d74e
unknown <aldimirprincipal@gmail.com>
2020-11-18 13:14:53 +0100

    1080p top bar finished

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -7,76 +7,77 @@ const Footer = () => {
         <>
             <div className={styles.first_half}>
                 <div className={styles.title_container}>
-                    <h1 className={styles.title}>A family of privacy focused services.</h1>
+                    <h1 className={`${styles.title} lg:text-4xl`}>A family of privacy focused services.</h1>
 
-                    <h1 className={styles.title}>Get started free for 30 days.</h1>
+                    <h1 className={`${styles.title} lg:text-4xl`}>Get started free for 30 days.</h1>
 
                     <div className={styles.image}>
                         <Image src={'/images/1440/Footer/Line.png'} width={68} height={9} />
                     </div>
                 </div>
 
-                <div className={styles.get_started_container}>
-                    <button className={styles.button}>
-                        <p className={styles.p_button}>Get started</p>
+                <div className={`${styles.get_started_container} lg:mt-10`}>
+                    <button className={`${styles.button} lg:h-8 lg:w-32`}>
+                        <p className={`${styles.text_button} lg:text-sm`}>Get started</p>
                     </button>
 
-                    <p className={styles.learn_more}>Learn more about Internxt plans</p>
+                    <p className={`${styles.learn_more} lg:text-xss lg:mt-1`}>Learn more about Internxt plans</p>
                 </div>
             </div>
 
             <div className={styles.second_half}>
                 <div className={styles.signup_container}>
-                    <p className={styles.keep_updated}>Keep me updated about products, news, tips and offers from Internxt.</p>
+                    <p className={`${styles.keep_updated} lg:text-xl lg:w-120`}>Keep me updated about products, news, tips and offers from Internxt.</p>
 
-                    <EmailNewsletter />              
+                    <EmailNewsletter value="Subscribe" />              
                 </div>
 
                 <div className={styles.line}></div>
 
-                <div className={styles.social_container}>
+                <div className={`${styles.social_container} lg:pb-12`}>
                     <div className={styles.p_container}>
-                        <div className={styles.logo_container}>
+                        <div className={`${styles.logo} lg:w-32`}>
                             <Image 
                                 src="/images/1440/Footer/Internxt.png"
                                 alt="INTERXT logo"
-                                width={298}
-                                height={32}
+                                width={153}
+                                height={16}
                                 quality={100}
                             />
                         </div>
-                        <p className={styles.p_social}>Copyright &copy; 2020 Internxt Inc.</p>
-                        <p className={styles.p_social}>All rights Reserved</p>
+
+                        <p className={`${`${styles.p_social} lg:text-xs`} lg:text-xs`}>Copyright &copy; 2020 Internxt Inc.</p>
+                        <p className={`${`${styles.p_social} lg:text-xs`} lg:text-xs`}>All rights Reserved</p>
                     </div>
 
                     <div className={styles.p_container}>
-                        <h1 className={styles.label}>Products</h1>
-                        <p className={styles.p_social}>Internxt Drive</p>
-                        <p className={styles.p_social}>Internxt Photos</p>
-                        <p className={styles.p_social}>Internxt Send</p>
-                        <p className={styles.p_social}>Internxt Token</p>
-                        <p className={styles.p_social}>Internxt Core</p>
+                        <h1 className={`${styles.label} lg:text-sm`}>Products</h1>
+                        <p className={`${styles.p_social} lg:text-xs`}>Internxt Drive</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Internxt Photos</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Internxt Send</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Internxt Token</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Internxt Core</p>
                     </div>
 
                     <div className={styles.p_container}>
-                        <h1 className={styles.label}>Company</h1>
-                        <p className={styles.p_social}>About us</p>
-                        <p className={styles.p_social}>Pricing</p>
-                        <p className={styles.p_social}>Contact</p>
-                        <p className={styles.p_social}>Legals</p>
+                        <h1 className={`${styles.label} lg:text-sm`}>Company</h1>
+                        <p className={`${styles.p_social} lg:text-xs`}>About us</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Pricing</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Contact</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Legals</p>
                     </div>
 
                     <div className={styles.p_container}>
-                        <h1 className={styles.label}>Follow us</h1>
-                        <p className={styles.p_social}>Twitter</p>
-                        <p className={styles.p_social}>Telegram</p>
-                        <p className={styles.p_social}>Github</p>
+                        <h1 className={`${styles.label} lg:text-sm`}>Follow us</h1>
+                        <p className={`${styles.p_social} lg:text-xs`}>Twitter</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Telegram</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Github</p>
                     </div>
 
                     <div className={styles.p_container}>
-                        <h1 className={styles.label}>Join</h1>
-                        <p className={styles.p_social}>Sign in</p>
-                        <p className={styles.p_social}>Get started</p>
+                        <h1 className={`${styles.label} lg:text-sm`}>Join</h1>
+                        <p className={`${styles.p_social} lg:text-xs`}>Sign in</p>
+                        <p className={`${styles.p_social} lg:text-xs`}>Get started</p>
                     </div>
                 </div>
             </div>
--- components/layout/TopBar.js
+++ components/layout/TopBar.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from './TopBar.module.css'
+import Image from 'next/image'
 
 const TopBar = () => {
 
@@ -8,25 +9,35 @@ const TopBar = () => {
 
     return ( 
         <header>
-            <Link href="/" ><a className={styles.logo}>INTERNXT</a></Link>
+            <Link href="/" >
+                <div className={`${styles.logo} lg:w-32 lg:px-4`}>
+                    <Image 
+                        src="/images/1440/Footer/Internxt.png"
+                        alt="INTERXT logo"
+                        width={153}
+                        height={16}
+                        quality={100}
+                    />
+                </div>
+            </Link>
 
             <div>
-                <Link href="/"><a className={router.pathname === '/' ? styles.active_link : styles.nonactive_link}>Drive</a></Link>
+                <Link href="/"><a className={router.pathname === '/' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} lg:text-sm`}>Drive</a></Link>
 
-                <Link href="/photos"><a className={router.pathname === '/photos' ? styles.active_link : styles.nonactive_link}>Photos</a></Link>
+                <Link href="/photos"><a className={router.pathname === '/photos' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} lg:text-sm`}>Photos</a></Link>
                 
-                <Link href="/"><a className={router.pathname === '/send' ? styles.active_link : styles.nonactive_link}>Send</a></Link>
+                <Link href="/"><a className={router.pathname === '/send' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} lg:text-sm`}>Send</a></Link>
                 
-                <Link href="/prices"><a className={router.pathname === '/prices' ? styles.active_link : styles.nonactive_link}>Pricing</a></Link>
+                <Link href="/prices"><a className={router.pathname === '/prices' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} lg:text-sm`}>Pricing</a></Link>
 
-                <Link href="/about-us"><a  className={router.pathname === '/about-us' ? styles.active_link : styles.nonactive_link}>About</a></Link>
+                <Link href="/about-us"><a  className={router.pathname === '/about-us' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} lg:text-sm`}>About</a></Link>
 
-                <Link href="/token"><a  className={router.pathname === '/token' ? styles.active_link : styles.nonactive_link}>Token</a></Link>
+                <Link href="/token"><a  className={router.pathname === '/token' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} lg:text-sm`}>Token</a></Link>
             </div>
 
             <div className={styles.links}>
-                <a className={styles.nonactive_link}>Sign in</a>
-                <a className={styles.button}><text className={styles.button_text}>Get started</text></a>
+                <a className={`${styles.nonactive_link} lg:text-sm`}>Sign in</a>
+                <a className={`${styles.button} lg:h-8`}><p className={`${styles.button_text} lg:text-sm`}>Get started</p></a>
             </div>
         </header>
      );
