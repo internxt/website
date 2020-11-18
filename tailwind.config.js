b7a837dafca49a6da46a446640d28a5e7dd5e378
unknown <aldimirprincipal@gmail.com>
2020-11-18 12:30:23 +0100

    Finished setting up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,45 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
+  corePlugins: {
+    container: false,
+  },
   purge: [],
+  important: true,
   theme: {
-    extend: {},
+    screens: {
+      'xl': {'min': '1440px'},
+      'lg': {'min': '1080px', 'max': '1439px'},
+      'md': {'min': '750px', 'max': '1079px'},
+      'sm': {'min': '450px', 'max': '749px'},
+    },
+    fontSize: {
+      'xxs': '.625rem',
+      'xss': '.6875rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      '8xl': '5.3rem'
+    },
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '100': '25.5rem',
+        '104': '27rem',
+        '112': '30rem',
+        '120': '33rem'
+      }
+    },
   },
   variants: {},
   plugins: [],
