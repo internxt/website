f2b2d550401f1d8bdb96e641f02d68a29e4b52e4
aldimir <aldimirprincipal@gmail.com>
2020-11-23 11:45:34 +0100

    Redirect from /drive to /

--- components/layout/TopBar.js
+++ components/layout/TopBar.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import styles from './TopBar.module.css'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { Navbar } from 'react-bootstrap'
 
 const TopBar = () => {
 
@@ -16,54 +17,56 @@ const TopBar = () => {
     }, [])
 
     return ( 
-        <header>
-            <Link href="/" >
-                <div className={`${styles.logo} lg:w-32 lg:px-4`}>
-                    <Image 
-                        src="/images/1440/Footer/Internxt.png"
-                        alt="INTERXT logo"
-                        width={153}
-                        height={16}
-                        quality={100}
-                    />
+        <Navbar expand="sm">
+            <header>
+                <Link href="/" >
+                    <div className={`${styles.logo} lg:w-32 lg:px-4`}>
+                        <Image 
+                            src="/images/1440/Footer/Internxt.png"
+                            alt="INTERXT logo"
+                            width={153}
+                            height={16}
+                            quality={100}
+                        />
+                    </div>
+                </Link>
+
+                <div>
+                    <Link href="/">
+                        <a className={router.pathname === '/' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} ${color} lg:text-sm`}>Drive</a>
+                    </Link>
+
+                    <Link href="/photos">
+                        <a className={router.pathname === '/photos' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Photos</a>
+                    </Link>
+                    
+                    <Link href="/">
+                        <a className={router.pathname === '/send' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Send</a>
+                    </Link>
+                    
+                    <Link href="/prices">
+                        <a className={router.pathname === '/prices' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Pricing</a>
+                    </Link>
+
+                    <Link href="/about">
+                        <a  className={router.pathname === '/about' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>About</a>
+                    </Link>
+
+                    <Link href="/token">
+                        <a  className={router.pathname === '/token' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Token</a>
+                    </Link>
+
+                    <Link href="/core">
+                        <a  className={router.pathname === '/core' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Core</a>
+                    </Link>
                 </div>
-            </Link>
 
-            <div>
-                <Link href="/">
-                    <a className={router.pathname === '/' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link} ${color} lg:text-sm`}>Drive</a>
-                </Link>
-
-                <Link href="/photos">
-                    <a className={router.pathname === '/photos' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Photos</a>
-                </Link>
-                
-                <Link href="/">
-                    <a className={router.pathname === '/send' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Send</a>
-                </Link>
-                
-                <Link href="/prices">
-                    <a className={router.pathname === '/prices' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Pricing</a>
-                </Link>
-
-                <Link href="/about">
-                    <a  className={router.pathname === '/about' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>About</a>
-                </Link>
-
-                <Link href="/token">
-                    <a  className={router.pathname === '/token' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Token</a>
-                </Link>
-
-                <Link href="/core">
-                    <a  className={router.pathname === '/core' ? `${styles.active_link} lg:text-sm` : `${styles.nonactive_link}  ${color} lg:text-sm`}>Core</a>
-                </Link>
-            </div>
-
-            <div className={styles.links}>
-                <a className={`${styles.nonactive_link} ${color} lg:text-sm`}>Sign in</a>
-                <a className={`${styles.button} lg:h-8`}><p className={`${styles.button_text} lg:text-sm`}>Get started</p></a>
-            </div>
-        </header>
+                <div className={styles.links}>
+                    <a className={`${styles.nonactive_link} ${color} lg:text-sm`}>Sign in</a>
+                    <a className={`${styles.button} lg:h-8`}><p className={`${styles.button_text} lg:text-sm`}>Get started</p></a>
+                </div>
+            </header>
+        </Navbar>
      );
 }
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/drive',
+                destination: '/',
+                permanent: true,
+            },
+        ]
+    },
     images: {
         domains: ["cdn-images-1.medium.com"]
     }
