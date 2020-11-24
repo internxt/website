4bd234aa00989dccfcbe3f48f1cef846072c6d47
aldimir <aldimirprincipal@gmail.com>
2020-11-24 14:03:07 +0100

    About us first section updated

--- components/about-us/Container1.js
+++ components/about-us/Container1.js
@@ -1,24 +1,26 @@
 import styles from './Container1.module.css'
 import descriptions from '../../assets/about-us-descriptions.json'
-import Image from 'next/image'
 import TopBar from '../layout/TopBar'
+import EmailNewsletter from '../EmailNewsletter'
 
 const Container1 = ({ id }) => {
 
     const description = descriptions.filter( desc => desc.id === id)
 
     return ( 
-        <div className={`${styles.background} lg:h-104 xl:h-136`}>
+        <div className={`${styles.background} lg:h-144`}>
             <TopBar />
 
-            <div className={`${styles.main} lg:pl-20 lg:pt-24 xl:h-full`}>
-                <h1 className={`${styles.title} lg:text-3xl lg:w-96`}>
+            <div className={`${styles.main} xl:h-full`}>
+                <h1 className={`${styles.title} lg:text-8xl lg:w-10/12`}>
                     {description[0].title}
                 </h1>
 
-                <button className={styles.button}>
-                    <p className={styles.text_button}>Get started</p>
-                </button>
+                <p className={`${styles.subtitle} mb-4 lg:text-xl`}>
+                    {description[0].subtitle}
+                </p>
+
+                <EmailNewsletter value="Subscribe" />
             </div>
         </div>
      );
