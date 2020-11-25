15444a58d560ff316235f35be80bb7533b0732b5
aldimir <aldimirprincipal@gmail.com>
2020-11-25 10:06:34 +0100

    375p Priicng finished

--- components/prices/Container1.js
+++ components/prices/Container1.js
@@ -25,62 +25,62 @@ const Container1 = ({ id }) => {
             {
                 individual ? 
                 <div>
-                    <div className={`${styles.speech} lg:w-36 lg:pt-32 lg:pl-16`}>
+                    <div className={`${styles.speech} sm:hidden lg:w-36 lg:pt-32 lg:pl-16`}>
                         <Image src="/images/1440/Prices Individual/12.png" width={118} height={110} /> 
                     </div>
 
-                    <div className={`${styles.letter} lg:pr-12 lg:w-32`}>
+                    <div className={`${styles.letter} sm:hidden lg:pr-12 lg:w-32`}>
                         <Image src="/images/1440/Prices Individual/13.png" width={112} height={121} /> 
                     </div>
                 </div>
                 :
                 <div>
-                    <div className={`${styles.face} lg:pr-12`}>
+                    <div className={`${styles.face} sm:hidden lg:pr-12`}>
                         <Image src="/images/1440/Prices Teams/14.png" width={66} height={67} /> 
                     </div>
 
-                    <div className={`${styles.star} lg:pr-32 lg:pt-28`}>
+                    <div className={`${styles.star} sm:hidden lg:pr-32 lg:pt-28`}>
                         <Image src="/images/1440/Prices Teams/15.png" width={39} height={41} /> 
                     </div>
 
-                    <div className={`${styles.mail} lg:pt-40 lg:w-40`}>
+                    <div className={`${styles.mail} sm:hidden lg:pt-40 lg:w-40`}>
                         <Image src="/images/1440/Prices Teams/16.png" width={114} height={75} /> 
                     </div>
                 </div>
             }
 
             <div className={`${styles.first_half} `}>
-                <h1 className={`${styles.title} lg:text-4.5xl`}>
+                <h1 className={`${styles.title} sm:text-4xl sm:w-80 lg:text-4.5xl`}>
                     {
                         individual ? description[0].individual_title : description[0].teams_title
                     }
                 </h1>
 
-                <p className={`${styles.subtitle} lg:text-lg`}>
+                <p className={`${styles.subtitle} sm:text-xl sm:w-80 lg:text-lg`}>
                     {description[0].subtitle}
                 </p>
 
-                <p className={`${styles.subtitle} lg:text-lg`}>
+                <p className={`${styles.subtitle} sm:text-xl sm:w-80 lg:text-lg`}>
                     {description[0].subtitle2}
                 </p>
 
-                <div className={`${styles.switch_container} lg:h-12 lg:w-72 lg:px-1 lg:mt-16`}>
-                    <button onClick={onClickIndividual} className={individual ? `${styles.button} lg:text-13 lg:h-10 lg:w-36 ${styles.grey}` : `${styles.button} lg:text-13 lg:h-10 lg:w-36`}>For individuals</button>
+                <div className={`${styles.switch_container} sm:mt-12 sm:w-auto sm:h-12 sm:px-2 lg:h-12 lg:w-72 lg:px-1 lg:mt-16`}>
+                    <button onClick={onClickIndividual} className={individual ? `${styles.button} sm:text-xs sm:w-32 sm:h-10 lg:text-13 lg:h-10 lg:w-36 ${styles.grey}` : `${styles.button} sm:text-xs sm:w-32 sm:h-10 lg:text-13 lg:h-10 lg:w-36`}>For individuals</button>
 
-                    <button onClick={onClickTeams}  className={teams ? `${styles.button} lg:text-13 lg:h-10 lg:w-36 ${styles.grey}` : `${styles.button} lg:text-13 lg:h-10 lg:w-36`}>For teams</button>
+                    <button onClick={onClickTeams}  className={teams ? `${styles.button} sm:text-xs sm:w-32 sm:h-10 lg:text-13 lg:h-10 lg:w-36 ${styles.grey}` : `${styles.button} sm:text-xs sm:w-32 sm:h-10 lg:text-13 lg:h-10 lg:w-36`}>For teams</button>
                 </div>
             </div>
             
             {
                 individual ?
-                    <div className={`grid grid-cols-4 gap-3`}>
+                    <div className={`grid grid-cols-4 gap-3 sm:grid-cols-1 sm:gap-y-8`}>
                         <PriceCard free="true" size={2} individual={true} />
                         <PriceCard size={20} pMonth="0.99" pre6months="0.95" preYear="0.89" mostPopular="true" />
                         <PriceCard size={200} pMonth="4.49" pre6months="3.99" preYear="3.49" />
                         <PriceCard size={2000} pMonth="9.99" pre6months="9.49" preYear="8.99" />
                     </div>
                 :
-                    <div className={`grid grid-cols-4 gap-2 lg:px-40 xl:px-48`}>
+                    <div className={`grid grid-cols-4 gap-2 sm:grid-cols-1 sm:gap-y-8 lg:px-40 xl:px-48`}>
                         <PriceCard size={200} pMonth="9.49" pre6months="8.99" preYear="8.49" mostPopular="true" />
                         <PriceCard size={2000} pMonth="19.99" pre6months="19.49" preYear="18.99" />
                         <PriceCard size={20000} pMonth="149.99" pre6months="145.49" preYear="139.99" />
--- components/prices/Container2.js
+++ components/prices/Container2.js
@@ -7,8 +7,8 @@ const Container2 = ({ id }) => {
     const description = descriptions.filter( desc => desc.id === id)
 
     return ( 
-        <div className={`${styles.image} lg:px-56 xl:px-96`}>
-           <Image src="/images/1440/Prices Individual/Text.png" width={915} height={989} />
+        <div className={`${styles.image} sm:p-0 sm:px-8 sm:my-16 lg:px-56 xl:px-96`}>
+           <Image src="/images/1440/Prices Individual/Text.png" width={915} height={898} />
         </div>
      );
 }
