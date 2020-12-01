3d69e93160ad2c1a1911d0ac27087c8623094a4c
aldimir <aldimirprincipal@gmail.com>
2020-12-01 13:37:06 +0100

    Token background smaller, more padding drive image

--- components/drive/Container3.js
+++ components/drive/Container3.js
@@ -17,11 +17,11 @@ const Container3 = ({ id, descriptions }) => {
     return ( 
         <div className={background}>
             <div className={`${styles.container} sm:justify-center sm:flex-col-reverse max-w-1600`}>
-                <div className={`${styles.image} sm:object-cover sm:w-auto lg:w-5/12`}>
+                <div className={`${styles.image} sm:object-cover sm:w-auto lg:w-5/12 xl:ml-8`}>
                     <Image src="/images/1440/Drive/Section 3/icons graphic.png" width={742} height={722} />
                 </div>
 
-                <div className={`${styles.main} sm:items-center lg:pl-16`}>
+                <div className={`${styles.main} sm:items-center lg:pl-16 xl:pl-12`}>
                     <h1 className={`${styles.title} leading-10 sm:text-center sm:w-84 sm:text-4xl sm:mt-12 lg:text-5xl lg:w-112 lg:leading-tight xl:leading-13`}>
                         {description[0].title}
                     </h1>
