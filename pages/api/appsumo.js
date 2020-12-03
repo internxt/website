fc59379abbafd0c6a58c21685a81ce8927b3826b
aldimir <aldimirprincipal@gmail.com>
2020-12-03 17:23:19 +0100

    _

--- pages/api/appsumo.js
+++ pages/api/appsumo.js
@@ -1,5 +1,19 @@
-export default async (req, res) => {
-  
+const jwt = require('jsonwebtoken')
+
+export default function handler(req, res) {
+  const username = req.body.username //const username = 'fran'
+  const password = req.body.password  //const password = 'villalba'
+  const JWT = jwt.sign({
+    username: username
+  }, 'ASDF1234')
+
+  if (req.method === 'POST') {
+    res.status(200).json({ JWT })
+  }
+  else {
+    res.status(200).end(JSON.stringify({ response: 'adads' }))
+  }
+
   console.log(req)
-  res.status(200).end(JSON.stringify({ response: req }))
+  //res.status(200).end(JSON.stringify({ response: 'adads' }))
 }
