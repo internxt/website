0279da4838655da70e675d2419b83a3aa65e2bf0
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-02-12 21:55:38 +0100

    useless code + aos init

--- pages/lifetime.js
+++ pages/lifetime.js
@@ -7,15 +7,21 @@ import Container7 from '../components/drive/Container7'
 import Container8 from '../components/drive/Container8'
 import Footer from '../components/layout/Footer'
 import Layout from '../components/layout/Layout'
-import cookies from '../lib/cookies'
 import TopBar from '../components/layout/TopBar'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-const Home = (props) => {
-
+const Lifetime = (props) => {
   const metatags = props.metatagsDescriptions.filter(desc => desc.id === "drive")
 
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
+
   return (
-    <Layout title={metatags[0].title} description={metatags[0].description} segmentName='home'>
+    <Layout title={metatags[0].title} description={metatags[0].description} segmentName='lifetime'>
       <TopBar hideSignIn={true} />
       <Container1 id='9' descriptions={props.descriptions} />
       <Container3 id='3' descriptions={props.descriptions} />
@@ -30,42 +36,15 @@ const Home = (props) => {
 }
 
 export async function getServerSideProps(ctx) {
-  const userAgent = require('useragent')
-  const ua = ctx.req.headers['user-agent']
-
-  const uaParsed = userAgent.parse(ua);
-
-  const downloadUrl = (() => {
-    switch (uaParsed.os.family) {
-      case 'iOS':
-        return 'https://apps.apple.com/es/app/internxt-drive/id1465869889';
-      case 'Android':
-        return 'https://play.google.com/store/apps/details?id=com.internxt.cloud&hl=es';
-      case 'Ubuntu':
-        return 'https://internxt.com/downloads/drive.deb';
-      case 'Windows':
-        return 'https://internxt.com/downloads/drive.exe';
-      case 'Mac OS X':
-        return 'https://internxt.com/downloads/drive.dmg';
-      default:
-        // No borrar
-        console.log('Unknown device %s. User-Agent: %s', uaParsed.os.family, ua)
-        return 'https://github.com/internxt/drive-desktop/releases'
-    }
-  })();
-
-
   const lang = ctx.locale
   const metatagsDescriptions = require(`../assets/lang/${lang}/metatags-descriptions.json`)
   const descriptions = require(`../assets/lang/${lang}/drive-descriptions.json`)
   const footerDescriptions = require(`../assets/lang/${lang}/footer-descriptions.json`)
   const cardDescriptions = require(`../assets/lang/${lang}/card-descriptions.json`)
 
-  cookies.setReferralCookie(ctx);
-
   return {
-    props: { downloadUrl, metatagsDescriptions, descriptions, footerDescriptions, cardDescriptions }
+    props: { metatagsDescriptions, descriptions, footerDescriptions, cardDescriptions }
   }
 }
 
-export default Home;
+export default Lifetime;
