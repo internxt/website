90fce732a8aa1f1c703916141d2fec955eb00852
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-02-15 16:17:06 +0100

    create session refactor

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -3,31 +3,25 @@ import { Spinner } from "react-bootstrap";
 import { getStripe } from '../lib/getstripe'
 
 export async function redirectToCheckoutAction() {
-    // Create a Checkout Session.
-    const response = await fetch('/api/stripe/session', {
-      method: 'post',
-      body: JSON.stringify({ amount: 1 })
-    });
+  // Create a Checkout Session.
+  const response = await fetch('/api/stripe/session', {
+    method: 'post',
+    body: JSON.stringify({ amount: 1 })
+  });
 
-    if (response.statusCode === 500) {
-      console.error(response.message);
-      return;
-    }
+  if (response.statusCode === 500) {
+    console.error(response.message);
+    return;
+  }
 
-    const body = await response.json()
+  const body = await response.json()
 
-    // Redirect to Checkout.
-    const stripe = await getStripe();
-    const { error } = await stripe.redirectToCheckout({
-      // Make the id field from the Checkout Session creation API response
-      // available to this file, so you can provide it as parameter here
-      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-      sessionId: body.id,
-    });
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `error.message`.
-    console.warn(error.message);
+  const stripe = await getStripe();
+  const { error } = await stripe.redirectToCheckout({
+    sessionId: body.id
+  });
+
+  console.warn(error.message);
 }
 
 export default function CheckoutForm(props) {
@@ -37,19 +31,19 @@ export default function CheckoutForm(props) {
     e.preventDefault();
     setLoading(true);
 
+    if (window && window.analytics) {
+      window.analytics.track('landing-lifetime-enter-checkout')
+    }
+
     redirectToCheckoutAction().finally(() => setLoading(false))
   };
 
   return <form onSubmit={handleSubmit}>
-
     <button
       type="submit"
       disabled={loading}
       {...props}
       style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
     >{loading ? <Spinner animation="border" style={{ color: '#e0e0e0' }} /> : props.value}</button>
-
-
   </form>
-
 }
