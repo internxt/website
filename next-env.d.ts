7edd013c96ffda4ede41910e37c9a9cd7c39785a
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-02-17 10:53:34 +0100

    TS: Envs defined

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,23 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: 'development' | 'production' | 'test'
+        // SERVER SIDE KEYS
+        SENDGRID_API_KEY: string
+        RECAPTCHA_V3_SK: string
+        COINMARKETCAP_API_KEY: string
+        STRIPE_PRIVATE_KEY: string
+        STRIPE_PRIVATE_KEY_TEST: string
+        JWT_DRIVE_SERVER: string
+
+        // PUBLIC KEYS
+        NEXT_PUBLIC_RECAPTCHA_V3_PK: string
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST: string
+        NEXT_PUBLIC_SEGMENT_KEY: string
+        NEXT_PUBLIC_SEGMENT_KEY_TEST: string
+
+    }
+}
