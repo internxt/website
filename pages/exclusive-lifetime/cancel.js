387ca4826c2867707d64215f21dfb62f8e696057
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-02-18 12:49:21 +0100

    post payment stripe

---
+++ pages/exclusive-lifetime/cancel.js
@@ -0,0 +1,8 @@
+import Layout from "../../components/layout/Layout";
+
+export default function Cancelled() {
+
+    return <Layout segmentName="landing-exclusive-lifetime-cancel">
+        <script dangerouslySetInnerHTML={{ __html: `analytics.track('landing-exclusive-lifetime-cancel', function() { window.location.href='https://internxt.com/exclusive-lifetime'; });` }} />
+    </Layout>
+}
