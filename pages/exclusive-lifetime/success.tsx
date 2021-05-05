cdb7d6a1403523b39cee5cd76dae417a7a02be9b
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-05-05 09:46:01 +0200

    success refactor

--- pages/exclusive-lifetime/success.tsx
+++ pages/exclusive-lifetime/success.tsx
@@ -1,38 +1,59 @@
+import React, { useEffect } from 'react';
 import { GetServerSideProps } from "next";
 import Layout from "../../components/layout/Layout";
 
 export default function Success(props) {
-    return <Layout segmentName="landing-exclusive-lifetime-success" disableMailerlite={true} disableDrift={true} title="Internxt Checkout Succcess" description="Redirect">
-        <script dangerouslySetInnerHTML={{ __html: `analytics.track('landing-exclusive-lifetime-converted', function() { window.location.href='${props.redirectUrl}'; });` }} />
-        <div>
-            Automatic redirecting to <a href={props.redirectUrl}>Drive Web</a>...
-        </div>
+  useEffect(() => {
+    if (props.email && props.token) {
+      window.analytics.track('landing-exclusive-lifetime-converted', () => {
+        setTimeout(() => {
+          window.location = props.redirectUrl;
+        }, 5000);
+      });
+    }
+  }, []);
+
+  return (
+    <Layout
+      segmentName="landing-lifetime-success"
+      disableMailerlite
+      disableDrift
+      title="Internxt Checkout Succcess"
+      description="Redirect"
+    >
+      <div>
+        Automatic redirecting to
+        {' '}
+        <a href={props.redirectUrl}>Drive Web</a>
+        ...
+      </div>
     </Layout>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const host = (ctx.req.headers['host'].match(/^localhost/) ? 'http://' : 'https://') + ctx.req.headers['host']
+  const host = (ctx.req.headers['host'].match(/^localhost/) ? 'http://' : 'https://') + ctx.req.headers['host'];
 
-    if (!ctx.query.sid) {
-        return {
-            props: {
-                token: '',
-                email: '',
-                redirectUrl: '/'
-            }
-        }
-    }
-
-    const request = await fetch(host + '/api/stripe/session/' + ctx.query.sid)
-    const body = await request.json()
-
-    const redirectUrl = `${process.env.DRIVE_API_URL}/appsumo/${body.email}?token=${body.token}`
-
+  if (!ctx.query.sid) {
     return {
-        props: {
-            token: body.token,
-            email: body.email,
-            redirectUrl
-        }
+      props: {
+        token: '',
+        email: '',
+        redirectUrl: '/'
+      }
+    };
+  }
+
+  const request = await fetch(host + '/api/stripe/session/' + ctx.query.sid);
+  const body = await request.json()
+
+  const redirectUrl = `${process.env.DRIVE_API_URL}/appsumo/${body.email}?token=${body.token}`;
+
+  return {
+    props: {
+      token: body.token,
+      email: body.email,
+      redirectUrl
     }
-}
+  };
+};
