15363dd179fcf405c01aa895dea12dd36cf79a4a
Alvaro <34165099+aboredvaro@users.noreply.github.com>
2021-09-01 17:58:17 +0200

    Updated Token links and logos

--- components/token/HeroSection.js
+++ components/token/HeroSection.js
@@ -28,14 +28,14 @@ const HeroSection = ({textContent, download, lang}) => {
           </h2>
 
           <div className="featured grid grid-cols-2 md:grid-cols-4 grid-flow-row gap-y-10 gap-x-6 sm:gap-8 justify-items-center place-items-center px-10 lg:px-32 mx-auto mb-20">
-            <img className="h-6 sm:h-8" src="../../images/token/bittrex.webp" draggable="false" alt="bittrex logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/uniswap.webp" draggable="false" alt="uniswap logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/mercatox.webp" draggable="false" alt="mercatox logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/latoken.webp" draggable="false" alt="latoken logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/exrates.webp" draggable="false" alt="exrates logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/yobit.webp" draggable="false" alt="yobit logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/coinbase.webp" draggable="false" alt="coinbase logo"/>
-            <img className="h-6 sm:h-8" src="../../images/token/coinmarketcap.webp" draggable="false" alt="coinmarketcap logo"/>
+            <a href="https://global.bittrex.com/Market/Index?MarketName=BTC-INXT" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/bittrex.webp" draggable="false" alt="bittrex logo"/></a>
+            <a href="https://info.uniswap.org/pair/0x73994f935b23511686ce1dd59c295e5100031f4b" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/uniswap.webp" draggable="false" alt="uniswap logo"/></a>
+            <a href="https://wallet.coinbase.com/" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/coinbasewallet.webp" draggable="false" alt="coinbase logo"/></a>
+            <a href="https://latoken.com/exchange/INXT_BTC" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/latoken.webp" draggable="false" alt="latoken logo"/></a>
+            <a href="https://exrates.me/trading/INXTBTC" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/exrates.webp" draggable="false" alt="exrates logo"/></a>
+            <a href="https://yobit.net/en/trade/INXT/BTC" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/yobit.webp" draggable="false" alt="yobit logo"/></a>
+            <a href="https://mercatox.com/exchange/INXT/BTC" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/mercatox.webp" draggable="false" alt="mercatox logo"/></a>
+            <a href="https://coinmarketcap.com/currencies/internxt/" target="_blank"><img className="h-6 sm:h-8" src="../../images/token/coinmarketcap.webp" draggable="false" alt="coinmarketcap logo"/></a>
           </div>
 
         </div>
