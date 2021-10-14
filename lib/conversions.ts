d5e86de1aa35a4ac5e7c3909487b2a4f810bac02
Joan Mora <elcorreudejoanmora@gmail.com>
2021-10-14 13:08:45 +0200

    Linter fix

--- lib/conversions.ts
+++ lib/conversions.ts
@@ -9,40 +9,39 @@ export default async function setUTM() {
     'gclid',
     'irclickid'
   ];
-  let links = document.querySelectorAll('a');
+  const links = document.querySelectorAll('a');
 
   // check if links contain domain from the domainsToDecorate array and then decorates
   for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
     for (let domainIndex = 0; domainIndex < domainsToDecorate.length; domainIndex++) {
-      if (links[linkIndex].href.indexOf(domainsToDecorate[domainIndex]) > -1 && links[linkIndex].href.indexOf("#") === -1) {
+      if (links[linkIndex].href.indexOf(domainsToDecorate[domainIndex]) > -1 && links[linkIndex].href.indexOf('#') === -1) {
         links[linkIndex].href = decorateUrl(links[linkIndex].href);
       }
     }
   }
   // decorates the URL with query params
   function decorateUrl(urlToDecorate) {
-    urlToDecorate = (urlToDecorate.indexOf('?') === -1) ? urlToDecorate + '?' : urlToDecorate + '&';
-    let collectedQueryParams = [];
+    urlToDecorate = (urlToDecorate.indexOf('?') === -1) ? `${urlToDecorate}?` : `${urlToDecorate}&`;
+    const collectedQueryParams = [];
     for (let queryIndex = 0; queryIndex < queryParams.length; queryIndex++) {
       if (getQueryParam(queryParams[queryIndex])) {
-        collectedQueryParams.push(queryParams[queryIndex] + '=' + getQueryParam(queryParams[queryIndex]))
+        collectedQueryParams.push(`${queryParams[queryIndex]}=${getQueryParam(queryParams[queryIndex])}`);
       }
     }
     return urlToDecorate + collectedQueryParams.join('&');
   }
   // a function that retrieves the value of a query parameter
   function getQueryParam(name) {
-
-    var key = name
+    const key = name;
     // var storage = window.localStorage.getItem(name)
-    var storage = window.sessionStorage.getItem(name)
-    var search = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(window.location.search)
+    const storage = window.sessionStorage.getItem(name);
+    const search = (new RegExp(`[?&]${encodeURIComponent(name)}=([^&]*)`)).exec(window.location.search);
 
     if (search ?? false) {
       // name is found in location.search
       if (search[1] !== null) {
         // window.localStorage.setItem(key, search[1])
-        window.sessionStorage.setItem(key, search[1])
+        window.sessionStorage.setItem(key, search[1]);
         // console.log("Found '" + key + "' in url with value '" + search[1] + "' (saving in local storage)")
         return search[1];
       }
