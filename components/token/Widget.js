c1dd0846a6584509f9c25311ba99202f0013d524
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2021-12-14 17:25:50 +0100

    Update Widget.js

--- components/token/Widget.js
+++ components/token/Widget.js
@@ -1,8 +1,23 @@
-/* eslint-disable react/self-closing-comp */
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Widget = () => (
-  <div className="flex flex-col w-full" id="crypto-widget-CoinList" data-transparent="true" data-design="modern" data-coins="internxt" />
-);
+const Widget = () => {
+  function addScript() {
+    const head = document.getElementsByTagName('head')[0];
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://crypto.com/price/static/widget/index.js';
+    head.appendChild(script);
+  }
+
+  useEffect(() => {
+    addScript();
+  });
+
+  return (
+    <>
+      <div className="flex flex-col w-full" id="crypto-widget-CoinList" data-transparent="true" data-design="modern" data-coins="internxt" />
+    </>
+  );
+};
 
 export default Widget;
