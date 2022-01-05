a2a214d160f9d42dad246c52192058d2d23352e6
Alvaro <34165099+aboredvaro@users.noreply.github.com>
2022-01-05 14:52:15 +0100

    Table first try

--- components/comparison/FeatureSection.js
+++ components/comparison/FeatureSection.js
@@ -7,12 +7,9 @@ const FeatureSection = ({
   <section>
     <div className="flex flex-col items-center justify-center space-y-20 px-6 py-20 bg-gradient-to-b from-cool-gray-5 to-white">
       {/* Why start using Internxt */}
-      <div className="flex flex-col items-center text-center mb-8">
-        <h3 className="text-3xl font-semibold mb-4">
-          {textContent.whyStartUsingInternxt.title.line1}
-          <br className="hidden sm:inline-flex" />
-          {' '}
-          {textContent.whyStartUsingInternxt.title.line2}
+      <div className="flex flex-col items-center text-center space-y-10">
+        <h3 className="text-3xl font-semibold">
+          {textContent.whyStartUsingInternxt.title}
         </h3>
 
         <h4 className="text-lg lg:text-xl text-cool-gray-80">
@@ -27,7 +24,7 @@ const FeatureSection = ({
       </div>
 
       {/* Create account */}
-      <div className="flex flex-row items-stretch justify-center bg-white shadow-subtle-hard rounded-3xl overflow-hidden">
+      <div className="flex flex-row items-stretch justify-center bg-white shadow-subtle rounded-3xl overflow-hidden">
         <div className="flex flex-col items-start justify-start p-16 mr-6 space-y-8">
           <div className="flex flex-col space-y-2">
             <h3 className="text-3xl font-semibold">
@@ -58,7 +55,36 @@ const FeatureSection = ({
           </a>
         </div>
 
-        <div className="flex flex-col w-80 bg-red-10">image goes here</div>
+        <div className="flex flex-col w-80 pt-16">
+          <img loading="lazy" className="object-cover object-left-top w-full h-full" src="/images/comparison/iphone-and-mac.webp" draggable="false" alt="Internxt Drive web and mobile apps" />
+        </div>
+      </div>
+
+      {/* Why people love Internxt */}
+      <div className="flex flex-col w-full items-center text-center space-y-10">
+        <h3 className="text-3xl font-semibold mb-4">
+          {textContent.whyPeopleLoveInternxt.title}
+        </h3>
+
+        <div className="flex flex-row items-start justify-center w-full text-left space-x-10">
+
+          {textContent.whyPeopleLoveInternxt.cards.map((card) => (
+            <div className="flex flex-col flex-shrink-0 w-80 p-8 rounded-2xl bg-cool-gray-5 space-y-4">
+              <h3 className="text-lg">
+                {`“${card.quote}”`}
+              </h3>
+
+              <div className="flex flex-col items-start justify-center text-sm">
+                <p>{card.author}</p>
+                <div className="flex flex-row justify-start items-center space-x-2 font-bold">
+                  <span>—</span>
+                  <span>{card.company}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+
+        </div>
       </div>
     </div>
   </section>
--- components/comparison/TableSection.js
+++ components/comparison/TableSection.js
@@ -1,13 +1,12 @@
-/* eslint-disable max-len */
-/* eslint-disable prefer-const */
 import React from 'react';
+import { UilCheck, UilMinus } from '@iconscout/react-unicons';
 
 const HeroSection = ({
   textContent
 }) => {
   const table = {
     internxt: {
-      line1: 'Table goes here'
+      line1: 'test'
     }
   };
 
@@ -18,7 +17,7 @@ const HeroSection = ({
       <div className="flex flex-col">
 
         {/* Header */}
-        <div className="relative flex flex-col items-center justify-center pt-32 pb-16 bg-blue-60 text-white overflow-hidden">
+        <div className="relative flex flex-col items-center justify-center pt-32 pb-16 bg-blue-60 text-white overflow-hidden z-20">
           <div className="relative flex flex-col items-center justify-center mb-8 z-10">
             <h1 className="text-6xl font-medium text-center mb-4">
               {textContent.title.line1}
@@ -48,7 +47,109 @@ const HeroSection = ({
 
           <div className="absolute top-2/3 left-0 w-full h-full rounded-t-full-percentage transform scale-y-200 filter blur-4xl bg-blue-70" />
         </div>
-        {table.internxt.line1}
+
+        {/* Table */}
+        <div className="flex flex-col items-center justify-start py-10 bg-white">
+          <table className="relative border-collapse table-auto text-base text-center text-cool-gray-80 bg-none mb-20">
+
+            {/* Competitors */}
+            <thead className="sticky top-16 h-44 z-10 text-cool-gray-90">
+              <tr className="relative z-10 bg-white bg-opacity-80 backdrop-filter backdrop-blur-md">
+                <th> </th>
+                <th className="relative w-32 align-bottom p-0 text-lg font-medium">
+                  <div className="flex flex-col items-center justify-center w-full h-32 bg-blue-10 rounded-t-2xl">
+                    Internxt
+                  </div>
+                </th>
+                <th className="relative w-24 align-bottom p-0 font-normal">
+                  <div className="flex flex-col items-center justify-center w-full h-24 rounded-t-lg">
+                    Others
+                  </div>
+                </th>
+              </tr>
+            </thead>
+
+            {/* Section */}
+            <thead className="sticky top-60 h-12 text-left text-cool-gray-90">
+              <tr className="bg-cool-gray-10 font-medium h-12">
+                <td className="rounded-l-lg px-6">Encryption & Security</td>
+                <td />
+                <td />
+              </tr>
+            </thead>
+
+            {/* Rows */}
+            <tbody className="divide-y divide-cool-gray-10">
+              <tr className="h-12">
+                <td className="text-left px-6">Encryption in data transit</td>
+                <td className="bg-blue-10">
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilCheck className="w-7 h-7 text-blue-50" />
+                  </div>
+                </td>
+                <td>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilCheck className="w-7 h-7 text-cool-gray-40" />
+                  </div>
+                </td>
+              </tr>
+              <tr className="h-12">
+                <td className="text-left px-6">End-to-end encrypted storage</td>
+                <td className="bg-blue-10">
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilCheck className="w-7 h-7 text-blue-50" />
+                  </div>
+                </td>
+                <td>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilMinus className="w-7 h-7 text-cool-gray-20" />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+
+            {/* Section */}
+            <thead className="sticky top-60 h-12 text-left text-cool-gray-90">
+              <tr className="bg-cool-gray-10 font-medium h-12">
+                <td className="rounded-l-lg px-6">Storage & File management</td>
+                <td />
+                <td />
+              </tr>
+            </thead>
+
+            {/* Rows */}
+            <tbody className="divide-y divide-cool-gray-10">
+              <tr className="h-12">
+                <td className="text-left px-6">Sync any folders</td>
+                <td className="bg-blue-10">
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilCheck className="w-7 h-7 text-blue-50" />
+                  </div>
+                </td>
+                <td>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilCheck className="w-7 h-7 text-cool-gray-40" />
+                  </div>
+                </td>
+              </tr>
+              <tr className="h-12">
+                <td className="text-left px-6">Desktop sync app</td>
+                <td className="relative bg-blue-10">
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilCheck className="w-7 h-7 text-blue-50" />
+                  </div>
+                  <a className="absolute top-full left-0 h-10 w-full flex flex-col items-center justify-center rounded-b-2xl bg-blue-60 text-white">Start now</a>
+                </td>
+                <td>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <UilMinus className="w-7 h-7 text-cool-gray-20" />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+
+          </table>
+        </div>
 
       </div>
 
