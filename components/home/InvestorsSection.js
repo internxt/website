56c1abec4752f6651002809acfc5ab8b5e5c5f24
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2022-06-06 16:01:40 +0200

    Fixed logo spacing

--- components/home/InvestorsSection.js
+++ components/home/InvestorsSection.js
@@ -10,22 +10,22 @@ const InvestorsSection = ({ textContent }) => (
         </h2>
 
         <div className="flex flex-row justify-around flex-wrap sm:px-10">
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-44 h-20 items-center justify-center">
+          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
             <img loading="lazy" src="../../logos/investors/ovhcloud.svg" draggable="false" alt="ovh cloud logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-44 h-20 items-center justify-center">
+          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
             <img loading="lazy" src="../../logos/investors/telefonica.svg" draggable="false" alt="telefonica logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-44 h-20 items-center justify-center">
+          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
             <img loading="lazy" src="../../logos/investors/theventurecity.svg" draggable="false" alt="the venture city logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-44 h-20 items-center justify-center">
+          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
             <img loading="lazy" src="../../logos/investors/esade.webp" className="h-7 mx-auto" draggable="false" alt="esade logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-44 h-20 items-center justify-center">
+          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
             <img loading="lazy" src="../../logos/investors/notion_vc.webp" className="h-4 mx-auto" draggable="false" alt="notion vc logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-44 h-20 items-center justify-center">
+          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
             <img loading="lazy" src="../../logos/investors/angelscapital.svg" draggable="false" alt="angels capital logo" />
           </div>
         </div>
