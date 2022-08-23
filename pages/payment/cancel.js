aac861437f5833436ff08e903a92d8b615cfd0f4
Joan Mora <elcorreudejoanmora@gmail.com>
2022-08-23 14:13:28 +0200

    Hot fix cancel payment

--- pages/payment/cancel.js
+++ pages/payment/cancel.js
@@ -4,15 +4,19 @@ import Layout from '../../components/layout/Layout';
 
 export default function Cancelled() {
   useEffect(() => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const checkoutSessionId = urlSearchParams.get('sid');
-    window.analytics.track(
-      'Checkout Canceled',
-      {
-        cs_id: checkoutSessionId
-      },
-      () => { window.location = 'http://internxt.com/pricing'; }
-    );
+    try {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const checkoutSessionId = urlSearchParams.get('sid');
+      window.analytics.track(
+        'Checkout Canceled',
+        {
+          cs_id: checkoutSessionId
+        },
+        () => { window.location = 'http://internxt.com/pricing'; }
+      );
+    } catch (err) {
+      window.location = 'http://internxt.com/pricing';
+    }
   }, []);
 
   return (
