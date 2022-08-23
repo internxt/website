ea0480dd8bfacd52db79b02e90e3d15385a509d3
Joan Mora <elcorreudejoanmora@gmail.com>
2022-08-23 14:22:18 +0200

    Remove reddit tracking

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -53,7 +53,6 @@ export default function Layout({
         {!disableMailerlite && <script defer src="/js/mailerlite.js" />}
         {!disableDrift && <script defer src="/js/drift.js" />}
         <script async dangerouslySetInnerHTML={{ __html: ' window.intercomSettings = { app_id: "ta2ffq6n" }; (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic(\'reattach_activator\');ic(\'update\',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement(\'script\');s.type=\'text/javascript\';s.async=true;s.src=\'https://widget.intercom.io/widget/ta2ffq6n\';var x=d.getElementsByTagName(\'script\')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent(\'onload\',l);}else{w.addEventListener(\'load\',l,false);}}})();' }} />
-        <script defer src="/js/reddit.js" />
         {!disableAdtrack && <script defer src="/js/adtrack.js" />}
       </Head>
 
