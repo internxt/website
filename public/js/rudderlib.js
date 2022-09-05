0b2ee709b2c179e78957664a9dfced141f3eb900
Joan Mora <joanmora.internxt@gmail.com>
2022-09-05 14:01:20 +0200

    Do not send adblocker flag

--- public/js/rudderlib.js
+++ public/js/rudderlib.js
@@ -1,7 +1,6 @@
 !function () {
   const k = "2EL15xyzDYBZ1V8C4ov6vX6MSOV"
   var e = window.rudderanalytics = window.rudderanalytics || []; e.methods = ["load", "page", "track", "identify", "alias", "group", "ready", "reset", "getAnonymousId", "setAnonymousId"], e.factory = function (t) { return function () { var r = Array.prototype.slice.call(arguments); return r.unshift(t), e.push(r), e } }; for (var t = 0; t < e.methods.length; t++) { var r = e.methods[t]; e[r] = e.factory(r) } e.loadJS = function (e, t) { var r = document.createElement("script"); r.type = "text/javascript", r.async = !0, r.src = "https://s3.us-east-1.wasabisys.com/djdjdkwl/ruddersnippet.js"; var a = document.getElementsByTagName("script")[0]; a.parentNode.insertBefore(r, a) }, e.loadJS(),
-    e.load(k, "https://cdp.internxt.com",
-      { sendAdblockPage: true }),
+    e.load(k, "https://cdp.internxt.com"),
     e.page()
 }();
