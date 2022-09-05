c287975aa77a85cb986e7b24d91ad4bd79a4e582
Joan Mora <elcorreudejoanmora@gmail.com>
2022-09-05 08:56:00 +0200

    Add self hostd rudder integration

---
+++ lib/brave.ts
@@ -0,0 +1,5 @@
+export default function isBrave() {
+  const maybeBrave = (window.navigator as { brave?: any }).brave;
+
+  return maybeBrave != undefined && maybeBrave.isBrave.name == "isBrave";
+}
