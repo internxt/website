6c29c191ba868d56a0ebd21d3e2b3a6dd1ac8d8d
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2022-09-22 08:04:54 +0200

    Update PriceCard.ts

--- components/prices/PriceCard.js
+++ components/prices/PriceCard.js
@@ -73,7 +73,7 @@ export default function PriceCard({
               {contentText.perUser}
             </span>
             <p className="flex flex-row items-start space-x-0.5 font-medium text-neutral-700">
-              <span className={`currency ${price <= 0 ? 'hidden' : ''}`}>{country === 'US' ? '$' : '€'}</span>
+              <span className={`currency ${price <= 0 ? 'hidden' : ''}`}>€</span>
               <span className="price text-4xl font-bold">{price <= 0 ? `${contentText.freePlan}` : price}</span>
             </p>
             {/* eslint-disable-next-line no-nested-ternary */}
@@ -86,7 +86,7 @@ export default function PriceCard({
             className={`priceBefore ${priceBefore ? 'flex' : 'hidden'
               } text-base font-medium text-neutral-80 line-through`}
           >
-            {country === 'US' ? '$' : '€'}{priceBefore}
+            €{priceBefore}
           </span>
 
           <div
@@ -95,7 +95,7 @@ export default function PriceCard({
           >
             <p className={`${price <= 0 ? 'hidden' : ''}`}>
               <span className={`totalBilled ${billingFrequency < 0 ? 'hidden' : ''}`}>
-                <span className="currency text-supporting-2">{country === 'US' ? '$' : '€'}</span>
+                <span className="currency text-supporting-2">€</span>
                 {totalBilled}{' '}
               </span>
               <span className="billingFrequency">
