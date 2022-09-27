9444e153ad5e8855d3e0dc15fdc0395c7c2a2cdf
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-09-27 15:57:34 +0200

    stripe-js package added for getStripe

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -184,10 +184,13 @@ export default function Navbar(props: NavbarProps) {
 
   return (
     <div
-      className={`section flex items-center ${!menuState && !props.fixed ? 'absolute' : 'fixed'
-        } h-16 w-full bg-white transition-all duration-100 ${props.fixed && 'backdrop-blur-lg backdrop-saturate-150 backdrop-filter'
-        } ${scrolled && props.fixed ? 'border-opacity-5 bg-opacity-90' : 'border-opacity-0 bg-opacity-0'} ${menuState ? 'bg-opacity-100' : ''
-        } z-40 border-b border-black`}
+      className={`section flex items-center ${
+        !menuState && !props.fixed ? 'absolute' : 'fixed'
+      } h-16 w-full bg-white transition-all duration-100 ${
+        props.fixed && 'backdrop-blur-lg backdrop-saturate-150 backdrop-filter'
+      } ${scrolled && props.fixed ? 'border-opacity-5 bg-opacity-90' : 'border-opacity-0 bg-opacity-0'} ${
+        menuState ? 'bg-opacity-100' : ''
+      } z-40 border-b border-black`}
     >
       <div className="mx-4 w-full lg:mx-10 xl:mx-32">
         <div className="navbar mx-auto flex max-w-screen-xl items-center justify-between">
@@ -204,14 +207,16 @@ export default function Navbar(props: NavbarProps) {
 
               {/* Mobile hamburger menu background */}
               <div
-                className={`pointer-events-none fixed left-0 top-14 flex h-full w-full bg-white transition-all duration-500 ${menuState ? 'opacity-100' : 'opacity-0'
-                  }`}
+                className={`pointer-events-none fixed left-0 top-14 flex h-full w-full bg-white transition-all duration-500 ${
+                  menuState ? 'opacity-100' : 'opacity-0'
+                }`}
               />
 
               {/* Mobile hamburger menu */}
               <div
-                className={`fixed left-0 top-14 flex w-full flex-col overflow-hidden bg-white text-xl transition-all duration-500 ${menuState ? 'h-screen overflow-y-auto pb-14' : 'h-0'
-                  }`}
+                className={`fixed left-0 top-14 flex w-full flex-col overflow-hidden bg-white text-xl transition-all duration-500 ${
+                  menuState ? 'h-screen overflow-y-auto pb-14' : 'h-0'
+                }`}
               >
                 <div className="my-6 font-medium">
                   <Link href="/pricing" locale={props.lang}>
@@ -221,8 +226,9 @@ export default function Navbar(props: NavbarProps) {
                       onClick={() => {
                         setMenuState(false);
                       }}
-                      className={`flex w-full translate-y-0 px-8 py-3 outline-none transition delay-100 duration-300 ${menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
-                        }`}
+                      className={`flex w-full translate-y-0 px-8 py-3 outline-none transition delay-100 duration-300 ${
+                        menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
+                      }`}
                     >
                       {props.textContent.links.pricing}
                     </a>
@@ -231,23 +237,27 @@ export default function Navbar(props: NavbarProps) {
                   <Disclosure as="div">
                     {({ open }) => (
                       <div
-                        className={`translate-y-0 transition delay-150 duration-300 ${menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
-                          }`}
+                        className={`translate-y-0 transition delay-150 duration-300 ${
+                          menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
+                        }`}
                       >
                         <div className={`${open ? 'bg-cool-gray-5' : ''}`}>
                           <Disclosure.Button
-                            className={`flex w-full items-center justify-between px-8 py-3 font-medium ${open ? 'bg-cool-gray-10' : ''
-                              }`}
+                            className={`flex w-full items-center justify-between px-8 py-3 font-medium ${
+                              open ? 'bg-cool-gray-10' : ''
+                            }`}
                           >
                             <span>{props.textContent.links.products}</span>
                             <span className="relative h-6 w-6">
                               <UilMinus
-                                className={`absolute top-0 left-0 h-6 w-6 transition duration-300 ${open ? 'text-cool-gray-60' : '-rotate-180 text-cool-gray-40'
-                                  }`}
+                                className={`absolute top-0 left-0 h-6 w-6 transition duration-300 ${
+                                  open ? 'text-cool-gray-60' : '-rotate-180 text-cool-gray-40'
+                                }`}
                               />
                               <UilMinus
-                                className={`absolute top-0 left-0 h-6 w-6 transition duration-300 ${open ? 'text-cool-gray-60' : '-rotate-90 text-cool-gray-40'
-                                  }`}
+                                className={`absolute top-0 left-0 h-6 w-6 transition duration-300 ${
+                                  open ? 'text-cool-gray-60' : '-rotate-90 text-cool-gray-40'
+                                }`}
                               />
                             </span>
                           </Disclosure.Button>
@@ -310,8 +320,9 @@ export default function Navbar(props: NavbarProps) {
                       onClick={() => {
                         setMenuState(false);
                       }}
-                      className={`flex w-full translate-y-0 cursor-pointer px-8 py-3 outline-none transition delay-200 duration-300 ${menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
-                        }`}
+                      className={`flex w-full translate-y-0 cursor-pointer px-8 py-3 outline-none transition delay-200 duration-300 ${
+                        menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
+                      }`}
                     >
                       {props.textContent.links.privacy}
                     </a>
@@ -324,8 +335,9 @@ export default function Navbar(props: NavbarProps) {
                       onClick={() => {
                         setMenuState(false);
                       }}
-                      className={`flex w-full translate-y-0 cursor-pointer px-8 py-3 outline-none transition delay-250 duration-300 ${menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
-                        }`}
+                      className={`flex w-full translate-y-0 cursor-pointer px-8 py-3 outline-none transition delay-250 duration-300 ${
+                        menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
+                      }`}
                     >
                       {props.textContent.links.about}
                     </a>
@@ -337,8 +349,9 @@ export default function Navbar(props: NavbarProps) {
                     }}
                     tabIndex={0}
                     href="https://drive.internxt.com/login"
-                    className={`flex w-full translate-y-0 px-8 py-3 text-primary outline-none transition delay-300 duration-300 ${menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
-                      }`}
+                    className={`flex w-full translate-y-0 px-8 py-3 text-primary outline-none transition delay-300 duration-300 ${
+                      menuState ? 'opacity-100' : '-translate-y-4 opacity-0'
+                    }`}
                   >
                     {props.textContent.links.login}
                   </a>
@@ -364,18 +377,20 @@ export default function Navbar(props: NavbarProps) {
             <div className="hidden space-x-2 lg:inline-flex">
               <Link href="/pricing" locale={props.lang}>
                 <a
-                  className={`whitespace-nowrap py-1.5 px-4 transition duration-150 ease-in-out ${props.darkMode ? 'text-white hover:text-cool-gray-20' : 'text-cool-gray-70 hover:text-cool-gray-90'
-                    } text-base font-medium`}
+                  className={`whitespace-nowrap py-1.5 px-4 transition duration-150 ease-in-out ${
+                    props.darkMode ? 'text-white hover:text-cool-gray-20' : 'text-cool-gray-70 hover:text-cool-gray-90'
+                  } text-base font-medium`}
                 >
                   {props.textContent.links.pricing}
                 </a>
               </Link>
 
               <div
-                className={`group relative flex space-x-1 py-1.5 px-4 pr-2 font-medium transition duration-150 ease-in-out ${props.darkMode
-                  ? 'text-white hover:bg-white hover:bg-opacity-10 hover:text-cool-gray-20'
-                  : 'text-cool-gray-70 hover:bg-cool-gray-100 hover:bg-opacity-5 hover:text-cool-gray-90'
-                  } cursor-default rounded-lg`}
+                className={`group relative flex space-x-1 py-1.5 px-4 pr-2 font-medium transition duration-150 ease-in-out ${
+                  props.darkMode
+                    ? 'text-white hover:bg-white hover:bg-opacity-10 hover:text-cool-gray-20'
+                    : 'text-cool-gray-70 hover:bg-cool-gray-100 hover:bg-opacity-5 hover:text-cool-gray-90'
+                } cursor-default rounded-lg`}
               >
                 <span>{props.textContent.links.products}</span>
                 <UilAngleDown className="h-6 w-6 translate-y-px text-cool-gray-20 transition duration-150 ease-in-out group-hover:text-cool-gray-30" />
@@ -387,8 +402,9 @@ export default function Navbar(props: NavbarProps) {
                   <div className="relative grid gap-0 whitespace-nowrap lg:grid-cols-1">
                     <Link href="/drive" locale={props.lang}>
                       <a
-                        className={`flex flex-row justify-start rounded-lg py-2 px-4 text-base font-medium text-cool-gray-80 ${props.darkMode ? 'hover:bg-cool-gray-10' : 'hover:bg-cool-gray-5'
-                          }`}
+                        className={`flex flex-row justify-start rounded-lg py-2 px-4 text-base font-medium text-cool-gray-80 ${
+                          props.darkMode ? 'hover:bg-cool-gray-10' : 'hover:bg-cool-gray-5'
+                        }`}
                       >
                         {props.textContent.products.drive}
                       </a>
@@ -396,8 +412,9 @@ export default function Navbar(props: NavbarProps) {
 
                     <Link href="/photos" locale={props.lang}>
                       <a
-                        className={`flex flex-row justify-start rounded-lg py-2 px-4 text-base font-medium text-cool-gray-80 ${props.darkMode ? 'hover:bg-cool-gray-10' : 'hover:bg-cool-gray-5'
-                          }`}
+                        className={`flex flex-row justify-start rounded-lg py-2 px-4 text-base font-medium text-cool-gray-80 ${
+                          props.darkMode ? 'hover:bg-cool-gray-10' : 'hover:bg-cool-gray-5'
+                        }`}
                       >
                         {props.textContent.products.photos}
                       </a>
@@ -407,8 +424,9 @@ export default function Navbar(props: NavbarProps) {
                       href="https://send.internxt.com"
                       target="_blank"
                       rel="noreferrer"
-                      className={`flex flex-row items-center justify-start rounded-lg py-2 px-4 text-base font-medium text-cool-gray-80 ${props.darkMode ? 'hover:bg-cool-gray-10' : 'hover:bg-cool-gray-5'
-                        }`}
+                      className={`flex flex-row items-center justify-start rounded-lg py-2 px-4 text-base font-medium text-cool-gray-80 ${
+                        props.darkMode ? 'hover:bg-cool-gray-10' : 'hover:bg-cool-gray-5'
+                      }`}
                     >
                       <span>{props.textContent.products.send}</span>
                       <span className="pointer-events-none ml-2 flex flex-row items-center whitespace-nowrap rounded-full bg-orange bg-opacity-15 px-2 text-supporting-2 font-medium uppercase text-orange">
@@ -421,8 +439,9 @@ export default function Navbar(props: NavbarProps) {
 
               <Link href="/privacy" locale={props.lang}>
                 <a
-                  className={`whitespace-nowrap py-1.5 px-4 transition duration-150 ease-in-out ${props.darkMode ? 'text-white hover:text-cool-gray-20' : 'text-cool-gray-70 hover:text-cool-gray-90'
-                    } text-base font-medium`}
+                  className={`whitespace-nowrap py-1.5 px-4 transition duration-150 ease-in-out ${
+                    props.darkMode ? 'text-white hover:text-cool-gray-20' : 'text-cool-gray-70 hover:text-cool-gray-90'
+                  } text-base font-medium`}
                 >
                   {props.textContent.links.privacy}
                 </a>
@@ -430,8 +449,9 @@ export default function Navbar(props: NavbarProps) {
 
               <Link href="/about" locale={props.lang}>
                 <a
-                  className={`whitespace-nowrap py-1.5 px-4 transition duration-150 ease-in-out ${props.darkMode ? 'text-white hover:text-cool-gray-20' : 'text-cool-gray-70 hover:text-cool-gray-90'
-                    } text-base font-medium`}
+                  className={`whitespace-nowrap py-1.5 px-4 transition duration-150 ease-in-out ${
+                    props.darkMode ? 'text-white hover:text-cool-gray-20' : 'text-cool-gray-70 hover:text-cool-gray-90'
+                  } text-base font-medium`}
                 >
                   {props.textContent.links.about}
                 </a>
@@ -441,29 +461,30 @@ export default function Navbar(props: NavbarProps) {
 
           {/* Login and CTA */}
           <div className="flex flex-1 flex-shrink-0 flex-grow flex-row items-center justify-end">
-            {
-              props.hideLogin === true ? '' : (
-                <button
-                  onClick={() => openAuthDialog('login')}
-                  className={`mr-2 hidden whitespace-nowrap rounded-full border py-1.5 px-4 transition duration-150 ease-in-out focus:border focus:outline-none md:flex ${props.darkMode && !menuState
+            {props.hideLogin === true ? (
+              ''
+            ) : (
+              <button
+                onClick={() => openAuthDialog('login')}
+                className={`mr-2 hidden whitespace-nowrap rounded-full border py-1.5 px-4 transition duration-150 ease-in-out focus:border focus:outline-none md:flex ${
+                  props.darkMode && !menuState
                     ? 'border-white text-white focus:opacity-80'
                     : 'border-primary text-primary active:border-primary-dark active:text-primary-dark'
-                    } text-sm font-medium`}
-                >
-                  {props.textContent.links.login}
-                </button>
-              )
-            }
-
+                } text-sm font-medium`}
+              >
+                {props.textContent.links.login}
+              </button>
+            )}
 
             {ctaAction[0] === 'default' ? (
               <button
                 onClick={() => openAuthDialog('signup')}
                 id="get-started-link"
-                className={`flex justify-center rounded-full border border-transparent py-1.5 px-4 text-sm font-medium focus:outline-none sm:inline-flex ${props.darkMode && !menuState
-                  ? 'bg-white text-cool-gray-90 focus:bg-cool-gray-10 active:bg-cool-gray-10'
-                  : 'bg-primary text-white active:bg-primary-dark'
-                  } transition-all duration-75`}
+                className={`flex justify-center rounded-full border border-transparent py-1.5 px-4 text-sm font-medium focus:outline-none sm:inline-flex ${
+                  props.darkMode && !menuState
+                    ? 'bg-white text-cool-gray-90 focus:bg-cool-gray-10 active:bg-cool-gray-10'
+                    : 'bg-primary text-white active:bg-primary-dark'
+                } transition-all duration-75`}
               >
                 <p className="whitespace-nowrap">{props.textContent.links.getStarted}</p>
               </button>
@@ -475,10 +496,11 @@ export default function Navbar(props: NavbarProps) {
               <button
                 type="button"
                 onClick={ctaAction[1]}
-                className={`flex justify-center rounded-full border border-transparent py-1.5 px-4 text-sm font-medium focus:outline-none sm:inline-flex ${props.darkMode && !menuState
-                  ? 'bg-white text-cool-gray-90 focus:bg-cool-gray-10 active:bg-cool-gray-10'
-                  : 'bg-primary text-white active:bg-primary-dark'
-                  } transition-all duration-75`}
+                className={`flex justify-center rounded-full border border-transparent py-1.5 px-4 text-sm font-medium focus:outline-none sm:inline-flex ${
+                  props.darkMode && !menuState
+                    ? 'bg-white text-cool-gray-90 focus:bg-cool-gray-10 active:bg-cool-gray-10'
+                    : 'bg-primary text-white active:bg-primary-dark'
+                } transition-all duration-75`}
               >
                 <p className="whitespace-nowrap">{props.textContent.links.checkout}</p>
               </button>
