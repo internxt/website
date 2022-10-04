ac7e93d580b3de7063dd1ea8116f45fa0d3bdbda
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-10-04 18:15:41 +0200

    Hero Section created

---
+++ components/cyber-awareness/HeroSection.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const HeroSection = ({ textContent }) => {
+  const router = useRouter();
+
+  const getPricingPage = (event) => {
+    event.preventDefault();
+    router.push('/pricing');
+  };
+
+  return (
+    <section className="overflow-hidden">
+      <div className="pt-16">
+        <div className=" flex h-60 items-center justify-center border-b border-white bg-primary">
+          <h1 className="text-center text-6xl font-semibold text-white">Cyber Awareness</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center p-5 md:p-20">
+          <div className="pt-10 xl:px-80">
+            <p className="text-2xl font-medium">{textContent.paragraph1.title}</p>
+            <p className="text-lg font-normal">{textContent.paragraph1.body}</p>
+            <p className="mt-8 text-lg font-normal">{textContent.paragraph1.body2}</p>
+          </div>
+          <div className="mt-16 flex xl:px-64">
+            <img src="/images/cyber-awareness/phising-emails.png" alt="phising emails" className="rounded-3xl" />
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-5 pt-11 md:p-20">
+          <div className="xl:px-80">
+            <p className="text-2xl font-medium">{textContent.paragraph2.title}</p>
+            <p className="text-lg font-normal">{textContent.paragraph2.body}</p>
+            <p className="mt-8 text-lg font-normal">{textContent.paragraph2.body2}</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-5 pt-11 md:p-20">
+          <div className="xl:px-80">
+            <p className="text-2xl font-medium">{textContent.paragraph3.title}</p>
+            <p className="text-lg font-normal">{textContent.paragraph3.body}</p>
+          </div>
+          <div className="mt-16 flex xl:px-64">
+            <img src="/images/cyber-awareness/all-eyes-of-you.png" alt="all eyes of you" className="rounded-3xl" />
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-5 pt-11 md:p-20">
+          <p className="text-center text-3xl font-semibold">{textContent.footer}</p>
+          <button
+            onClick={getPricingPage}
+            className="mt-8 flex h-10 w-56 items-center justify-center rounded-full bg-primary text-white"
+          >
+            {textContent.button}
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ pages/cyber-awareness.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import cookies from '../lib/cookies';
+import Navbar from '../components/layout/Navbar';
+import Layout from '../components/layout/Layout';
+import HeroSection from '../components/cyber-awareness/HeroSection';
+
+const CyberAwareness = ({ metatagsDescriptions, textContent, footerLang, navbarLang, lang }) => {
+  return (
+    <Layout segmentName={'Cyber Awareness'} isSendSnackbar={false}>
+      <Navbar textContent={navbarLang} cta={['default']} lang={lang} />
+
+      <HeroSection textContent={textContent.HeroSection} />
+    </Layout>
+  );
+};
+
+export async function getServerSideProps(ctx) {
+  const lang = ctx.locale;
+  const metatagsDescriptions = require(`../assets/lang/${lang}/metatags-descriptions.json`);
+  const textContent = require(`../assets/lang/${lang}/cyber-awareness.json`);
+  const footerLang = require(`../assets/lang/${lang}/footer.json`);
+  const navbarLang = require(`../assets/lang/${lang}/navbar.json`);
+
+  cookies.setReferralCookie(ctx);
+
+  return {
+    props: {
+      metatagsDescriptions,
+      textContent,
+      footerLang,
+      navbarLang,
+      lang,
+    },
+  };
+}
+
+export default CyberAwareness;
