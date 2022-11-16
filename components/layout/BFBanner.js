066ef386a8e4e9478943248c1c2915593e6e185a
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-11-16 16:08:54 +0100

    BfBanner lang updated
    TextWithoutJson file created for manage all lang into banners and popup

--- components/layout/BFBanner.js
+++ components/layout/BFBanner.js
@@ -2,11 +2,14 @@ import { CircleWavyCheck, X } from 'phosphor-react';
 import React from 'react';
 import styles from '../black-friday/BF-HeroSection.module.scss';
 import { useRouter } from 'next/router';
+import { buttonDeal, buttonLink } from '../TextWithoutJson';
+import { BFBannerText } from '../TextWithoutJson';
 
 const BFBanner = ({ bannerJson }) => {
   const [hideBanner, setHideBanner] = React.useState(false);
   const [showBanner, setShowBanner] = React.useState(false);
   const router = useRouter();
+  const lang = router.locale;
 
   setTimeout(() => {
     if (router.pathname !== '/black-friday') {
@@ -36,28 +39,28 @@ const BFBanner = ({ bannerJson }) => {
 
   const cards = [
     {
-      title: bannerJson.BFBanner.card.title1,
+      title: BFBannerText[lang].cards.title1,
     },
     {
-      title: bannerJson.BFBanner.card.title2,
+      title: BFBannerText[lang].cards.title2,
     },
     {
-      title: bannerJson.BFBanner.card.title3,
+      title: BFBannerText[lang].cards.title3,
     },
     {
-      title: bannerJson.BFBanner.card.title4,
+      title: BFBannerText[lang].cards.title4,
     },
     {
-      title: bannerJson.BFBanner.card.title5,
+      title: BFBannerText[lang].cards.title5,
     },
     {
-      title: bannerJson.BFBanner.card.title6,
+      title: BFBannerText[lang].cards.title6,
     },
   ];
 
   return (
     showBanner && (
-      //Fondo en gris
+      //Background
       <div
         className={`${
           !hideBanner ? 'flex' : 'hidden'
@@ -75,19 +78,17 @@ const BFBanner = ({ bannerJson }) => {
           <div className="flex w-auto flex-col p-20 md:flex-row">
             <div className="flex flex-col items-center justify-center text-center md:items-start md:justify-between md:pr-20 md:text-start">
               <div className="flex w-72 flex-col">
-                <p className="text-3xl font-semibold text-white">{bannerJson.BFBanner.head}</p>
-                <p className="pt-5 text-5xl font-bold text-white">{bannerJson.BFBanner.title}</p>
+                <p className="text-3xl font-semibold text-white">{BFBannerText[lang].head}</p>
+                <p className="pt-5 text-5xl font-bold text-white">{BFBannerText[lang].title}</p>
               </div>
               <div className="flex pt-7 md:pt-24">
                 <button
                   className="relative flex h-14 w-48 flex-row items-center justify-center space-x-4 rounded-4xl bg-primary px-8 text-base text-white transition duration-100 focus:outline-none focus-visible:bg-primary-dark active:bg-primary-dark sm:text-lg"
                   onClick={() => {
-                    window.location.replace(
-                      'https://internxt.com/black-friday?utm_source=website&utm_medium=banner&utm_campaign=blackfriday y https://drive.internxt.com/new?utm_source=website&utm_medium=banner&utm_campaign=blackfriday',
-                    );
+                    window.location.replace(buttonLink[lang]);
                   }}
                 >
-                  {bannerJson.BFBanner.buttonText}
+                  {buttonDeal[lang]}
                 </button>
               </div>
             </div>
--- components/layout/Popup.js
+++ components/layout/Popup.js
@@ -3,11 +3,17 @@ import styles from 'components/black-friday/BF-HeroSection.module.scss';
 import { X } from 'phosphor-react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { buttonDeal } from '../TextWithoutJson';
+import { buttonLink } from '../TextWithoutJson';
 
 const Popup = () => {
   const [hidePopup, setHidePopup] = React.useState(false);
   const router = useRouter();
   const lang = router.locale;
+
+  if (router.pathname === '/black-friday') {
+    return null;
+  }
 
   const handleClose = () => {
     localStorage.setItem('hidePopup', true);
@@ -50,35 +56,6 @@ const Popup = () => {
     }
   };
 
-  const buttonDeal = () => {
-    switch (lang) {
-      case 'en':
-        return 'Get the deal';
-
-      case 'es':
-        return 'Ir a la oferta';
-
-      case 'fr':
-        return "Obtenez l'offre";
-
-      default:
-        return 'Get the deal';
-    }
-  };
-
-  const buttonLinks = () => {
-    switch (lang) {
-      case 'en':
-        return 'https://internxt.com/black-friday?utm_source=website&utm_medium=banner&utm_campaign=blackfriday';
-      case 'es':
-        return 'https://internxt.com/es/black-friday?utm_source=website&utm_medium=banner&utm_campaign=blackfriday';
-      case 'fr':
-        return 'https://internxt.com/fr/black-friday?utm_source=website&utm_medium=banner&utm_campaign=blackfriday';
-      default:
-        return 'https://internxt.com/black-friday?utm_source=website&utm_medium=banner&utm_campaign=blackfriday';
-    }
-  };
-
   return (
     <div
       className={`fixed bottom-8 right-8 z-50 hidden max-h-[350px] max-w-[300px] flex-col py-5 px-8 lg:${
@@ -93,9 +70,9 @@ const Popup = () => {
       <div className="flex flex-col items-center justify-center space-y-5 text-center text-white">
         <p className="text-2xl font-bold">{title()}</p>
         <img src="/images/special-offer/black-friday/discount.png" className="flex h-auto w-auto" />
-        <Link href={buttonLinks()}>
+        <Link href={buttonLink[lang]}>
           <button className="relative flex h-14 w-48 flex-row items-center justify-center space-x-4 rounded-4xl bg-primary px-8 text-base text-white transition duration-100 focus:outline-none focus-visible:bg-primary-dark active:bg-primary-dark sm:text-lg">
-            {buttonDeal()}
+            {buttonDeal[lang]}
           </button>
         </Link>
       </div>
