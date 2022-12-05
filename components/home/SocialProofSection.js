a3ac2167f4f22754c1a3080042c92198891cee0a
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-12-05 18:00:57 +0100

    OTC logo added

--- components/home/SocialProofSection.js
+++ components/home/SocialProofSection.js
@@ -1,21 +1,22 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import Testimonial from './Testimonial';
+import { OTCLink } from '../TextWithoutJson';
 
-const SocialProofSection = ({ textContent }) => (
+const SocialProofSection = ({ textContent, lang }) => (
   <section>
     <div className="content">
-      <div className="flex flex-col items-center justify-center w-full text-center flex-shrink-0 px-10 md:px-0 py-12 md:py-24">
+      <div className="flex w-full flex-shrink-0 flex-col items-center justify-center px-10 py-12 text-center md:px-0 md:py-24">
         <h3 className="mb-8 text-4xl font-medium">{textContent.title}</h3>
 
-        <div className="flex flex-row justify-around flex-wrap sm:px-10 mb-8">
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
+        <div className="mb-8 flex flex-row flex-wrap justify-around sm:px-10">
+          <div className="flex h-20 w-2/5 flex-shrink-0 items-center justify-center md:w-1/3 xl:w-auto xl:px-6">
             <img loading="lazy" src="../../logos/investors/ovhcloud.svg" draggable="false" alt="ovh cloud logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
+          <div className="flex h-20 w-2/5 flex-shrink-0 items-center justify-center md:w-1/3 xl:w-auto xl:px-6">
             <img loading="lazy" src="../../logos/investors/telefonica.svg" draggable="false" alt="telefonica logo" />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
+          <div className="flex h-20 w-2/5 flex-shrink-0 items-center justify-center md:w-1/3 xl:w-auto xl:px-6">
             <img
               loading="lazy"
               src="../../logos/investors/theventurecity.svg"
@@ -23,25 +24,25 @@ const SocialProofSection = ({ textContent }) => (
               alt="the venture city logo"
             />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
+          <div className="flex h-20 w-2/5 flex-shrink-0 items-center justify-center md:w-1/3 xl:w-auto xl:px-6">
             <img
               loading="lazy"
               src="../../logos/investors/esade.webp"
-              className="h-7 mx-auto"
+              className="mx-auto h-7"
               draggable="false"
               alt="esade logo"
             />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
+          <div className="flex h-20 w-2/5 flex-shrink-0 items-center justify-center md:w-1/3 xl:w-auto xl:px-6">
             <img
               loading="lazy"
               src="../../logos/investors/notion_vc.webp"
-              className="h-4 mx-auto"
+              className="mx-auto h-4"
               draggable="false"
               alt="notion vc logo"
             />
           </div>
-          <div className="flex flex-shrink-0 w-2/5 md:w-1/3 xl:w-auto xl:px-6 h-20 items-center justify-center">
+          <div className="flex h-20 w-2/5 flex-shrink-0 items-center justify-center md:w-1/3 xl:w-auto xl:px-6">
             <img
               loading="lazy"
               src="../../logos/investors/angelscapital.svg"
@@ -49,6 +50,22 @@ const SocialProofSection = ({ textContent }) => (
               alt="angels capital logo"
             />
           </div>
+          {lang === 'es' && (
+            <div
+              className="flex h-20 w-2/5 flex-shrink-0 cursor-pointer items-center justify-center md:w-1/3 xl:w-auto xl:px-6"
+              onClick={() => {
+                window.location.href = 'https://opentrustedcloud.ovhcloud.com/es-es/';
+              }}
+            >
+              <img
+                loading="lazy"
+                src="../../logos/investors/open–trusted-cloud.png"
+                draggable="false"
+                alt="OTC logo"
+                className="mx-auto h-7"
+              />
+            </div>
+          )}
         </div>
 
         {/* <h3 className="my-8 text-3xl font-medium">
