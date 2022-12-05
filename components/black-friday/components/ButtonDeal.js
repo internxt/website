ae4476fabb66c88ed99c965ec7426675077604e5
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-12-05 08:29:25 +0100

    Banners and square removed

--- components/black-friday/components/ButtonDeal.js
+++ components/black-friday/components/ButtonDeal.js
@@ -10,7 +10,7 @@ const ButtonDeal = ({ lang }) => {
         className="relative flex flex-row items-center justify-center space-x-4 rounded-4xl bg-primary py-4 px-9 text-base text-white transition duration-100 focus:outline-none focus-visible:bg-primary-dark active:bg-primary-dark sm:text-lg"
         onClick={() => {
           // checkout(getPlanId(stripeObject));
-          window.location.href = 'https://internxt.com/pricing';
+          window.location.href = '/pricing';
         }}
       >
         {lang === 'fr' ? "Obtenez l'offre" : 'Get our subscription plans'}
