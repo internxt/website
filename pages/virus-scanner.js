d27e59f34596213ef9cf44e8ceb7bf1ff26e5f14
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-12-15 11:06:32 +0100

    Set english for all url langs (/es, /fr)

--- pages/virus-scanner.js
+++ pages/virus-scanner.js
@@ -5,52 +5,28 @@ import Layout from '../components/layout/Layout';
 import HeroSection from '../components/virus-scanner/HeroSection';
 import FeaturesSection from '../components/virus-scanner/FeaturesSection';
 
-const Scan = ({
-  metatagsDescriptions,
-  langJson,
-  navbarLang,
-  footerLang,
-  lang
-}) => {
+const Scan = ({ metatagsDescriptions, langJson, navbarLang, footerLang, lang }) => {
   const metatags = metatagsDescriptions.filter((desc) => desc.id === 'virus-scanner');
 
   return (
-
     <Layout segmentName="Virus Scanner" title={metatags[0].title} description={metatags[0].description} lang={lang}>
+      <Navbar textContent={navbarLang} lang={lang} cta={['default']} fixed />
 
-      <Navbar
-        textContent={navbarLang}
-        lang={lang}
-        cta={['default']}
-        fixed
-      />
+      <HeroSection textContent={langJson.HeroSection} />
 
-      <HeroSection
-        textContent={langJson.HeroSection}
-      />
+      <FeaturesSection textContent={langJson.FeaturesSection} lang={lang} />
 
-      <FeaturesSection
-        textContent={langJson.FeaturesSection}
-        lang={lang}
-      />
-
-      <Footer
-        textContent={footerLang}
-        lang={lang}
-        hideNewsletter={false}
-      />
-
+      <Footer textContent={footerLang} lang={lang} hideNewsletter={false} />
     </Layout>
-
   );
 };
 
 export async function getServerSideProps(ctx) {
   const lang = ctx.locale;
-  const metatagsDescriptions = require(`../assets/lang/${lang}/metatags-descriptions.json`);
-  const langJson = require(`../assets/lang/${lang}/virus-scanner.json`);
-  const footerLang = require(`../assets/lang/${lang}/footer.json`);
-  const navbarLang = require(`../assets/lang/${lang}/navbar.json`);
+  const metatagsDescriptions = require(`../assets/lang/en/metatags-descriptions.json`);
+  const langJson = require(`../assets/lang/en/virus-scanner.json`);
+  const footerLang = require(`../assets/lang/en/footer.json`);
+  const navbarLang = require(`../assets/lang/en/navbar.json`);
 
   return {
     props: {
@@ -58,7 +34,7 @@ export async function getServerSideProps(ctx) {
       langJson,
       footerLang,
       navbarLang,
-      lang
+      lang,
     },
   };
 }
