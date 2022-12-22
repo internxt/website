a722571ede46ada0b2d56d2216571722f3191eb9
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-12-22 13:48:04 +0100

    All sections added
    The only thing left to do is to update the converter and add the table

--- components/converter-tool/FaqSection.js
+++ components/converter-tool/FaqSection.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import FaqAccordion from '../password-checker/FaqAccordion';
 
 const FaqSection = ({ textContent }) => {
   return (
-    <section className="overflow-hidden">
-      <div className="flex flex-col items-center justify-center py-[60px]"></div>
+    <section className="overflow-hidden bg-gray-1">
+      <div className="flex flex-col items-center justify-center space-y-16 py-[60px]">
+        <p className="text-4xl font-semibold">{textContent.title}</p>
+        <div className="flex w-full max-w-[850px] flex-col space-y-2 pt-10">
+          {textContent.faq.faq.map((item, index) => (
+            <div className="rounded-lg border border-gray-20 px-5" key={index}>
+              <FaqAccordion key={item.question} question={item.question} answer={item.answer} isQuestionBigger />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
--- pages/converter-tool.js
+++ pages/converter-tool.js
@@ -6,10 +6,12 @@ import ExplanationSection from '../components/converter-tool/ExplanationSection'
 import CtaSection from '../components/converter-tool/CtaSection';
 import InfoSection from '../components/converter-tool/InfoSection';
 import ConversionTableSection from '../components/converter-tool/ConversionTableSection';
+import FaqSection from '../components/converter-tool/FaqSection';
+import Footer from '../components/layout/Footer';
 
 const CONVERTER_TOOL_METATAG_ID = 'converter-tool';
 
-const ConverterTool = ({ lang, metatagsDescriptions, navbarLang, langJson }) => {
+const ConverterTool = ({ lang, metatagsDescriptions, navbarLang, langJson, footerLang }) => {
   const metatags = metatagsDescriptions.filter((desc) => desc.id === CONVERTER_TOOL_METATAG_ID);
 
   return (
@@ -27,6 +29,12 @@ const ConverterTool = ({ lang, metatagsDescriptions, navbarLang, langJson }) => 
       <CtaSection textContent={langJson.ctaSection2} />
 
       <ConversionTableSection textContent={langJson.ConversionTableSection} />
+
+      <FaqSection textContent={langJson.FaqSection} />
+
+      <CtaSection textContent={langJson.ctaSection3} />
+
+      <Footer textContent={footerLang} lang={lang} />
     </Layout>
   );
 };
@@ -37,6 +45,7 @@ export async function getServerSideProps(ctx) {
   const metatagsDescriptions = require(`../assets/lang/en/metatags-descriptions.json`);
   const langJson = require(`../assets/lang/en/converter-tool.json`);
   const navbarLang = require(`../assets/lang/en/navbar.json`);
+  const footerLang = require(`../assets/lang/en/footer.json`);
 
   return {
     props: {
@@ -44,6 +53,7 @@ export async function getServerSideProps(ctx) {
       metatagsDescriptions,
       navbarLang,
       langJson,
+      footerLang,
     },
   };
 }
