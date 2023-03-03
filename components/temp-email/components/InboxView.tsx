bfe72039d29193ce5c7c80df82b89f3d076f54ce
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-03-03 13:49:42 +0100

    fix EmptyView

--- components/temp-email/components/InboxView.tsx
+++ components/temp-email/components/InboxView.tsx
@@ -69,87 +69,88 @@ const Inbox = ({ email }) => {
 const InboxWeb = ({ email, getProps }: { email: string; getProps: Record<string, any> }) => {
   const { messages, selectedMessage, setSelectedMessage, setIsRefreshed } = getProps;
 
-  return messages.length < 0 ? (
+  return (
     <div className="flex h-[512px] w-full max-w-3xl flex-row space-y-2 overflow-hidden rounded-xl border border-gray-10 shadow-subtle-hard">
-      <div className="flex flex-col">
-        {/* Messages list */}
-        <div className="flex h-full w-screen max-w-[256px] flex-col items-start justify-start rounded-l-xl border-r border-gray-10">
-          {/* Header messages list */}
-          <div className="flex w-full flex-row justify-between rounded-tl-xl border-b border-gray-10 bg-gray-5 px-4 py-5">
-            <div className="flex flex-row items-center space-x-1">
-              <Tray size={20} weight="bold" />
-              <p className="text-base font-medium">Inbox</p>
-            </div>
-            <ArrowsClockwise
-              size={24}
-              className="cursor-pointer text-gray-50"
-              onClick={() => {
-                setIsRefreshed((prevState) => !prevState);
-              }}
-            />
-          </div>
-          {/* Messages list */}
-          <div className="flex flex-col overflow-y-scroll">
-            {messages.map((item, index) => {
-              const date = moment(item.date);
-              return (
-                <button
-                  key={item.id}
+      {messages.length < 0 ? (
+        <>
+          <div className="flex flex-col">
+            <div className="flex h-full w-screen max-w-[256px] flex-col items-start justify-start rounded-l-xl border-r border-gray-10">
+              <div className="flex w-full flex-row justify-between rounded-tl-xl border-b border-gray-10 bg-gray-5 px-4 py-5">
+                <div className="flex flex-row items-center space-x-1">
+                  <Tray size={20} weight="bold" />
+                  <p className="text-base font-medium">Inbox</p>
+                </div>
+                <ArrowsClockwise
+                  size={24}
+                  className="cursor-pointer text-gray-50"
                   onClick={() => {
-                    setSelectedMessage(item);
-                    const newMessages = [...messages];
-                    newMessages[index].opened = true;
+                    setIsRefreshed((prevState) => !prevState);
                   }}
-                  className={`flex h-full ${
-                    !item.opened ? 'border-l-2 border-l-primary' : ''
-                  } w-full flex-col px-4 text-start hover:bg-primary hover:bg-opacity-15  focus:bg-primary focus:bg-opacity-10`}
-                >
-                  <div className="flex flex-col border-b border-gray-10 py-4">
-                    <p title={item.from} className="text-xs font-medium text-gray-50">
-                      {item.from}
-                    </p>
-                    {item.attachments.length > 0 ? (
-                      <div className="flex flex-row items-center space-x-1">
-                        <Paperclip size={14} className="text-gray-60" />
-                        <p title={item.subject} className="flex-row text-sm font-semibold">
-                          {item.subject}
-                        </p>
-                      </div>
-                    ) : (
-                      <p title={item.subject} className="flex-row text-sm font-semibold">
-                        {item.subject}
-                      </p>
-                    )}
-                    <div className="flex flex-row items-end justify-end space-x-2">
-                      <p className="w-full text-xs line-clamp-2">{item.textBody}</p>
-                      <p className="text-supporting-2 font-semibold text-gray-60">
-                        {moment().isSame(date, 'day') ? date.format('HH:mm') : date.format('MMM DD')}
-                      </p>
-                    </div>
-                  </div>
-                </button>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-      {/* Show message box */}
+                />
+              </div>
 
-      <Transition
-        show={selectedMessage ? true : false}
-        enter="transition-opacity duration-800"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-800"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <Messages.MessageSelected email={email} item={selectedMessage} />
-      </Transition>
-      {!selectedMessage && <Messages.NoMessageSelected />}
+              <div className="flex flex-col overflow-y-scroll">
+                {messages.map((item, index) => {
+                  const date = moment(item.date);
+                  return (
+                    <button
+                      key={item.id}
+                      onClick={() => {
+                        setSelectedMessage(item);
+                        const newMessages = [...messages];
+                        newMessages[index].opened = true;
+                      }}
+                      className={`flex h-full ${
+                        !item.opened ? 'border-l-2 border-l-primary' : ''
+                      } w-full flex-col px-4 text-start hover:bg-primary hover:bg-opacity-15  focus:bg-primary focus:bg-opacity-10`}
+                    >
+                      <div className="flex flex-col border-b border-gray-10 py-4">
+                        <p title={item.from} className="text-xs font-medium text-gray-50">
+                          {item.from}
+                        </p>
+                        {item.attachments.length > 0 ? (
+                          <div className="flex flex-row items-center space-x-1">
+                            <Paperclip size={14} className="text-gray-60" />
+                            <p title={item.subject} className="flex-row text-sm font-semibold">
+                              {item.subject}
+                            </p>
+                          </div>
+                        ) : (
+                          <p title={item.subject} className="flex-row text-sm font-semibold">
+                            {item.subject}
+                          </p>
+                        )}
+                        <div className="flex flex-row items-end justify-end space-x-2">
+                          <p className="w-full text-xs line-clamp-2">{item.textBody}</p>
+                          <p className="text-supporting-2 font-semibold text-gray-60">
+                            {moment().isSame(date, 'day') ? date.format('HH:mm') : date.format('MMM DD')}
+                          </p>
+                        </div>
+                      </div>
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <Transition
+            show={selectedMessage ? true : false}
+            enter="transition-opacity duration-800"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-800"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Messages.MessageSelected email={email} item={selectedMessage} />
+          </Transition>
+          {!selectedMessage && <Messages.NoMessageSelected />}
+        </>
+      ) : (
+        <EmptyInbox />
+      )}
     </div>
-  ) : (
-    <EmptyInbox />
   );
 };
 
