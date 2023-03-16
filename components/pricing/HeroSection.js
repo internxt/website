02628345966ce850cb84a5f7d45cbda5f9d84be3
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-03-16 15:06:39 +0100

    Update HeroSection view on small screens

--- components/pricing/HeroSection.js
+++ components/pricing/HeroSection.js
@@ -24,29 +24,31 @@ const HeroSection = ({ textContent }) => {
 
   return (
     <section className="overflow-hidden pt-12">
-      <div className="flex flex-col items-center justify-center space-y-10 py-24 px-6 md:flex-row md:space-y-0 md:space-x-48">
+      <div className="flex flex-col items-center justify-center space-y-10 py-24 px-6 lg:flex-row lg:space-y-0 lg:space-x-48">
         <div className="flex flex-col space-y-10">
-          <div className="flex max-w-[470px] flex-col items-center justify-center space-y-10 md:items-start">
+          <div className="flex max-w-[470px] flex-col items-center justify-center space-y-10 lg:items-start">
             <div className="flex flex-row rounded-lg bg-gray-5 px-5 py-2">
               <Alarm size={32} className="mr-4 text-primary" />
               <Countdown textColor={'black'} />
             </div>
             <div className="flex flex-col space-y-16">
-              <div className="flex flex-col text-center md:text-start">
+              <div className="flex flex-col text-center lg:text-start">
                 <p className="text-7xl font-bold">{textContent.title.line1}</p>
                 <p className="text-7xl font-bold text-primary">{textContent.title.line2}</p>
               </div>
-              <div className="flex flex-col space-y-4">
-                {feeds.map((feed) => (
-                  <div className="flex flex-row items-center space-x-4" key={feed.title}>
-                    <feed.icon size={32} className="text-primary" />
-                    <p className="text-xl font-medium text-gray-80">{feed.title}</p>
-                  </div>
-                ))}
+              <div className="flex flex-col items-center justify-center lg:items-start">
+                <div className="flex flex-col items-start space-y-4">
+                  {feeds.map((feed) => (
+                    <div className="flex flex-row items-center space-x-4" key={feed.title}>
+                      <feed.icon size={32} className="text-primary" />
+                      <p className="text-xl font-medium text-gray-80">{feed.title}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col space-x-8 md:flex-row md:items-center md:justify-center">
+          <div className="flex flex-col items-center justify-center space-y-5 space-x-8 lg:flex-row lg:space-y-0">
             <button
               className="flex w-full items-center justify-center rounded-lg bg-primary px-5 py-3 font-semibold text-white"
               onClick={() =>
