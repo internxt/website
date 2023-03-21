8660d9eafdb70abb86df8cd280090c86bc702ac2
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2023-03-21 14:56:34 +0100

    Added FAQ for Drive
    - Fixed language typos for Directory of Privacy Organizations FAQ

--- pages/drive.js
+++ pages/drive.js
@@ -1,6 +1,9 @@
+import Script from 'next/script';
 import React from 'react';
+
 import HeroSection from '../components/drive/HeroSection';
 import FeaturesSection from '../components/drive/FeaturesSection';
+import FAQSection from '../components/drive/FAQSection';
 import Footer from '../components/layout/Footer';
 import Navbar from '../components/layout/Navbar';
 import ProductsNavigation from '../components/layout/ProductsNavigation';
@@ -8,52 +11,31 @@ import Layout from '../components/layout/Layout';
 import cookies from '../lib/cookies';
 import { downloadDriveLinks } from '../lib/get-download-url';
 
-const Drive = ({
-  metatagsDescriptions,
-  download,
-  langJson,
-  navbarLang,
-  footerLang,
-  lang
-}) => {
+import { sm_faq } from '../components/utils/schema-markup-generator';
+
+const Drive = ({ metatagsDescriptions, download, langJson, navbarLang, footerLang, lang }) => {
   const metatags = metatagsDescriptions.filter((desc) => desc.id === 'drive');
 
   return (
+    <>
+      <Script type="application/ld+json" strategy="beforeInteractive">
+        {sm_faq(langJson.FaqSection.faq)}
+      </Script>
 
-    <Layout title={metatags[0].title} description={metatags[0].description} segmentName="Drive" lang={lang}>
+      <Layout title={metatags[0].title} description={metatags[0].description} segmentName="Drive" lang={lang}>
+        <Navbar textContent={navbarLang} lang={lang} cta={['default']} fixed />
 
-      <Navbar
-        textContent={navbarLang}
-        lang={lang}
-        cta={['default']}
-        fixed
-      />
+        <ProductsNavigation textContent={navbarLang} lang={lang} selectedItem="drive" />
 
-      <ProductsNavigation
-        textContent={navbarLang}
-        lang={lang}
-        selectedItem="drive"
-      />
+        <HeroSection textContent={langJson.HeroSection} lang={lang} download={download} />
 
-      <HeroSection
-        textContent={langJson.HeroSection}
-        lang={lang}
-        download={download}
-      />
+        <FeaturesSection textContent={langJson.FeaturesSection} lang={lang} download={download} />
 
-      <FeaturesSection
-        textContent={langJson.FeaturesSection}
-        lang={lang}
-        download={download}
-      />
+        <FAQSection textContent={langJson.FaqSection} />
 
-      <Footer
-        textContent={footerLang}
-        lang={lang}
-      />
-
-    </Layout>
-
+        <Footer textContent={footerLang} lang={lang} />
+      </Layout>
+    </>
   );
 };
 
@@ -70,7 +52,12 @@ export async function getServerSideProps(ctx) {
 
   return {
     props: {
-      lang, download, metatagsDescriptions, langJson, navbarLang, footerLang
+      lang,
+      download,
+      metatagsDescriptions,
+      langJson,
+      navbarLang,
+      footerLang,
     },
   };
 }
