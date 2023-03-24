ebc62338a86a3d6735b41436f68f3c1da6a24808
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2023-03-24 12:33:48 +0100

    Fixed parallax responsiveness and key error

--- components/home/SecondFeaturesSection.tsx
+++ components/home/SecondFeaturesSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import RevealY from '../components/RevealY';
 import RevealX from '../components/RevealX';
 import { CloudCheck, Gauge, Image, SketchLogo } from 'phosphor-react';
@@ -36,9 +36,8 @@ const SecondFeaturesSection = ({ textContent }) => {
       <RevealY className="hidden flex-row items-start justify-center lg:flex">
         <RevealX direction="right" className="flex max-w-[250px] flex-col">
           {textContent.info.map((info, index) => (
-            <>
+            <Fragment key={info.title}>
               <div
-                key={info.title}
                 className={`flex ${
                   cardIndex === index ? 'border-gray-80' : 'border-gray-10'
                 } cursor-pointer flex-row items-center border-r-4  pr-8`}
@@ -49,7 +48,7 @@ const SecondFeaturesSection = ({ textContent }) => {
                 <p className="text-2xl font-medium text-gray-100">{info.title}</p>
               </div>
               <div className="h-8 border-r-4 border-gray-10 pr-8 last:hidden" />
-            </>
+            </Fragment>
           ))}
         </RevealX>
         <div className="flex flex-col justify-start">
