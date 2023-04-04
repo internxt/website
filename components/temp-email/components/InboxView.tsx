29e3ced7c3feae4f045e2b60f077584118582b7a
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-04-04 18:32:01 +0200

    Update empty inbox

--- components/temp-email/components/InboxView.tsx
+++ components/temp-email/components/InboxView.tsx
@@ -99,94 +99,90 @@ const InboxWeb = ({ email, getProps }: { email: string; getProps: Record<string,
 
   return (
     <div className="flex h-[512px] w-full max-w-3xl flex-row space-y-2 overflow-hidden rounded-xl border border-gray-10 shadow-subtle-hard">
-      {messages.length > 0 ? (
-        <>
-          <div className="flex flex-col">
-            <div className="flex h-full w-screen max-w-[256px] flex-col items-start justify-start rounded-l-xl border-r border-gray-10">
-              <div className="flex w-full flex-row justify-between rounded-tl-xl border-b border-gray-10 bg-gray-5 px-4 py-5">
-                <div className="flex flex-row items-center space-x-1">
-                  <Tray size={20} weight="bold" />
-                  <p className="text-base font-medium">Inbox</p>
-                </div>
-                <ArrowsClockwise
-                  size={24}
-                  className="cursor-pointer text-gray-50 hover:text-gray-80"
-                  onClick={() => {
-                    setIsRefreshed((prevState) => !prevState);
-                  }}
-                />
+      <>
+        <div className="flex flex-col">
+          <div className="flex h-full w-screen max-w-[256px] flex-col items-start justify-start rounded-l-xl border-r border-gray-10">
+            <div className="flex w-full flex-row justify-between rounded-tl-xl border-b border-gray-10 bg-gray-5 px-4 py-5">
+              <div className="flex flex-row items-center space-x-1">
+                <Tray size={20} weight="bold" />
+                <p className="text-base font-medium">Inbox</p>
               </div>
+              <ArrowsClockwise
+                size={24}
+                className="cursor-pointer text-gray-50 hover:text-gray-80"
+                onClick={() => {
+                  setIsRefreshed((prevState) => !prevState);
+                }}
+              />
+            </div>
 
-              <div className="flex w-full flex-col overflow-y-scroll">
-                {messages.map((item, index) => {
-                  const date = moment(item.date);
-                  return (
-                    <button
-                      key={item.id}
-                      onClick={() => {
-                        //Update the message to local storage
-                        const newMessages = [...JSON.parse(localStorage.getItem('inbox'))];
-                        newMessages[index].opened = true;
-                        setMessages(newMessages);
-                        setSelectedMessage(item);
-                        localStorage.setItem('inbox', JSON.stringify(newMessages));
-                        localStorage.setItem('selectedMessage', JSON.stringify(item));
-                      }}
-                      className={`flex h-full ${
-                        !item.opened ? 'border-l-2 border-l-primary' : ''
-                      } w-full flex-col px-4 text-start hover:bg-primary hover:bg-opacity-15 ${
-                        item.id === selectedMessage?.id ? 'bg-primary bg-opacity-10' : null
-                      } `}
-                    >
-                      <div className="flex w-full max-w-[224px] flex-col border-b border-gray-10 py-4">
-                        <p title={item.from} className="truncate text-xs font-medium text-gray-50">
-                          {item.from}
-                        </p>
-                        {item.attachments.length > 0 ? (
-                          <div className="flex flex-row items-center space-x-1">
-                            <Paperclip size={14} className="text-gray-60" />
-                            <p title={item.subject} className="flex-row text-sm font-semibold line-clamp-2">
-                              {item.subject ? item.subject : '(no subject)'}
-                            </p>
-                          </div>
-                        ) : (
+            <div className="flex w-full flex-col overflow-y-scroll">
+              {messages.map((item, index) => {
+                const date = moment(item.date);
+                return (
+                  <button
+                    key={item.id}
+                    onClick={() => {
+                      //Update the message to local storage
+                      const newMessages = [...JSON.parse(localStorage.getItem('inbox'))];
+                      newMessages[index].opened = true;
+                      setMessages(newMessages);
+                      setSelectedMessage(item);
+                      localStorage.setItem('inbox', JSON.stringify(newMessages));
+                      localStorage.setItem('selectedMessage', JSON.stringify(item));
+                    }}
+                    className={`flex h-full ${
+                      !item.opened ? 'border-l-2 border-l-primary' : ''
+                    } w-full flex-col px-4 text-start hover:bg-primary hover:bg-opacity-15 ${
+                      item.id === selectedMessage?.id ? 'bg-primary bg-opacity-10' : null
+                    } `}
+                  >
+                    <div className="flex w-full max-w-[224px] flex-col border-b border-gray-10 py-4">
+                      <p title={item.from} className="truncate text-xs font-medium text-gray-50">
+                        {item.from}
+                      </p>
+                      {item.attachments.length > 0 ? (
+                        <div className="flex flex-row items-center space-x-1">
+                          <Paperclip size={14} className="text-gray-60" />
                           <p title={item.subject} className="flex-row text-sm font-semibold line-clamp-2">
                             {item.subject ? item.subject : '(no subject)'}
                           </p>
-                        )}
-                        <div className="flex flex-row items-end justify-end space-x-2">
-                          <p className="w-full text-xs line-clamp-2">{item.textBody}</p>
-                          <p className="text-supporting-2 font-semibold text-gray-60">
-                            {moment().isSame(date, 'day') ? date.format('HH:mm') : date.format('MMM DD')}
-                          </p>
                         </div>
+                      ) : (
+                        <p title={item.subject} className="flex-row text-sm font-semibold line-clamp-2">
+                          {item.subject ? item.subject : '(no subject)'}
+                        </p>
+                      )}
+                      <div className="flex flex-row items-end justify-end space-x-2">
+                        <p className="w-full text-xs line-clamp-2">{item.textBody}</p>
+                        <p className="text-supporting-2 font-semibold text-gray-60">
+                          {moment().isSame(date, 'day') ? date.format('HH:mm') : date.format('MMM DD')}
+                        </p>
                       </div>
-                    </button>
-                  );
-                })}
-              </div>
+                    </div>
+                  </button>
+                );
+              })}
             </div>
           </div>
+        </div>
 
-          <Transition
-            show={selectedMessage ? true : false}
-            enter="transition-opacity easy-in-out duration-800"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity easy-in-out duration-800"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            className={'flex overflow-y-scroll'}
-          >
-            <div className="flex h-full w-screen">
-              <Messages.MessageSelected email={email} item={selectedMessage} />
-            </div>
-          </Transition>
-          {!selectedMessage && <Messages.NoMessageSelected messagesLength={openedMessages} />}
-        </>
-      ) : (
-        <EmptyInbox />
-      )}
+        <Transition
+          show={selectedMessage ? true : false}
+          enter="transition-opacity easy-in-out duration-800"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity easy-in-out duration-800"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className={'flex overflow-y-scroll'}
+        >
+          <div className="flex h-full w-screen">
+            <Messages.MessageSelected email={email} item={selectedMessage} />
+          </div>
+        </Transition>
+        {!selectedMessage && <Messages.NoMessageSelected messagesLength={openedMessages} />}
+      </>
     </div>
   );
 };
@@ -300,7 +296,7 @@ const InboxMobile = ({ email, getProps }: { email: string; getProps: Record<stri
           </Transition>
         </>
       ) : (
-        <EmptyInbox />
+        !selectedMessage && <Messages.NoMessageSelected messagesLength={0} />
       )}
     </div>
   );
