113b9f2821038f9fcb4a4bec6e096e41c179c7b2
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-04-24 14:05:20 +0200

    Add prompt banner for Android devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,36 @@ import Script from 'next/script';
 import { Intercom, LiveChatLoaderProvider } from 'react-live-chat-loader';
 import { GlobalDialog, GlobalUIManager } from '../contexts/GlobalUIManager';
 import { useRouter } from 'next/router';
+import { isMobile } from 'react-device-detect';
 
 // const excludedPaths = ['/byte-converter', '/virus-scanner', '/password-checker', '/temporary-email'];
 
 function MyApp({ Component, pageProps }: AppProps) {
   const route = useRouter();
+  let deferredPrompt: any;
+  const [showPromoteBanner, setShowPromoteBanner] = React.useState(false);
   // const pathname = route.pathname;
   // const isExcludedPath = excludedPaths.findIndex((path) => pathname.includes(path)) !== -1;
   // const bannerLang = require(`../assets/lang/${route.locale}/banners.json`);
+
+  //Add promote banner for Android users to download the app
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', function (e) {
+      console.log('beforeinstallprompt Event fired');
+      // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setShowPromoteBanner(true);
+
+      // Optionally, send analytics event that PWA install promo was shown.
+      console.log(`'beforeinstallprompt' event was fired.`);
+
+      // Show the prompt
+      deferredPrompt.prompt();
+    });
+  }, []);
 
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
@@ -20,6 +42,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       <GlobalUIManager initialDialogs={[{ key: GlobalDialog.Auth, isOpen: false }]}>
         <Script strategy="beforeInteractive" src="/js/rudderlib.js" />
         <Component {...pageProps} />
+        {showPromoteBanner && (
+          <div className="bottom-0 flex w-full bg-black">
+            <p className="text-white">Promote banner works properly</p>
+          </div>
+        )}
         {/* {isExcludedPath ? null : <GeneralBanner textContent={bannerLang.GeneralBanner} />} */}
         <Intercom />
       </GlobalUIManager>
