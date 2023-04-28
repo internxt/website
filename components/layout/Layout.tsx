4e0bbe63aecd574ff2633c2cda8e3b267bb0991f
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-04-28 14:42:22 +0200

    Fix x-default link

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -81,7 +81,7 @@ LayoutProps) {
         <link rel="alternate" hrefLang="fr" href={`${INTERNXT_URL}/fr${pathname}`} />
         <link rel="alternate" hrefLang="it" href={`${INTERNXT_URL}/it${pathname}`} />
         <link rel="alternate" hrefLang="zh" href={`${INTERNXT_URL}/cn${pathname}`} />
-        <link rel="alternate" hrefLang="x-default" href={`https://internxt.com/${pathname}`} />
+        <link rel="alternate" hrefLang="x-default" href="https://internxt.com/" />
         <meta charSet="utf-8" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
