a9262c90e79cc06ff8b1c415ef696e1c0f8c3b8b
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-04-28 15:19:03 +0200

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -80,7 +80,7 @@ LayoutProps) {
         <link rel="alternate" hrefLang="es" href={`${INTERNXT_URL}/es${pathname}`} />
         <link rel="alternate" hrefLang="fr" href={`${INTERNXT_URL}/fr${pathname}`} />
         <link rel="alternate" hrefLang="it" href={`${INTERNXT_URL}/it${pathname}`} />
-        <link rel="alternate" hrefLang="zh" href={`${INTERNXT_URL}/cn${pathname}`} />
+        <link rel="alternate" hrefLang="zh" href={`${INTERNXT_URL}/zh${pathname}`} />
         <link rel="alternate" hrefLang="x-default" href="https://internxt.com/" />
         <meta charSet="utf-8" />
         <meta property="og:title" content={title} />
