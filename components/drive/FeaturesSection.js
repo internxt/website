a4417273eefe789d9b3c73047756e2a124ac92d0
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-05-03 17:08:09 +0200

    Fix the Reveal direction

--- components/drive/FeaturesSection.js
+++ components/drive/FeaturesSection.js
@@ -158,7 +158,7 @@ const FeaturesSection = ({ textContent, lang, download }) => (
       {/* Feature #3 - Share your files with ease, security is on us */}
       <div className="grid w-full grid-cols-1 items-center justify-center lg:grid-cols-2 lg:gap-20 lg:pt-20 lg:pb-20">
         <div className="hidden flex-col items-start object-contain object-left pt-10 lg:left-0 lg:flex">
-          <RevealX className="hidden flex-col shadow-subtle-hard lg:flex" direction="left">
+          <RevealX className="hidden flex-col shadow-subtle-hard lg:flex" direction="right">
             <Image
               className="h-full w-full bg-no-repeat "
               src="/images/drive/File-sharing.webp"
