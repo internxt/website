e9f657dab1a3ca62ee141b26fe9a0a7913dc2934
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-05-05 13:16:48 +0200

    Fix banner design

--- components/banners/GeneralBanner.tsx
+++ components/banners/GeneralBanner.tsx
@@ -34,15 +34,15 @@ const GeneralBanner = ({ textContent }) => {
         <button className="absolute right-0 m-7 flex w-auto text-white" onClick={onClose}>
           <X size={32} />
         </button>
-        <div className="flex  flex-col items-center justify-center p-14">
+        <div className="flex  flex-col items-center justify-center px-24 pt-20 pb-16">
           <div className="flex flex-col items-center justify-center space-y-10 text-center">
-            <div className="flex max-w-[610px] flex-col items-center">
-              <p className="text-5xl font-bold text-white ">{textContent.title}</p>
+            <div className="flex w-full max-w-[610px] flex-col items-center">
+              <p className="text-5xl font-bold text-white">{textContent.title}</p>
               <p className=" pt-4 text-3xl font-medium text-white">{textContent.description}</p>
             </div>
             <div className="flex flex-col items-center justify-center space-y-6">
               <div
-                className="relative flex cursor-pointer flex-col items-center justify-center rounded-lg bg-primary py-4 px-9 text-base text-white focus:outline-none focus-visible:bg-primary-dark active:bg-primary-dark sm:text-lg"
+                className="relative flex cursor-pointer flex-col items-center justify-center rounded-lg bg-primary py-3 px-5 text-base font-medium text-white focus:outline-none focus-visible:bg-primary-dark active:bg-primary-dark sm:text-lg"
                 onClick={() => {
                   checkout({
                     planId: 'plan_FkTXxEg3GZW0pg',
@@ -53,7 +53,7 @@ const GeneralBanner = ({ textContent }) => {
                 {textContent.cta}
               </div>
               <div
-                className="relative flex w-auto cursor-pointer flex-col items-center justify-center  rounded-lg border border-white bg-transparent py-4 px-9 text-base text-white transition duration-100 focus:outline-none active:bg-white active:text-black sm:text-lg"
+                className="relative flex w-full cursor-pointer flex-col items-center justify-center rounded-lg bg-white py-3 px-5 text-base font-medium text-primary transition duration-100 focus:outline-none active:bg-white active:text-black sm:text-lg"
                 onClick={() => {
                   onClose();
                 }}
