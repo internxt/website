9d7c25f0f256c252325d87e2cc4d578725f56fd6
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-06-26 08:30:28 +0200

    Remove summer banner from stackcommerce page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router';
 import SummerBanner from '../components/banners/SummerBanner';
 import ShowSnackbar from '../components/Snackbar';
 
-const excludedPaths = ['/techcult', '/pricing'];
+const excludedPaths = ['/techcult', '/pricing', '/stackcommerce'];
 
 function MyApp({ Component, pageProps }: AppProps) {
   const route = useRouter();
