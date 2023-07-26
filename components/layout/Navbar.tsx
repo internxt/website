55126b32678ced0cb6d0fa8d1ed76618d90c831e
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-07-26 12:22:49 +0200

    wip

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -298,8 +298,8 @@ export default function Navbar(props: NavbarProps) {
                 {/* Mobile hamburger menu */}
                 {
                   <div
-                    className={`absolute left-0 top-20 w-full bg-white font-semibold transition-all duration-500 ${
-                      menuState ? 'h-screen pb-14' : 'h-0 overflow-hidden'
+                    className={`absolute left-0 top-20 w-full overflow-hidden bg-white font-semibold transition-all duration-500 ${
+                      menuState ? 'h-screen pb-14' : 'h-0 '
                     }`}
                   >
                     <div className="mt-4 flex flex-col text-gray-100">
