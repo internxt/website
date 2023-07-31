55d5e6a746735fcf00b5a3c81826bb137cb35b8e
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-07-31 11:13:50 +0200

    feat: add Header component to comparison page

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,13 @@
-const Header = ({ children }) => {
-  return (
-    <h1 className="max-w-[796px] text-5xl font-semibold text-gray-100 sm:text-6xl lg:leading-tight">{children}</h1>
-  );
+const Header = ({
+  children,
+  maxWidth = 'max-w-[796px]',
+  className,
+}: {
+  children: React.ReactNode;
+  maxWidth?: string;
+  className?: string;
+}) => {
+  return <h1 className={`${maxWidth} ${className} text-5xl font-semibold sm:text-6xl lg:leading-tight`}>{children}</h1>;
 };
 
 export default Header;
