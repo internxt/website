5e559725452699b54db7fdde602dff102e27afea
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-08-02 14:03:02 +0200

    fix: privacy directory (German lang)

--- pages/privacy-directory.tsx
+++ pages/privacy-directory.tsx
@@ -42,7 +42,7 @@ const PrivacyDirectory = ({
         description={metatags[0].description}
         lang={lang}
       >
-        {!['en'].includes(lang) ? (
+        {!['en', 'de'].includes(lang) ? (
           <>
             <Navbar textContent={navbarLang} lang={lang} cta={['default']} darkMode />
             <HeroSection
