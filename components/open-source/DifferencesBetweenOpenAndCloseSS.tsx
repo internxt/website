440b055139ce56f1f5fec59f0f99a2f8926f3ddb
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-08-08 12:29:04 +0200

    fix: tables in other languages (height)

--- components/open-source/DifferencesBetweenOpenAndCloseSS.tsx
+++ components/open-source/DifferencesBetweenOpenAndCloseSS.tsx
@@ -11,49 +11,53 @@ const DifferencesBetweenOpenAndCloseSS = ({ textContent }) => {
         </div>
 
         {/* Tables */}
-        <div className="flex w-full flex-col space-y-10 md:flex-row md:space-y-0 md:space-x-20 lg:items-center lg:justify-center">
-          <div className="flex max-w-[387px] flex-col lg:w-full lg:max-w-[387px]">
-            <div className="flex w-full items-center space-x-4 rounded-l-lg bg-primary bg-opacity-3 py-4 pl-6">
-              <img src={'/images/open-source/open-source.svg'} className="h-8 w-8" alt="Open Source" />
-              <p className="text-lg font-semibold text-gray-100">{textContent.table.OpenSource.title}</p>
+        <div className="flex w-full">
+          <div className="flex w-full flex-col space-y-10 md:flex-row md:space-y-0 md:space-x-20 lg:items-start lg:justify-center">
+            {/* Table 1 */}
+            <div className="flex h-full max-w-[387px] flex-1 flex-col">
+              <div className="flex w-full items-center space-x-4 rounded-l-lg bg-primary bg-opacity-3 py-4 pl-6">
+                <img src={'/images/open-source/open-source.svg'} className="h-8 w-8" alt="Open Source" />
+                <p className="text-lg font-semibold text-gray-100">{textContent.table.OpenSource.title}</p>
+              </div>
+              <div className="flex h-full flex-col">
+                {textContent.table.OpenSource.feat.map((item, index) => (
+                  <div className="flex h-full w-full flex-row items-center bg-primary bg-opacity-3" key={index}>
+                    <div className="flex h-full w-full flex-row items-center justify-between border-b border-primary border-opacity-3 bg-gray-1 pl-6">
+                      <div className="flex w-[275px] py-4 lg:max-w-[275px]">
+                        <p className="text-gray-80">{item}</p>
+                      </div>
+                    </div>
+                    <div className="flex h-full w-full flex-col md:w-max md:px-4">
+                      <div className="flex flex-grow flex-col items-center justify-center">
+                        <Check size={32} className="text-green-dark" weight="bold" />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-            <div className="flex h-full flex-col">
-              {textContent.table.OpenSource.feat.map((item, index) => (
-                <div className="flex h-full w-full flex-row items-center bg-primary bg-opacity-3" key={index}>
-                  <div className="flex h-full w-full flex-row items-center justify-between border-b border-primary border-opacity-3 bg-gray-1 pl-6">
-                    <div className="flex w-[275px] py-4 lg:max-w-[275px]">
-                      <p className="text-gray-80">{item}</p>
+            {/* Table 2 */}
+            <div className="flex h-full w-full max-w-[387px] flex-col">
+              <div className="flex w-full items-center space-x-4 rounded-l-lg bg-primary bg-opacity-3 py-4 pl-6">
+                <img src={'/images/open-source/close-source.svg'} className="h-8 w-8" alt="Closed Source" />
+                <p className="text-lg font-semibold text-gray-100">{textContent.table.CloseSource.title}</p>
+              </div>
+              <div className="flex h-full flex-col">
+                {textContent.table.CloseSource.disadvantages.map((item, index) => (
+                  <div className="flex h-full w-full flex-row items-center bg-primary bg-opacity-3" key={index}>
+                    <div className="flex h-full w-full flex-row items-center justify-between border-b border-primary border-opacity-3 bg-gray-1 pl-6">
+                      <div className="flex w-[275px] py-4 lg:max-w-[275px]">
+                        <p className="text-gray-80">{item}</p>
+                      </div>
+                    </div>
+                    <div className="flex h-full w-full flex-col md:w-max md:px-4">
+                      <div className="flex flex-grow flex-col items-center justify-center">
+                        <X size={32} className="text-red-dark" weight="bold" />
+                      </div>
                     </div>
                   </div>
-                  <div className="flex h-full w-full flex-col md:w-max md:px-4">
-                    <div className="flex flex-col items-center justify-center ">
-                      <Check size={32} className="text-green-dark" weight="bold" />
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="flex w-full max-w-[387px] flex-col lg:w-full lg:max-w-[387px]">
-            <div className="flex w-full items-center space-x-4 rounded-l-lg bg-primary bg-opacity-3 py-4 pl-6">
-              <img src={'/images/open-source/close-source.svg'} className="h-8 w-8" alt="Closed Source" />
-              <p className="text-lg font-semibold text-gray-100">{textContent.table.CloseSource.title}</p>
-            </div>
-            <div className="flex h-full flex-col">
-              {textContent.table.CloseSource.disadvantages.map((item, index) => (
-                <div className="flex h-full w-full flex-row items-center bg-primary bg-opacity-3" key={index}>
-                  <div className="flex h-full w-full flex-row items-center justify-between border-b border-primary border-opacity-3 bg-gray-1 pl-6">
-                    <div className="flex w-[275px] py-4 lg:max-w-[275px]">
-                      <p className="text-gray-80">{item}</p>
-                    </div>
-                  </div>
-                  <div className="flex h-full w-full flex-col md:w-max md:px-4">
-                    <div className="flex flex-grow flex-col items-center justify-center md:px-4">
-                      <X size={32} className="text-red-dark" weight="bold" />
-                    </div>
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
