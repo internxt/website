675f9b2ae6666284bea848a22602a0c9d3a270e9
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-08-28 10:21:39 +0200

    [_]: feat/remove lifetime banner
    Lifetime banner removed due to end of campaign

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -134,7 +134,6 @@ LayoutProps) {
       {showBanner ? (
         <>
           <TopBannerHomePage isBannerFixed={isBannerFixed} />
-          <SquareBanner /> 
           <div className="z-50 flex flex-col overflow-hidden pt-[64px] md:pt-[54px]">{children}</div>
         </>
       ) : (
