d7453fb3186de9a5faee9f1dc5ac120e65c0d07b
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-10-11 15:53:01 +0200

    fix: 3 LP created
    * internxt-for-journalists
    * internxt-for-digital-nomads
    * internxt-for-students

--- components/success-stories/HeroSection.tsx
+++ components/success-stories/HeroSection.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 const HeroSection = ({ textContent }) => {
   return (
     <section className="overflow-hidden">
-      <div className="mx-4 w-full">
-        <div className="lg: mx-auto flex max-w-screen-xl items-center justify-center lg:justify-start">
-          <div className="flex flex-col space-y-10 py-20 px-10 lg:flex-row lg:space-y-0 lg:space-x-32 lg:pt-32 lg:pb-20">
+      <div className="w-full lg:mx-4">
+        <div className="flex max-w-screen-xl items-center justify-center lg:mx-auto lg:justify-start">
+          <div className="flex flex-col space-y-10 space-x-0 py-20 px-5 lg:flex-row lg:space-y-0 lg:space-x-32 lg:pt-32 lg:pb-20">
             <div className="flex max-w-[533px] flex-col space-y-8  text-center lg:text-left">
               <h1 className="text-5xl font-semibold text-gray-100 md:text-6xl">
                 {textContent.title.how} <span className="text-primary">{textContent.title.blueText}</span>{' '}
