037aaa2fc085a44e69035b6cc07af5f73c57e301
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-10-13 10:23:31 +0200

    feat: add learn more button linked to WhatWeDo section

--- components/success-stories/WhatWeDo.tsx
+++ components/success-stories/WhatWeDo.tsx
@@ -4,7 +4,7 @@ import SignUpBanner from '../banners/SignUpBanner';
 
 const WhatWeDo = ({ textContent, bannerLang }) => {
   return (
-    <section className="overflow-hidden bg-gray-1">
+    <section id={'whatWeDo'} className="overflow-hidden bg-gray-1">
       <div className="flex flex-col items-center space-y-16 py-20 px-5">
         <div className="flex max-w-[796px] flex-col space-y-6">
           <div className="flex flex-row items-center space-x-6">
--- components/use-cases/HeroSection.tsx
+++ components/use-cases/HeroSection.tsx
@@ -2,8 +2,8 @@ import Header from '../shared/Header';
 
 const HeroSection = ({ textContent }) => {
   return (
-    <section className="overflow-hidden">
-      <div className="flex flex-col items-center space-y-6 py-20 px-5 text-center lg:pt-32 lg:pb-20">
+    <section className="overflow-hidden pt-14">
+      <div className="flex flex-col items-center space-y-6 px-5 py-16 pb-20 text-center lg:py-20">
         <Header className="text-gray-100">
           {textContent.title.normalText} <br />
           <span className="text-primary">{textContent.title.blueText}</span>
