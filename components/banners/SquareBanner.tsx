f186ea4c3343d01681246b2ca91886d9c9b014af
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-10-17 17:20:11 +0200

    fix: update next, react and react-dom to last version

--- components/banners/SquareBanner.tsx
+++ components/banners/SquareBanner.tsx
@@ -44,7 +44,7 @@ const SquareBanner = () => {
       <div className="relative flex flex-col items-center justify-center space-y-5 text-center text-white">
         <p className="text-2xl font-medium">Cyber Security Awareness Month</p>
         <div className="flex flex-col">
-          <Image src="/images/banners/80off-white.svg" width={217} height={95} />
+          <Image src="/images/banners/80off-white.svg" width={217} height={95} alt="80 off image" />
         </div>
         <button
           className="flex w-max flex-row items-center justify-center space-x-4 rounded-lg bg-white py-2.5 px-5 text-base font-medium text-primary transition duration-100 focus:outline-none focus-visible:bg-gray-10 active:bg-gray-10 sm:text-lg"
