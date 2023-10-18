76be127224cd041c1cf037dfa5c148fcbbe68b58
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-10-18 14:34:41 +0200

    fix: slack notification Action

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: process.env.NODE_ENV === 'production' ? 'https://internxt.com' : 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 1500,
     viewportHeight: 660,
     scrollBehavior: 'center',
