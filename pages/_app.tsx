c8a5cda101c0c9f75d655dbace9ed122d4107f68
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-11-08 15:58:33 +0100

    fix: remove unused code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -60,29 +60,6 @@ function MyApp({ Component, pageProps }: AppProps) {
               `,
             }}
           />
-          {/* <Script
-            strategy="afterInteractive"
-            id="matomo"
-            dangerouslySetInnerHTML={{
-              __html: `
-            var _paq = (window._paq = window._paq || []);
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function () {
-              var u = 'https://inxt.matomo.cloud/';
-              _paq.push(['setTrackerUrl', u + 'matomo.php']);
-              _paq.push(['setSiteId', '1']);
-              var d = document,
-                g = d.createElement('script'),
-                s = d.getElementsByTagName('script')[0];
-              g.async = true;
-              g.defer=true;
-              g.src = u + 'matomo.js';
-              s.parentNode.insertBefore(g, s);
-            })();
-            `,
-            }}
-          /> */}
         </>
 
         <Component {...pageProps} />
