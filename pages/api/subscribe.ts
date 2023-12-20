05e24a805bcbf21809e53a15e82ccb6c09af3441
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-12-20 13:13:38 +0100

    feat: add mailerlite endpoint to eBook pages

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const email = req.body.email;
+  const { email, firstName, groups } = req.body;
 
   try {
-    const user = await createUser(email);
+    const user = await createUser(email, groups, firstName);
 
     res.status(200).json({ status: 'OK', data: user });
   } catch (error) {
@@ -14,18 +14,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
-async function createUser(email: string) {
-  const createUser = await axios.post(
-    `${process.env.MAILERLITE_API}/api/subscribers`,
-    { email: email, status: 'active', groups: ['88604472985519229'] },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${process.env.MAILERLITE_API_KEY}`,
-      },
+async function createUser(email: string, groups: string[], firstName?: string) {
+  const object = {
+    email: email,
+    fields: {
+      name: firstName,
     },
-  );
+    status: 'active',
+    groups: groups,
+  };
+
+  const createUser = await axios.post(`${process.env.MAILERLITE_API}/api/subscribers`, object, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.MAILERLITE_API_KEY}`,
+    },
+  });
 
   return createUser.data;
 }
