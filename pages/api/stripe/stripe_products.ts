6e52661026386a793d59ddf84b9b7be6b8beb34d
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-01-03 09:42:26 +0100

    feat: change name of cached stripe prod

--- pages/api/stripe/stripe_products.ts
+++ pages/api/stripe/stripe_products.ts
@@ -15,7 +15,7 @@ const PRODUCTS_URL = `${process.env.NEXT_PUBLIC_PAYMENTS_API}/prices`;
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<Product | void> {
   if (req.method === 'GET') {
     const { currency } = req.query;
-    const cachedProducts = cache.get('products');
+    const cachedProducts = cache.get('priceProducts');
     const productsRequest = await axios.get(PRODUCTS_URL + `?currency=${currency ?? 'eur'}`);
     const productsData: Product[] = productsRequest.data;
 
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     // Cached for 5 hours
-    cache.put('products', productsData, 1000 * 60 * 60 * 5);
+    cache.put('priceProducts', productsData, 1000 * 60 * 60 * 5);
     res.status(200).json(productsData);
   } else {
     res.status(405).end(); // Method Not Allowed
