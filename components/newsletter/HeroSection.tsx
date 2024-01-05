92cf24411ff124cd599e5ec27d7131010f1f4d9b
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-01-05 18:18:28 +0100

    feat: add mailerlite to the main card

--- components/newsletter/HeroSection.tsx
+++ components/newsletter/HeroSection.tsx
@@ -17,15 +17,36 @@ const HeroSection = ({ textContent }) => {
           <p className="max-w-[700px] text-xl text-gray-80">{textContent.description}</p>
         </div>
         {/* Card */}
-        <div className="flex w-full max-w-[568px] flex-col justify-center rounded-lg border border-primary/7 bg-primary/2 p-9 ring-5 ring-primary/7">
-          <TextInput placeholder={textContent.form.emailLabel} onChange={(e) => setEmail(e.target.value)} />
+        <form
+          data-code="r3s4c1"
+          method="post"
+          target="_blank"
+          rel="noopener"
+          action="https://app.mailerlite.com/webforms/submit/r3s4c1"
+          className="flex w-full max-w-[568px] flex-col justify-center rounded-lg border border-primary/7 bg-primary/2 p-9 ring-5 ring-primary/7"
+        >
+          <input type="hidden" name="ml-submit" value="1" />
+          <input
+            name="fields[email]"
+            type="email"
+            placeholder={`${textContent.form.emailLabel}`}
+            className={`flex h-auto w-full appearance-none flex-row rounded-lg border border-cool-gray-20 bg-white px-4 py-3 text-left text-lg outline-none
+            transition-all duration-150 focus:border-blue-50 focus:ring focus:ring-primary focus:ring-opacity-20 sm:py-2 sm:text-base`}
+            required
+          />
           <div className="flex flex-col space-y-6 pt-9">
             <CheckboxItem checked={checkbox} setCheckbox={setCheckbox} label={textContent.form.checkboxText} />
-            <button className="flex w-full justify-center rounded-lg bg-primary py-3 font-medium text-white">
-              {textContent.form.cta}
-            </button>
+            <input
+              name="signup"
+              type="submit"
+              disabled={!checkbox}
+              value={`${textContent.form.cta}`}
+              className={`${
+                !checkbox ? 'cursor-default bg-gray-20 text-gray-40' : 'bg-primary hover:bg-primary-dark'
+              } flex w-full cursor-pointer items-center justify-center rounded-lg border border-transparent px-4 py-3 text-lg font-medium text-white transition-all duration-75 focus:outline-none sm:mb-2 sm:py-2 sm:text-base`}
+            />
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
