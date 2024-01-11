43f8c038f914ff846993350cf76b3b80c20b80a0
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-01-11 11:54:01 +0100

    fix: show signup banner in mobile

--- components/banners/SignUpBanner.tsx
+++ components/banners/SignUpBanner.tsx
@@ -6,17 +6,17 @@ const SignUpBanner = ({ textContent, lang }: { textContent: any; lang: string })
   const signUpLang = require(`../../assets/lang/${lang}/home.json`);
 
   return (
-    <section className="hidden w-full max-w-[857px] overflow-hidden bg-gradient-to-br from-blue-20 to-white lg:flex">
+    <section className="flex w-full max-w-[857px] overflow-hidden bg-gradient-to-br from-blue-20 to-white">
       <div className="flex flex-row items-center justify-center">
-        <div className="mt-11 mb-11 ml-11 flex w-full max-w-[495px] flex-col items-center justify-center space-y-10">
-          <p className="text-4xl font-semibold">
+        <div className="mt-11 mb-11 flex w-full max-w-[495px] flex-col items-center justify-center space-y-10 px-5 lg:ml-11 lg:px-0">
+          <p className="text-center text-4xl font-semibold lg:text-left">
             {textContent.line1} <span className="text-primary">{textContent.blueText}</span>
           </p>
           <div className="flex w-full">
             <SignUpInline textContent={signUpLang.HeroSection.SignUp} isBanner />
           </div>
         </div>
-        <div className="absolute -right-64 flex items-center">
+        <div className="absolute -right-64 hidden items-center lg:flex">
           <div className="relative right-0  flex flex-col bg-contain">
             <Image
               src="/images/home/Internxt-secure-cloud-storage.webp"
