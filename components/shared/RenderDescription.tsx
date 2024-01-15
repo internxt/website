dda00ac2812f639d40f24ba98331c9f5bd81b777
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-01-15 09:58:30 +0100

    feat: change text in section6 (password-checker)

--- components/shared/RenderDescription.tsx
+++ components/shared/RenderDescription.tsx
@@ -19,7 +19,7 @@ const RenderDescription = ({ description, fontSize }: { description: string[]; f
   return (
     <div id="render-description-section" className="markdown flex flex-col space-y-4">
       {description.map((item, index) => (
-        <ReactMarkdown key={item[index]} className={`${fontSize ? fontSize : 'text-lg'} text-gray-80`}>
+        <ReactMarkdown key={item} className={`${fontSize ? fontSize : 'text-lg'} text-gray-80`}>
           {item}
         </ReactMarkdown>
       ))}
