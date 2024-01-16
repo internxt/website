3202f6f7a520b92cb51e9b29ca3317b6d717cb81
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-01-16 13:09:16 +0100

    fix: temporary mail box (email/copy/delete buttons)

--- components/temp-email/HeroSection.tsx
+++ components/temp-email/HeroSection.tsx
@@ -69,7 +69,7 @@ const HeroSection = ({ textContent }) => {
           <Header isToolsPage>{textContent.title}</Header>
           <p className="max-w-2xl pt-5 text-xl text-gray-80">{textContent.subtitle}</p>
         </div>
-        <div className="flex flex-col items-center rounded-2xl border-4 border-primary/7 bg-primary/2 p-9">
+        <div className="flex flex-col items-center rounded-2xl border-4 border-primary/7 bg-primary/2 px-5 py-5 lg:p-9">
           <div className="flex w-full flex-col items-center justify-center space-y-3">
             <div
               className={`flex h-full w-full max-w-[400px] items-center justify-center rounded-xl ${
