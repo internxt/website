31a69592ff4af7210ff0ca63ed78eacbc46f2494
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-02 17:42:12 +0100

    feat: create src directory and add sonar cloud properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const { defaultTheme } = require('react-select');
 
 module.exports = {
-  content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     colors: {
