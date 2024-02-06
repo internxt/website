c2c59ffba78a494f5f07ca11653bd2ab4586b2e9
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-06 16:41:12 +0100

    feat: fetch coupon code in custom hook

--- src/components/partner-discount/PriceTable.tsx
+++ src/components/partner-discount/PriceTable.tsx
@@ -1,38 +1,13 @@
 /* eslint-disable max-len */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Transition } from '@headlessui/react';
 import PriceCard from '@/components/partner-discount/PriceCard';
-import { ProductsProps, stripeService } from '@/components/services/stripe.service';
 import CardSkeleton from '@/components/components/CardSkeleton';
 import { CouponType } from '@/pages/api/stripe/get_coupons';
-import { currencyService } from '@/components/services/currency.service';
+import useStripeAndCurrency from '@/hooks/useProducts';
 
 export default function PriceTable({ lang }: { lang: string }) {
-  const [products, setProducts] = useState<ProductsProps>();
-  const [loadingCards, setLoadingCards] = useState(true);
-  const [coupon, setCoupon] = useState<string>();
-  const [currency, setCurrency] = useState({
-    symbol: '€',
-    value: 1,
-  });
-
-  useEffect(() => {
-    Promise.all([
-      stripeService.getAllPrices(),
-      currencyService.filterCurrencyByCountry(),
-      stripeService.getCoupon(CouponType.Special15Coupon),
-    ])
-      .then((res) => {
-        setProducts(res[0]);
-        setCurrency({
-          symbol: res[1].symbol,
-          value: res[1].value,
-        });
-        setCoupon(res[2]);
-        setLoadingCards(false);
-      })
-      .catch((err) => console.error(err));
-  }, []);
+  const { products, currency, loadingCards, coupon } = useStripeAndCurrency(CouponType.Special15Coupon);
 
   return (
     <section id="priceTable" className="">
@@ -71,7 +46,7 @@ export default function PriceTable({ lang }: { lang: string }) {
                     cta={['checkout', product.priceId]}
                     lang={lang}
                     country={currency.symbol}
-                    coupon={coupon}
+                    coupon={coupon ?? undefined}
                   />
                 );
               })}
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,47 +1,83 @@
 import { currencyService } from '@/components/services/currency.service';
 import { ProductsProps, stripeService } from '@/components/services/stripe.service';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
+import { CouponType } from '@/pages/api/stripe/get_coupons';
 
-function useStripeAndCurrency() {
-  const [products, setProducts] = useState<ProductsProps>();
-  const [loadingCards, setLoadingCards] = useState(true);
-  const [currency, setCurrency] = useState({ symbol: '€', value: 1 });
+// Definir los tipos de acción
+type ActionType =
+  | { type: 'SET_PRODUCTS'; payload: ProductsProps | undefined }
+  | { type: 'SET_LOADING_CARDS'; payload: boolean }
+  | { type: 'SET_CURRENCY'; payload: { symbol: string; value: number } }
+  | { type: 'SET_COUPON'; payload: CouponType | null };
 
-  useEffect(() => {
-    const fetchData = async () => {
+const reducer = (state: any, action: ActionType) => {
+  switch (action.type) {
+    case 'SET_PRODUCTS':
+      return { ...state, products: action.payload };
+    case 'SET_LOADING_CARDS':
+      return { ...state, loadingCards: action.payload };
+    case 'SET_CURRENCY':
+      return { ...state, currency: action.payload };
+    case 'SET_COUPON':
+      return { ...state, coupon: action.payload };
+    default:
+      return state;
+  }
+};
+
+function useStripeAndCurrency(couponCode?: CouponType): {
+  products: ProductsProps | undefined;
+  loadingCards: boolean;
+  currency: { symbol: string; value: number };
+  coupon: CouponType | null;
+} {
+  const initialState = {
+    products: undefined,
+    loadingCards: true,
+    currency: { symbol: '€', value: 1 },
+    coupon: null,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchData = async () => {
+    try {
+      const prices = await stripeService.getAllPrices();
+      dispatch({ type: 'SET_PRODUCTS', payload: prices });
+      dispatch({ type: 'SET_LOADING_CARDS', payload: false });
+    } catch (error) {
       try {
-        const prices = await stripeService.getAllPrices();
-        setProducts(prices);
-        setLoadingCards(false);
+        const res = await stripeService.getAllPrices(true);
+        dispatch({ type: 'SET_PRODUCTS', payload: res });
+        dispatch({ type: 'SET_LOADING_CARDS', payload: false });
       } catch (error) {
-        try {
-          const res = await stripeService.getAllPrices(true);
-          setProducts(res);
-          setLoadingCards(false);
-        } catch (error) {
-          console.error('Error getting prices');
-        }
+        console.error('Error getting prices');
       }
+    }
 
+    try {
+      const res = await currencyService.filterCurrencyByCountry();
+      dispatch({ type: 'SET_CURRENCY', payload: { symbol: res.symbol, value: res.value } });
+    } catch (err) {
+      dispatch({ type: 'SET_CURRENCY', payload: { symbol: '€', value: 1 } });
+      console.error(err);
+    }
+
+    if (couponCode) {
       try {
-        const res = await currencyService.filterCurrencyByCountry();
-        setCurrency({
-          symbol: res.symbol,
-          value: res.value,
-        });
+        const coupon = await stripeService.getCoupon(couponCode);
+        dispatch({ type: 'SET_COUPON', payload: coupon });
       } catch (err) {
-        setCurrency({
-          symbol: '€',
-          value: 1,
-        });
         console.error(err);
       }
-    };
+    }
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
 
-  return { products, loadingCards, currency };
+  return { ...state };
 }
 
 export default useStripeAndCurrency;
