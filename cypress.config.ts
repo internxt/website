40d3fb8b2d2a297b4ab964a3d2abfc18eca1cdd8
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-07 17:46:55 +0100

    wip (updating sonar action)

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config);
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
     },
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1500,
