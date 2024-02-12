504259d0509f5df3989924125bc644ca8bd916fd
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-12 13:11:37 +0100

    WIP (adding an artifact to the cypress Action)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     viewportWidth: 1500,
     viewportHeight: 660,
-    scrollBehavior: 'center',
+    scrollBehavior: "center",
     chromeWebSecurity: false,
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
   },
 });
---
+++ cypress/e2e/temp-mail.cy.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+export {};
+
+describe('Testing Temporary Email API', () => {
+  it('Should generate a temporary email', () => {
+    cy.request({
+      method: 'GET',
+      url: '/api/temp-mail/create-email',
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+
+      expect(response.body).to.have.property('address');
+      expect(response.body).to.have.property('expired');
+    });
+  });
+});
