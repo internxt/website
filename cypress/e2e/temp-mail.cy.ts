4a153cb5bd375dca216f782230cfd3fe9199d112
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-12 13:22:17 +0100

    WIP

--- cypress/e2e/temp-mail.cy.ts
+++ cypress/e2e/temp-mail.cy.ts
@@ -2,15 +2,21 @@
 export {};
 
 describe('Testing Temporary Email API', () => {
-  it('Should generate a temporary email', () => {
-    cy.request({
-      method: 'GET',
-      url: '/api/temp-mail/create-email',
-    }).then((response) => {
-      expect(response.status).to.eq(200);
+  it(
+    'Should generate a temporary email',
+    {
+      baseUrl: 'https://internxt.com',
+    },
+    () => {
+      cy.request({
+        method: 'GET',
+        url: '/api/temp-mail/create-email',
+      }).then((response) => {
+        expect(response.status).to.eq(200);
 
-      expect(response.body).to.have.property('address');
-      expect(response.body).to.have.property('expired');
-    });
-  });
+        expect(response.body).to.have.property('address');
+        expect(response.body).to.have.property('token');
+      });
+    },
+  );
 });
