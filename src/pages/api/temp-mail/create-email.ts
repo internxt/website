a430d226918406e4e05d8ffb9fc3c973577023ed
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-14 14:10:10 +0100

    feat: add rate-limit to temp mail API

--- src/pages/api/temp-mail/create-email.ts
+++ src/pages/api/temp-mail/create-email.ts
@@ -1,11 +1,19 @@
+import rateLimit from '@/components/utils/rate-limit';
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const TEMP_MAIL_API_KEY = process.env.TEMP_MAIL_API_KEY;
 
+const limiter = rateLimit({
+  interval: 60 * 1000,
+  uniqueTokenPerInterval: 1000,
+});
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
+      await limiter.check(res, 5, 'create-email');
+
       const email = await axios.get(`${process.env.NEXT_PUBLIC_TEMP_MAIL_URL}/inbox/create`, {
         headers: {
           Authorization: `Bearer ${TEMP_MAIL_API_KEY}`,
