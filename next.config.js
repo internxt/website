c3f8937d94f3f44e92c4ad90c07d22a428e2db7d
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-14 16:55:59 +0100

    fix: adding domain to Access control allow origin

--- next.config.js
+++ next.config.js
@@ -29,11 +29,10 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'internxt.com' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
         ],
       },
