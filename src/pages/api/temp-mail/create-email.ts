9c67f73d4080d5f0d7f8f1d390e8ecea0f1682f8
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-15 10:02:33 +0100

    fix: remove console log

--- src/pages/api/temp-mail/create-email.ts
+++ src/pages/api/temp-mail/create-email.ts
@@ -12,8 +12,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         },
       });
 
-      console.log('REFERER: ', req.headers.referer);
-
       return res.status(200).json(email.data);
     } catch (err) {
       const error = err as Error;
