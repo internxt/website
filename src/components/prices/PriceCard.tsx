db3deceac49b17dcedce8d965cfb7e7393ee02b7
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-21 16:00:54 +0100

    feat: add most popular label

--- src/components/prices/PriceCard.tsx
+++ src/components/prices/PriceCard.tsx
@@ -1,5 +1,6 @@
 import { checkout, goToSignUpURL } from '@/lib/auth';
 import { CouponType } from '@/lib/types/types';
+import { Fire } from '@phosphor-icons/react';
 
 export interface PriceCardProps {
   planType: string;
@@ -52,11 +53,15 @@ export default function PriceCard({
       } m-2 flex max-w-xs flex-shrink-0 flex-grow-0 flex-col overflow-hidden rounded-2xl xs:w-72`}
     >
       <div className={`info flex flex-col items-center justify-center space-y-6 rounded-t-2xl bg-white p-6 pt-6`}>
-        <div className="flex flex-col items-center justify-center space-y-2">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          {popular ? (
+            <div className="flex flex-row items-center justify-center space-x-2 rounded-full bg-primary px-3 py-1">
+              <Fire size={28} className="text-white" />
+              <p className="font-semibold text-white">{contentText.mostPopular}</p>
+            </div>
+          ) : null}
           <div className="flex rounded-full bg-primary/10 px-3 py-0.5">
-            <p className="text-lg font-medium text-primary">
-              <p>{storage}</p>
-            </p>
+            <p className="text-lg font-medium text-primary">{storage}</p>
           </div>
         </div>
         <div
@@ -106,7 +111,7 @@ export default function PriceCard({
           }}
           className={`flex w-full flex-col items-center rounded-lg border ${
             popular
-              ? 'bg-primary text-white hover:bg-primary-dark'
+              ? 'border-primary bg-primary text-white hover:bg-primary-dark'
               : 'border-primary text-primary hover:bg-gray-1 active:bg-gray-5'
           } whitespace-nowrap px-20 py-2.5 font-medium`}
         >
