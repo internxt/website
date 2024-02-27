aa99280142123783087017161f976eb86b5b8805
Fran Villalba Segarra <fvsegarra@gmail.com>
2024-02-27 23:10:28 +0100

    Update temporary-email.tsx

--- src/pages/temporary-email.tsx
+++ src/pages/temporary-email.tsx
@@ -24,7 +24,6 @@ const TempEmail = ({ metatagsDescriptions, toolsContent, textContent, footerLang
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6684818764777307"
           crossOrigin="anonymous"
         ></script>
-
         <script async src="https://fundingchoicesmessages.google.com/i/pub-6684818764777307?ers=1" nonce="zBx-cZ19JdrTC6F0HQkCnQ"></script><script nonce="zBx-cZ19JdrTC6F0HQkCnQ">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>   
       </Head>
       <Script type="application/ld+json" strategy="beforeInteractive">
