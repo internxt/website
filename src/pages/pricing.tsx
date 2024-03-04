fb884ceb3a130698387aa31f7d437093b8526990
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-03-04 16:08:40 +0100

    feat: hide "Save up to..." label

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -34,7 +34,7 @@ const Pricing = ({ metatagsDescriptions, navbarLang, footerLang, lang, textConte
 
         {/* <HeroSection textContent={textContent.HeroSection} /> */}
 
-        <PriceTable setSegmentPageName={setPageName} lang={lang} textContent={textContent.tableSection} discount={23} />
+        <PriceTable setSegmentPageName={setPageName} lang={lang} textContent={textContent.tableSection} />
 
         <CtaSection textContent={textContent.CtaSection} freePlan />
 
