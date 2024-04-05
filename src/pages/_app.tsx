14984da92be0cdfaa9ebd88c3f835be5e82a5a28
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-05 10:52:00 +0200

    fix: remove intercom button from pc componentes page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ const EXCLUDED_PATHS = [
   '/oystervpn',
 ];
 
-const excludeIntercomPaths = ['/temporary-email', '/virus-scanner', 'pccomponentes-products'];
+const excludeIntercomPaths = ['/temporary-email', '/virus-scanner', '/pccomponentes-products'];
 
 const isProduction = process.env.NODE_ENV === 'production';
 
