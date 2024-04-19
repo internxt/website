a53951e9853a2ea835a6766874536e56f4235456
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-19 17:09:54 +0200

    feat: add the bg images for WhenUseVPNSection

--- src/components/vpn-extension/WhenUseVPNSection.tsx
+++ src/components/vpn-extension/WhenUseVPNSection.tsx
@@ -1,5 +1,6 @@
 import { CheckSquare } from '@phosphor-icons/react';
 import Button from '../shared/Button';
+import Image from 'next/image';
 
 export const WhenUseVPNSection = ({ textContent }) => {
   return (
@@ -16,10 +17,20 @@ export const WhenUseVPNSection = ({ textContent }) => {
             // NO OP RN
           }}
         />
-        <div className="grid w-full max-w-5xl grid-cols-1 flex-row justify-between gap-5 lg:grid-cols-2">
+        <div className="relative grid w-full max-w-6xl grid-cols-1 flex-row justify-between gap-5 bg-[url(/images/vpn-extension/map.svg)] bg-contain bg-center bg-no-repeat lg:grid-cols-2">
+          <div className="absolute inset-0 ml-5 flex items-center justify-center">
+            <Image
+              src="/images/vpn-extension/superhero2.svg"
+              alt="Super Hero"
+              width={444}
+              height={420}
+              className="mx-auto"
+            />
+          </div>
+          <div className="absolute"></div>
           {textContent.cards.map((card) => (
             <div className="flex even:justify-end">
-              <div className="flex w-full max-w-[300px] flex-col space-y-2 rounded-[10px] border border-gray-10 p-10 text-start">
+              <div className="flex w-full max-w-[300px] flex-col space-y-2 rounded-[10px] border border-gray-10 bg-white p-10 text-start">
                 <CheckSquare size={32} className="text-green" />
                 <p className="max-w-[260px] text-2xl font-medium text-gray-80">{card}</p>
               </div>
