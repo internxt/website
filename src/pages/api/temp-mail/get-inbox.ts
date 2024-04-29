840d315234bef1f24ff3b6f049f5ecafe250c8e8
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-29 13:13:20 +0200

    fix: remove console.log

--- src/pages/api/temp-mail/get-inbox.ts
+++ src/pages/api/temp-mail/get-inbox.ts
@@ -10,8 +10,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       const inbox = await axios.get(`${CONVERTER_URL}/api/temp-mail/messages/${token}`);
 
-      console.log(inbox.data);
-
       return res.status(200).json(inbox.data);
     } catch (err) {
       const error = err as Error;
