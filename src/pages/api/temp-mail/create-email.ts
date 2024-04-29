fe81b26be42894a22d82f09f9a85bc8fbff051ad
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-29 10:51:05 +0200

    re-deploy in vercel

--- src/pages/api/temp-mail/create-email.ts
+++ src/pages/api/temp-mail/create-email.ts
@@ -11,7 +11,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           Authorization: `Bearer ${TEMP_MAIL_API_KEY}`,
         },
       });
-
       return res.status(200).json(email.data);
     } catch (err) {
       const error = err as Error;
