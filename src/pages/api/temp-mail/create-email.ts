2a41ecbda850b5c80a474755260128a8b32672b0
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-29 10:48:04 +0200

    fix: add auth for temp mail endpoints

--- src/pages/api/temp-mail/create-email.ts
+++ src/pages/api/temp-mail/create-email.ts
@@ -7,9 +7,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'GET') {
     try {
       const email = await axios.get(`${process.env.NEXT_PUBLIC_TEMP_MAIL_URL}/inbox/create`, {
-        // headers: {
-        //   Authorization: `Bearer ${TEMP_MAIL_API_KEY}`,
-        // },
+        headers: {
+          Authorization: `Bearer ${TEMP_MAIL_API_KEY}`,
+        },
       });
 
       return res.status(200).json(email.data);
--- src/pages/api/temp-mail/get-inbox.ts
+++ src/pages/api/temp-mail/get-inbox.ts
@@ -8,9 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { token } = req.query;
     try {
       const inbox = await axios.get(`${process.env.NEXT_PUBLIC_TEMP_MAIL_URL}/inbox?token=${token}`, {
-        // headers: {
-        //   Authorization: `Bearer ${TEMP_MAIL_API_KEY}`,
-        // },
+        headers: {
+          Authorization: `Bearer ${TEMP_MAIL_API_KEY}`,
+        },
       });
 
       return res.status(200).json(inbox.data);
