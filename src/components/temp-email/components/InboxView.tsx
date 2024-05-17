3f9df7aa2a7c4ef424478913402ad509858aa839
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-05-17 10:44:30 +0200

    fix: sidenav for temp mail

--- src/components/temp-email/components/InboxView.tsx
+++ src/components/temp-email/components/InboxView.tsx
@@ -26,8 +26,8 @@ export const Inbox = ({
 }: InboxProps) => {
   return (
     <div className="relative px-52">
-      <div id="sidebar_right" className="left-0 hidden lg:absolute"></div>
-      <div id="sidebar_left" className="right-0 hidden lg:absolute"></div>
+      <div id="sidebar_right" className="left-0 hidden lg:absolute lg:flex"></div>
+      <div id="sidebar_left" className="right-0 hidden lg:absolute lg:flex"></div>
 
       <div className="hidden w-full justify-center md:flex">
         <InboxWeb
