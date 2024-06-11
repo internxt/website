c4005c5fc097bc362674fd795b95d47ad21d31a7
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-06-11 16:34:29 +0200

    feat: Euro 2024 campaign

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,6 +13,7 @@ import {
   INCLUDED_PATHS_FOR_SNIGEL,
   INTERNXT_URL,
   PATHS_WITH_CUSTOM_SNIGEL_BANNERS,
+  EXCLUDED_PATHS_FOR_BANNER,
 } from '@/constants';
 
 const IMPACT_API = process.env.NEXT_PUBLIC_IMPACT_API as string;
@@ -55,8 +56,7 @@ LayoutProps) {
   const router = useRouter();
   const pathname = router.pathname === '/' ? '' : router.pathname;
   const lang = router.locale;
-  const shouldShowBanner = false;
-  // !EXCLUDED_PATHS_FOR_BANNER.includes(pathname);
+  const shouldShowBanner = !EXCLUDED_PATHS_FOR_BANNER.includes(pathname);
 
   const snigelBanners = PATHS_WITH_CUSTOM_SNIGEL_BANNERS.includes(pathname)
     ? [...SNIGEL_BANNERS.DEFAULT_BANNERS, ...SNIGEL_BANNERS.CUSTOM_BANNERS]
