235feb95912361fab75a05871ddb0019388fd173
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-06-12 09:39:45 +0200

    fix: pricing banner for mobile view

--- src/components/lifetime/PriceBannerForCampaigns.tsx
+++ src/components/lifetime/PriceBannerForCampaigns.tsx
@@ -1,5 +1,6 @@
 import { GlobalDialog, useGlobalDialog } from '@/contexts/GlobalUIManager';
 import { CheckCircle } from '@phosphor-icons/react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const PriceBannerForCampaigns = ({ textContent }) => {
@@ -9,7 +10,7 @@ export const PriceBannerForCampaigns = ({ textContent }) => {
   return (
     <div className={`${shouldShowBanner ? 'flex' : 'hidden'} flex-col overflow-hidden px-5`}>
       <div className="flex w-full  flex-col justify-between rounded-[32px] bg-white bg-[url('/images/campaigns/euro/grass.webp')] lg:flex-row">
-        <div className="flex w-full flex-col gap-6 p-10 lg:py-16 xl:w-full">
+        <div className="flex w-full flex-col items-center gap-6 p-10 text-center lg:items-start lg:py-16 lg:text-left xl:w-full">
           <div className="flex w-max rounded-2xl bg-gray-100 py-2 px-4 ring-4 ring-primary">
             <p className="text-5xl font-bold text-white">{textContent.label}</p>
           </div>
@@ -30,12 +31,21 @@ export const PriceBannerForCampaigns = ({ textContent }) => {
           </div>
           <p className="text-sm font-medium text-gray-50">{textContent.lastCta}</p>
         </div>
-        <div className="w-full max-w-xl flex-row justify-end  xl:flex">
+        <div className="hidden w-full max-w-xl flex-row justify-end  lg:flex">
           <img
             src="/images/campaigns/euro/pricing.webp"
             draggable={false}
             alt="Internxt Cloud Storage Pricing"
             className="h-full w-full rounded-r-[32px] object-cover"
+          />
+        </div>
+        <div className="flex h-full w-full flex-col object-cover lg:hidden">
+          <Image
+            src="/images/campaigns/euro/mobile.webp"
+            width={377}
+            height={190}
+            alt="Spring sale image"
+            className="w-full rounded-b-[32px] object-cover"
           />
         </div>
       </div>
