253a73476ca70693e4f9724797b29698b516effa
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-07-05 15:43:59 +0200

    fix: show before close tab banner

--- src/components/banners/BeforeCloseTabBanner.tsx
+++ src/components/banners/BeforeCloseTabBanner.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
 import { GlobalDialog, useGlobalDialog } from '@/contexts/GlobalUIManager';
 import { getImage } from '@/lib/getImage';
 import { event } from '@/lib/gtag';
 import { CheckCircle, X } from '@phosphor-icons/react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
 
 const CLOSE_TAB_BANNER_NAME = 'close_tab_banner_date';
 const ONE_MONTH_IN_MS = 30 * 24 * 60 * 60 * 1000;
@@ -18,31 +20,31 @@ export const BeforeCloseTabBanner = () => {
 
   const textContent = require(`../../assets/lang/${lang}/banners.json`);
 
-  // useEffect(() => {
-  //   const handleMouseLeave = (event) => {
-  //     const closeTabBannerInLocalStorage = localStorage.getItem(CLOSE_TAB_BANNER_NAME);
-  //     const todayDate = new Date().getTime();
+  useEffect(() => {
+    const handleMouseLeave = (event) => {
+      const closeTabBannerInLocalStorage = localStorage.getItem(CLOSE_TAB_BANNER_NAME);
+      const todayDate = new Date().getTime();
 
-  //     if (closeTabBannerInLocalStorage) {
-  //       const lastCloseDate = new Date(closeTabBannerInLocalStorage).getTime();
+      if (closeTabBannerInLocalStorage) {
+        const lastCloseDate = new Date(closeTabBannerInLocalStorage).getTime();
 
-  //       if (todayDate - lastCloseDate > ONE_MONTH_IN_MS) {
-  //         localStorage.removeItem(CLOSE_TAB_BANNER_NAME);
-  //         openDialog(GlobalDialog.BeforeYouGoBanner);
-  //       } else {
-  //         closeDialog(GlobalDialog.BeforeYouGoBanner);
-  //       }
-  //     } else if (event.clientY <= 0) {
-  //       openDialog(GlobalDialog.BeforeYouGoBanner);
-  //     }
-  //   };
+        if (todayDate - lastCloseDate > ONE_MONTH_IN_MS) {
+          localStorage.removeItem(CLOSE_TAB_BANNER_NAME);
+          openDialog(GlobalDialog.BeforeYouGoBanner);
+        } else {
+          closeDialog(GlobalDialog.BeforeYouGoBanner);
+        }
+      } else if (event.clientY <= 0) {
+        openDialog(GlobalDialog.BeforeYouGoBanner);
+      }
+    };
 
-  //   document.addEventListener('mouseleave', handleMouseLeave);
+    document.addEventListener('mouseleave', handleMouseLeave);
 
-  //   return () => {
-  //     document.removeEventListener('mouseleave', handleMouseLeave);
-  //   };
-  // }, []);
+    return () => {
+      document.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, []);
 
   const handleCloseBanner = () => {
     event({
@@ -56,7 +58,7 @@ export const BeforeCloseTabBanner = () => {
   };
 
   const handleOnClick = () => {
-    router.push('/pricing');
+    router.push('/specialoffer/freeuser');
     handleCloseBanner();
   };
 
