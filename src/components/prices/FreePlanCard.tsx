3ac0040424dcaa8bf6e9c6029fa54b3cd41667a0
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-07-26 10:13:15 +0200

    fix: space between paid cards and free card

--- src/components/prices/FreePlanCard.tsx
+++ src/components/prices/FreePlanCard.tsx
@@ -5,7 +5,12 @@ const FreePlanCard = ({ textContent }) => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-5 rounded-2xl border border-gray-10 px-5 py-9 md:flex-row md:justify-between">
       <div className="flex flex-col space-y-5 text-center md:text-left">
-        <p className="text-2xl font-bold text-gray-100">{textContent.getStarted}</p>
+        <div className="flex flex-row items-center gap-2">
+          <div className="flex w-max flex-col items-center space-y-2 rounded-full bg-primary/7 px-5 py-2 text-center">
+            <p className="text-xl font-medium text-primary">{textContent.freeForever}</p>
+          </div>
+          <p className="text-2xl font-bold text-gray-100">{textContent.getStarted}</p>
+        </div>
         <div className="flex flex-row space-x-2 md:items-center">
           <img
             loading="lazy"
@@ -14,12 +19,10 @@ const FreePlanCard = ({ textContent }) => {
             draggable="false"
             alt="check icon"
           />
-          <p className="text-sm text-gray-80">{textContent.enjoy10gb}</p>
+          <p className="text-gray-80">{textContent.enjoy10gb}</p>
         </div>
       </div>
-      <div className="flex flex-col items-center space-y-2 text-center ">
-        <p className="text-4xl font-bold text-gray-100">{textContent.freeForever}</p>
-      </div>
+
       <div className="flex">
         <button
           className="flex w-full flex-col rounded-lg border border-primary px-20 py-2.5 font-medium text-primary hover:bg-gray-1 active:bg-gray-5"
