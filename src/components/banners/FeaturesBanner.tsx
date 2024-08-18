db9bdfa7787b2585cf348c2bd6be3346ac553995
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-08-18 17:56:54 +0200

    feat: update top banner text and remove bottom banner

--- src/components/banners/FeaturesBanner.tsx
+++ src/components/banners/FeaturesBanner.tsx
@@ -2,12 +2,6 @@ import { CheckCircle, TreePalm, X } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-const KeyHoleIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="rgb(0,102,255)" viewBox="0 0 256 256">
-    <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm29.52,146.39a4,4,0,0,1-3.66,5.61H102.14a4,4,0,0,1-3.66-5.61L112,139.72a32,32,0,1,1,32,0Z"></path>
-  </svg>
-);
-
 const HIDE_BANNER_DATE = new Date('2024-08-19');
 const TODAY_DATE = new Date();
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,6 @@ import BottomBanner from '@/components/banners/BottomBanner';
 import { EXCLUDED_PATHS_FOR_BANNER } from '@/constants';
 import { FreeCardPromoBanner } from '@/components/banners/FreeCardPromoBanner';
 import { BeforeCloseTabBanner } from '@/components/banners/BeforeCloseTabBanner';
-import FeaturesBanner from '@/components/banners/FeaturesBanner';
 
 const EXCLUDE_INTERCOM_PATHS = [
   '/temporary-email',
@@ -78,7 +77,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           },
           {
             key: GlobalDialog.BottomBanner,
-            isOpen: true,
+            isOpen: false,
           },
           {
             key: GlobalDialog.FreeSpaceCardBanner,
@@ -121,8 +120,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           {shouldShowBanner ? <BottomBanner /> : undefined}
           {shouldShowBeforeYouGoBanner ? <BeforeCloseTabBanner /> : undefined}
         </div>
-        {shouldShowFeaturesBanner ? <FeaturesBanner /> : undefined}
+        {/* {shouldShowFeaturesBanner ? <FeaturesBanner /> : undefined} */}
         <FreeCardPromoBanner />
+
         {/* Show snackbar in all pages */}
         <ShowSnackbar />
       </GlobalUIManager>
