824c59e98064d1154f38635119a95f2e70901558
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-08-28 11:41:33 +0200

    fix: add cloud-object-storaje to excluded paths

--- src/constants.ts
+++ src/constants.ts
@@ -32,6 +32,7 @@ export const EXCLUDED_PATHS_FOR_BANNER = [
   '/affiliates/[filename]',
   '/cloudwards',
   '/lifetime/security82',
+  '/cloud-object-storage',
   '/cloud-object-storage/checkout',
   '/aeebcee77ac34a3d81bd10133fd53cc7869978f3',
 ];
