f8028644eacc9254ad3bbce6ee4283869fb74c52
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-09-02 08:31:33 +0200

    fix: top banner text (Spanish)

--- src/components/banners/TopBanner.tsx
+++ src/components/banners/TopBanner.tsx
@@ -23,8 +23,7 @@ const TopBanner = ({ isBannerFixed }: TopBannerProps) => {
         <div className="mx-auto flex flex-row items-center justify-center space-x-3">
           <div className="flex cursor-default">
             <p className="font-normal">
-              💼 {textContent.title.normalText}
-              <span className="font-bold">{textContent.title.boldText}</span>
+              💼 {textContent.title.normalText} <span className="font-bold">{textContent.title.boldText}</span>
             </p>
           </div>
           <Link
