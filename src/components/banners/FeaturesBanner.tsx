811f614f849cb0f120e8a9c7648f454287d972d6
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-10-07 17:46:01 +0200

    fix: width for mobile

--- src/components/banners/FeaturesBanner.tsx
+++ src/components/banners/FeaturesBanner.tsx
@@ -55,7 +55,7 @@ const FeaturesBanner = () => {
         >
           <X size={32} />
         </button>
-        <div className="flex w-max max-w-[900px] flex-col space-x-10 py-14 lg:flex-row">
+        <div className="flex max-w-[900px] flex-col space-x-10 py-14 lg:flex-row">
           <div className="flex w-full flex-col  items-center justify-center space-y-3 text-center lg:items-start lg:justify-between lg:text-start">
             <div className="flex rounded-lg border-4 border-primary/7 px-3 py-1.5">
               <p className="text-2xl font-bold text-primary">{textContent.featuresBanner.label}</p>
