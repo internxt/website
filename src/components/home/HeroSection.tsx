eda7682c75866b0396ccc5ae983174f72c177f28
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-10-08 09:11:25 +0200

    fix: cyber awareness styles

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -35,7 +35,7 @@ export default function HeroSection({ textContent, lang, isHomePageV2 }: HeroSec
           style={{ backgroundImage: `url('${blurBgImage}')`, filter: 'blur(24px)' }}
         />
 
-        <div className="relative mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between lg:flex-row lg:items-stretch lg:py-10">
+        <div className="relative mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between lg:flex-row lg:items-center lg:py-10">
           <div className="absolute inset-y-0 left-1/2 z-0 hidden w-screen -translate-x-1/2 bg-cover bg-center bg-no-repeat md:flex" />
           <div
             className={`flex w-screen flex-shrink-0 ${componentsFlow} items-center justify-center gap-5 px-5 pt-5 text-center sm:w-auto sm:px-0 md:ml-2 lg:ml-0 lg:items-start lg:text-left`}
@@ -103,7 +103,7 @@ export default function HeroSection({ textContent, lang, isHomePageV2 }: HeroSec
           {/* <div className=" hidden h-screen max-h-[600px] w-full justify-center lg:flex">
             <Animation />
           </div> */}
-          <div className="hidden lg:flex">
+          <div className="hidden h-max w-max lg:flex">
             <Image
               src={getImage('/images/cyber-awareness/cyber-awareness-2024/internxt_secure_cloud_storage.webp')}
               alt="Internxt Secure Cloud Storage"
