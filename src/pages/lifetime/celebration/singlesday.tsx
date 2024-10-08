d1de0308eef562d4007d82e77a81cd91c2039606
jaaaaavier <javipiagui13@gmail.com>
2024-10-08 18:21:36 +0200

    fix: singlesdayLP

--- src/pages/lifetime/celebration/singlesday.tsx
+++ src/pages/lifetime/celebration/singlesday.tsx
@@ -5,10 +5,14 @@ import Navbar from '@/components/layout/navbars/Navbar';
 import { MinimalFooter } from '@/components/layout/footers/MinimalFooter';
 import FAQSection from '@/components/shared/sections/FaqSection';
 import CtaSection from '@/components/shared/CtaSection';
-import PaymentSection from '@/components/lifetime/celebration/singles-day/PaymentSection';
 import FeatureSection from '@/components/lifetime/celebration/singles-day/FeatureSection';
-import HeroSection from '@/components/lifetime/celebration/singles-day/HeroSection';
+import PaymentSection from '@/components/lifetime/PaymentSection';
+import {HeroSection} from '@/components/shared/components/HeroSection';
+import Button from '@/components/shared/Button';
+import { getImage } from '@/lib/getImage';
 import { PromoCodeName } from '@/lib/types';
+
+
 
 const SinglesdayCelebration = ({
   lang,
@@ -25,7 +29,6 @@ const percent='80%'
 const currencySpecified='US';
 const locale = lang as string;
 const couponCode=PromoCodeName.SinglesDay;
-
   return (
     <Layout
       title={metatags[0].title}
@@ -35,41 +38,55 @@ const couponCode=PromoCodeName.SinglesDay;
       specialOffer={`https://internxt.com/images/previewLink/LifetimePreviewLink.png`}
     >
     <Navbar textContent={navbarLang} lang={locale}  cta={['default']} fixed mode="payment" isLinksHidden hideNavbar />
-    <div className="mt-16">
-      <HeroSection textContent={langJson.HeroSection} percent={percent}/>
-      <PaymentSection
-        textContent={langJson.PaymentSection}
+     <div className="mt-16">
+      <HeroSection 
+        TextComponent={
+          <>
+          <>
+            <p className='text-6xl font-bold text-primary'>{langJson.HeroSection.title}</p>
+            <p className='text-xl font-regular text-gay-100'>{langJson.HeroSection.description}</p>
+          </>
+              <Button text={langJson.HeroSection.cta}></Button></>
+        }
+        imageProperties={{
+            src: getImage('/images/lifetime/Group_1194.png'),
+            alt: 'Oferta especial Black Friday',
+            width: 562,
+            height: 529,
+          }}
+        background="bg-gray-1" 
+      />
+
+      <PaymentSection 
+       textContent={langJson.PaymentSection}
         discount={discount}
         showPriceBefore
         lang={lang}
         percent={percent}
         currencySpecified={currencySpecified}
-        lifetimeMode="celebration"
+         lifetimeMode="celebration"
         couponCode={couponCode}
-      />
+        />
       <FeatureSection textContent={langJson.FeatureSection} backgroundColor='bg-white' />
-      <CtaSection textContent={textContent.CtaSection1} url={''}/>
-      <TestimonialsSection textContent={textContent.TestimonialsSection}/>
-      <FAQSection textContent={textContent.FaqSection} bgColor='bg-gray-1'/>
-      <CtaSection textContent={textContent.CtaSection2} url={''}/>
-    </div>
-    <MinimalFooter footerLang={footerLang.FooterSection} lang={lang} />
+      <CtaSection textContent={langJson.CtaSection1} url={'/pricing'}/>
+      <TestimonialsSection textContent={langJson.TestimonialsSection}/>
+      <FAQSection textContent={langJson.FaqSection} bgColor='bg-gray-1'/>
+      <CtaSection textContent={langJson.CtaSection2} url={'/pricing'}/>
+     </div>
+       <MinimalFooter footerLang={footerLang.FooterSection} lang={lang} />
     </Layout>
-  );
+     );
 };
-
 export async function getServerSideProps(ctx) {
   let lang = 'zh';
+  cookies.setReferralCookie(ctx);
 
   const metatagsDescriptions = require(`@/assets/lang/${lang}/metatags-descriptions.json`);
   const langJson = require(`@/assets/lang/${lang}/singles-day.json`);
   const testimonialsJson = require(`@/assets/lang/${lang}/home.json`);
   const navbarLang = require(`@/assets/lang/${lang}/navbar.json`);
   const footerLang = require(`@/assets/lang/${lang}/footer.json`);
-  const textContent = require(`@/assets/lang/${lang}/singles-day.json`);
-
-  cookies.setReferralCookie(ctx);
-
+  
   return {
     props: {
       lang,
@@ -77,9 +94,8 @@ export async function getServerSideProps(ctx) {
       langJson,
       testimonialsJson,
       navbarLang,
-      footerLang,
-      textContent
-    },
+      footerLang
+       },
   };
 }
 
