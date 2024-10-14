d2f0d06bda7b3268c723cc75a37b53710c80bb1b
jaaaaavier <javipiagui13@gmail.com>
2024-10-14 13:26:59 +0200

    Update black-friday.tsx
    Changing blackfriday page for the new design implementing PriceSection and others

--- src/pages/black-friday.tsx
+++ src/pages/black-friday.tsx
@@ -1,25 +1,58 @@
+import { useState } from 'react';
 import cookies from '@/lib/cookies';
 import Layout from '@/components/layout/Layout';
 import Navbar from '@/components/layout/navbars/Navbar';
-import HeroSection from '@/components/black-friday/HeroSection';
-
-import SuiteSection from '@/components/black-friday/SuiteSection';
-import CtaSection from '@/components/black-friday/CtaSection';
 import FeatureSection from '@/components/black-friday/FeatureSection';
 import PlatformSection from '@/components/black-friday/PlatformSection';
 import TestimonialsSection from '@/components/black-friday/TestimonialsSection';
 import FaqSection from '@/components/black-friday/FAQSection';
-import BestStorageSection from '@/components/black-friday/BestStorageSection';
-import LoginBFBanner from '@/components/banners/LoginBFBanner';
-import FooterSection from '@/components/black-friday/FooterSection';
+import { MinimalFooter } from '@/components/layout/footers/MinimalFooter';
+import { PricingSectionWrapper } from '@/components/shared/pricing/PricingSectionWrapper';
+import usePricing from '@/hooks/usePricing';
+import { PromoCodeName } from '@/lib/types';
+import { Interval, stripeService } from '@/components/services/stripe.service';
+import HeroSection from '@/components/black-friday/HeroSection';
+
 
 const BLACK_FRIDAY_METATAG_ID = 'black-friday';
 
-const BlackFriday = ({ lang, metatagsDescriptions, langJson, navbarLang }) => {
+const BlackFriday = ({ lang, metatagsDescriptions, langJson, navbarLang, footerLang }) => {
   const metatags = metatagsDescriptions.filter((desc) => desc.id === BLACK_FRIDAY_METATAG_ID);
 
+  const {
+    products,
+    loadingCards,
+    currencyValue,
+    coupon: individualCoupon,
+    businessCoupon,
+    lifetimeCoupons,
+  } = usePricing({
+    couponCode: PromoCodeName.BlackFriday,
+    couponCodeForBusiness: PromoCodeName.BlackFriday,
+  });
+
+  const [isBusiness, setIsBusiness] = useState<boolean>();
+
+  const onBusinessPlansSelected = (isBusiness: boolean) => {
+    setIsBusiness(isBusiness);
+  };
+
+  const onCheckoutButtonClicked = (priceId: string, isCheckoutForLifetime: boolean) => {
+    const lifetimeSpacePlan = products?.individuals[Interval.Lifetime].find((product) => product.priceId === priceId);
+
+    const couponCodeForB2CPlans =
+      lifetimeSpacePlan && lifetimeCoupons
+        ? (lifetimeCoupons?.[lifetimeSpacePlan.storage] as any).promoCodeName
+        : individualCoupon?.name;
+
+    const couponCodeForCheckout = isBusiness ? businessCoupon?.name : couponCodeForB2CPlans;
+    const planType = isBusiness ? 'business' : 'individual';
+
+    stripeService.redirectToCheckout(priceId, currencyValue, planType, isCheckoutForLifetime, couponCodeForCheckout);
+  };
+
   return (
-    <Layout title={metatags[0].title} description={metatags[0].description} segmentName="Black Friday">
+    <Layout title={metatags[0].title} description={metatags[0].description} segmentName="Black Friday" >
       <Navbar
         lang={lang}
         darkMode={true}
@@ -27,28 +60,34 @@ const BlackFriday = ({ lang, metatagsDescriptions, langJson, navbarLang }) => {
         textContent={navbarLang}
         cta={['Hide Login']}
         isLinksHidden
+        
       />
-      <LoginBFBanner />
+      <HeroSection textContent={langJson.blackFriday} lang={lang} />
 
-      <HeroSection lang={lang} textContent={langJson.blackFriday} />
-
-      <BestStorageSection textContent={langJson.blackFriday} lang={lang} />
-
-      <SuiteSection lang={lang} textContent={langJson.blackFriday} />
-
-      <CtaSection textContent={langJson.cta} lang={lang} />
+      <PricingSectionWrapper
+        textContent={langJson.tableSection}
+        decimalDiscount={{
+          individuals: individualCoupon?.percentOff && 100 - individualCoupon?.percentOff,
+          business: businessCoupon?.percentOff && 100 - businessCoupon?.percentOff,
+          lifetime: individualCoupon?.percentOff && 100 - individualCoupon.percentOff,
+        }}
+        lifetimeCoupons={lifetimeCoupons}
+        lang={lang}
+        products={products}
+        loadingCards={loadingCards}
+        onBusinessPlansSelected={onBusinessPlansSelected}
+        onCheckoutButtonClicked={onCheckoutButtonClicked}
+        hideFreeCard
+      />
 
       <FeatureSection textContent={langJson.blackFriday} />
 
       <PlatformSection textContent={langJson.blackFriday} />
 
       <TestimonialsSection textContent={langJson.blackFriday} lang={lang} />
-
-      <CtaSection textContent={langJson.cta2} lang={lang} />
-
       <FaqSection textContent={langJson.blackFriday} />
 
-      <FooterSection textContent={langJson.blackFriday} lang={lang} />
+      <MinimalFooter lang={lang} footerLang={footerLang.FooterSection} />
     </Layout>
   );
 };
@@ -66,7 +105,7 @@ export async function getServerSideProps(ctx) {
   const footerLang = require(`@/assets/lang/${lang}/footer.json`);
 
   cookies.setReferralCookie(ctx);
-
+  
   return {
     props: {
       lang,
