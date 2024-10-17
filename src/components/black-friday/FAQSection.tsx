2e2644e8db035b3318f9d825fed6a2362dbca67c
jaaaaavier <javipiagui13@gmail.com>
2024-10-17 11:10:43 +0200

    Updating Blackfriday Components
    Changing colors

--- src/components/black-friday/FAQSection.tsx
+++ src/components/black-friday/FAQSection.tsx
@@ -4,13 +4,13 @@ import FaqAccordion from '@/components/shared/FAQAccordion';
 const FaqSection = ({ textContent }) => {
   return (
     <section className="overflow-hidden">
-      <div className="flex flex-col items-center bg-gray-1 py-20 lg:px-44">
+      <div className="flex flex-col items-center bg-black py-20 lg:px-44">
         <div className="center flex flex-col items-center text-center">
-          <p className="text-4xl font-semibold">{textContent.faq.title}</p>
+          <p className="text-4xl font-semibold text-white ">{textContent.faq.title}</p>
         </div>
         <div className="flex w-full max-w-screen-sm flex-col space-y-2 pt-10">
           {textContent.faq.faq.map((item, index) => (
-            <div className="rounded-lg border border-gray-20 px-5" key={index}>
+            <div className="rounded-lg border border-gray-90 px-5 text-gray-5" key={index}>
               <FaqAccordion key={item.question} question={item.question} answer={item.answer} />
             </div>
           ))}
--- src/components/black-friday/FeatureSection.tsx
+++ src/components/black-friday/FeatureSection.tsx
@@ -26,22 +26,22 @@ const FeatureSection = ({ textContent }) => {
   ];
 
   return (
-    <section className="overflow-hidden">
-      <div className="content flex flex-1 flex-col items-center bg-white px-10 py-20">
+    <section className="overflow-hidden bg-highlight">
+      <div className="content flex flex-1 flex-col items-center  px-10 py-20">
         <div className="center flex flex-col items-center pb-16 text-center lg:max-w-[778px]">
-          <p className="text-4xl font-semibold">{textContent.FeatureSection.title}</p>
-          <p className="mt-4 text-center text-xl font-normal">{textContent.FeatureSection.subtitle}</p>
+          <p className="text-4xl font-semibold text-white">{textContent.FeatureSection.title}</p>
+          <p className="mt-4 text-center text-xl font-normal text-gray-5">{textContent.FeatureSection.subtitle}</p>
         </div>
         <div className="grid grid-cols-1 flex-row flex-wrap justify-center gap-8 sm:grid-cols-2">
           {cards.map((card) => (
             <div
               key={card.title}
-              className={`flex flex-col items-start justify-start rounded-2xl bg-gray-1 p-8 sm:p-10 md:max-w-[488px]`}
+              className={`flex flex-col items-start justify-start rounded-2xl bg-gray-1 p-8 sm:p-10 md:max-w-[488px] bg-gray-100`}
             >
               <card.icon className="mb-6 text-4xl text-primary" size={32} />
               <div className="flex w-full max-w-[400px] flex-col">
-                <p className="mb-6 text-2xl font-medium text-gray-100">{card.title}</p>
-                <p className="text-base text-cool-gray-80 sm:text-lg">{card.subtitle}</p>
+                <p className="mb-6 text-2xl font-medium text-white">{card.title}</p>
+                <p className="text-base text-white sm:text-lg">{card.subtitle}</p>
               </div>
             </div>
           ))}
