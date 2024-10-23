ba5ac022e2002bd90a1d7e27d250eb04ac31114e
jaaaaavier <javipiagui13@gmail.com>
2024-10-23 17:43:50 +0200

    Update HomePageBannerForMobile.tsx

--- src/components/banners/HomePageBannerForMobile.tsx
+++ src/components/banners/HomePageBannerForMobile.tsx
@@ -10,14 +10,16 @@ export const HomePageBannerForMobile = (redirectTo) => {
   const router = useRouter();
   const lang = router.locale;
   const textContent = require(`../../assets/lang/${lang}/pricing.json`);
-
+const bgImage= getImage('/images/campaigns/halloween/web.svg');
   return (
-    <div
-      className={`${
-        shouldShowBanner ? 'flex' : 'hidden'
-      } w-full max-w-[400px] flex-col overflow-hidden rounded-[32px] bg-orange pt-10 lg:hidden`}
-    >
-      <div className="flex w-full flex-col items-center gap-4 px-2 py-6 text-center">
+    <div className="flex lg:hidden flex-col justify-between gap-10 rounded-[16px] bg-orange w-full relative z-10"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="flex w-full flex-col items-center gap-4 px-2 py-6 text-center">
           <div className="flex w-max rounded-xl border-4 border-yellow bg-gray-100 px-6 py-3">
             <p className="text-4xl sm:text-5xl font-bold text-white">{textContent.tableSection.ctaBanner.label}</p>
           </div>
@@ -35,7 +37,7 @@ export const HomePageBannerForMobile = (redirectTo) => {
               href={redirectTo ?? '#billingButtons'}
               className="flex w-max items-center rounded-lg bg-white px-3 py-2 text-base font-medium text-gray-100 hover:bg-gray-5"
             >
-              {textContent.tableSection.ctaBanner.cta} 
+              {textContent.tableSection.ctaBanner.ctaMbl} 
             </Link>
           </div>
         </div>
@@ -49,6 +51,6 @@ export const HomePageBannerForMobile = (redirectTo) => {
             className="w-full rounded-b-[16px] object-cover"
           />
         </div>
-    </div>
+      </div>
   );
 };
