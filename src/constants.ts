412ccda4ca8c6504eb8f2c792b45f1fa03136001
jaaaaavier <javipiagui13@gmail.com>
2024-10-29 13:12:15 +0100

    Banners Update

--- src/constants.ts
+++ src/constants.ts
@@ -35,8 +35,7 @@ export const EXCLUDED_PATHS_FOR_BANNER = [
   '/cloud-object-storage',
   '/cloud-object-storage/checkout',
   '/affiliates',
-  '/comeback',
-  '/black-friday',
+  '/comeback'
 ];
 
 export const SNIGEL_BANNERS = {
