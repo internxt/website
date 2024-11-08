b9a23cc5d4ae266d50fb293b7a3d7a7067e77e91
jaaaaavier <javipiagui13@gmail.com>
2024-11-08 15:19:19 +0100

    Update HomePageBannerForMobile.tsx

--- src/components/banners/HomePageBannerForMobile.tsx
+++ src/components/banners/HomePageBannerForMobile.tsx
@@ -36,7 +36,7 @@ export const HomePageBannerForMobile = (redirectTo) => {
             <p className="whitespace-nowrap text-sm font-medium">{textContent.tableSection.ctaBanner.guarantee}</p>
           </div>
           <Link
-            href={redirectTo ?? '#billingButtons'}
+            href={'#priceTable'}
             className="flex w-max items-center rounded-lg bg-white px-3 py-2 text-base font-medium text-gray-100 hover:bg-gray-5"
           >
             {textContent.tableSection.ctaBanner.cta}
