c6f6f41141c949746db00d27828973f9c3d4811a
jaaaaavier <javipiagui13@gmail.com>
2024-11-18 12:28:07 +0100

    Cahnging alternative text for image in pwnedElement

--- src/components/monitor/PwnedElement.tsx
+++ src/components/monitor/PwnedElement.tsx
@@ -10,7 +10,7 @@ const PwnedElementCard: React.FC<PwnedElementCardProps> = ({ textContent }) => {
     <div className="flex max-w-[1019px] flex-col pb-5 md:flex-row">
       <div className="order-1 mb-4 flex items-center justify-center bg-gray-5 p-4 md:order-2 md:mb-0">
         <div className="flex h-20 w-20 items-center justify-center rounded-full bg-white md:h-24 md:w-24">
-          <Image src={getImage(textContent.logoPath)} alt="icon" width={172} height={172} />
+          <Image src={getImage(textContent.logoPath)} alt={`${textContent.domain} logo`} width={172} height={172} />
         </div>
       </div>
       <div className="order-2 max-w-full space-y-5 bg-white px-6 pb-6 pt-6 text-center md:order-1 md:max-w-[1019px] md:px-10 md:pb-8 md:pt-8 md:text-left">
