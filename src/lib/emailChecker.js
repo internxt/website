0b38ca71f04dc7285da405c41f86be5d21421f4d
jaaaaavier <javipiagui13@gmail.com>
2024-11-21 13:32:06 +0100

    Updating metatags

--- src/lib/emailChecker.js
+++ src/lib/emailChecker.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 const API_URL = 'https://haveibeenpwned.com/api/v3/breachedaccount';
-const API_KEY = process.env.HIBP_API_KEY; // Carga la clave API desde las variables de entorno
+const API_KEY = process.env.HIBP_API_KEY;
 
 export default async function checkEmail(email) {
-  if (typeof email !== 'string' || !email.includes('@')) {
-    throw new Error('El correo electrónico no es válido.');
-  }
-
   const url = `${API_URL}/${encodeURIComponent(email)}?truncateResponse=false`;
   const headers = {
     'hibp-api-key': API_KEY,
@@ -16,11 +12,8 @@ export default async function checkEmail(email) {
 
   try {
     const response = await axios.get(url, { headers });
-    return response.data; // Devuelve las brechas encontradas
+    return response.data;
   } catch (err) {
-    if (err.response?.status === 404) {
-      return { message: `No se encontraron brechas para el correo: ${email}` }; // Sin brechas
-    }
-    throw new Error(err.response?.data || 'Error al conectar con HIBP.');
+    throw new Error(err.response?.data);
   }
 }
--- src/pages/api/breaches.js
+++ src/pages/api/breaches.js
@@ -2,20 +2,19 @@ import checkEmail from '@/lib/emailChecker';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Método no permitido.' });
+    return res.status(405);
   }
 
   const { email } = req.query;
 
   if (!email) {
-    return res.status(400).json({ error: 'El email es requerido.' });
+    return res.status(400);
   }
 
   try {
     const result = await checkEmail(email);
     res.status(200).json(result);
   } catch (error) {
-    console.error('Error en la API:', error.message);
-    res.status(500).json({ error: 'Error procesando la solicitud.' });
+    res.status(500);
   }
 }
