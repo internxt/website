648652ed7f57ef577d5d4a78745fd15cab7fbf59
jaaaaavier <javipiagui13@gmail.com>
2024-11-28 11:11:33 +0100

    Updating coupon

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -50,7 +50,7 @@ const Pricing = ({ metatagsDescriptions, navbarLang, footerLang, lang, textConte
     businessCoupon,
     lifetimeCoupons,
   } = usePricing({
-    couponCode: PromoCodeName.freeUserCoupon,
+    couponCode: PromoCodeName.SoftSales,
   });
 
   const [pageName, setPageName] = useState('Pricing Individuals Annually');
