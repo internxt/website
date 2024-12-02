705f5eb0319d74d7ac3047856dd907960cd0396c
jaaaaavier <javipiagui13@gmail.com>
2024-12-02 11:52:24 +0100

    feat/Top Banner New BgColor

--- src/components/banners/TopBanner.tsx
+++ src/components/banners/TopBanner.tsx
@@ -18,7 +18,7 @@ const TopBanner = ({ isBannerFixed }: TopBannerProps) => {
       <div
         className={`group ${
           isBannerFixed ? 'absolute' : 'fixed'
-        } left-0 z-50 hidden h-[54px] w-screen items-center justify-center overflow-hidden bg-primary text-white lg:flex`}
+        } left-0 z-50 hidden h-[54px] w-screen items-center justify-center overflow-hidden bg-red text-white lg:flex`}
       >
         <div className="mx-auto flex flex-row items-center justify-center space-x-3">
           <div className="flex cursor-default">
