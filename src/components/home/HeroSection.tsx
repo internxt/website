802337332d7e7ddc4c6b16515be48ddc9df552e3
jaaaaavier <javipiagui13@gmail.com>
2024-12-05 13:21:46 +0100

    update: back to math.random

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -24,8 +24,7 @@ function getSecureRandom(min, max) {
     return randomValue * (max - min) + min;
   }
 
-  const randomValue = crypto.randomBytes(4).readUInt32BE(0) / (0xffffffff + 1);
-  return randomValue * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 export default function HeroSection({ textContent, lang, isHomePageV2 }: HeroSectionForHomeProps): JSX.Element {
   const componentsFlow = isHomePageV2 ? 'flex-col-reverse' : 'flex-col';
