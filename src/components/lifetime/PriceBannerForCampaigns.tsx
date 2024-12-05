ae1053aaef6e413f000a515de5144e4baa207fb5
jaaaaavier <javipiagui13@gmail.com>
2024-12-05 11:29:57 +0100

    Update PriceBannerForCampaigns.tsx
    update: price banner design

--- src/components/lifetime/PriceBannerForCampaigns.tsx
+++ src/components/lifetime/PriceBannerForCampaigns.tsx
@@ -22,37 +22,39 @@ export const PriceBannerForCampaigns = ({
       <div
         className={`relative z-10 hidden w-full flex-col justify-between rounded-[16px] sm:gap-40 sm:rounded-[32px] lg:flex lg:flex-row ${styles.linearGradient}`}
       >
-        <div className="flex w-full min-w-[800px] flex-col items-center gap-2 px-2 py-6 text-center sm:max-w-[900px] sm:gap-4 sm:px-3 sm:py-10 lg:items-start lg:pl-10 lg:text-left">
-          <div className="flex w-max rounded-xl border-2 border-pink bg-red-dark px-2 py-1 sm:rounded-2xl sm:border-4 sm:px-4 sm:py-2">
-            <p className="text-xl font-bold text-white sm:text-2xl md:text-5xl">{textContent.label}</p>
+        <div className="flex w-full min-w-[800px] flex-col items-center gap-6 px-2 py-6 text-center lg:flex-row lg:items-start lg:pl-10  lg:text-left">
+          <div className="flex  flex-col items-center gap-2 text-center lg:items-start lg:text-left">
+            <div className="flex w-max rounded-xl border-2 border-pink bg-red-dark px-2 py-5 sm:rounded-2xl sm:border-4 sm:px-4 sm:py-2">
+              <p className="text-xl font-bold text-white sm:text-2xl md:text-5xl">{textContent.label}</p>
+            </div>
+            <div className="flex w-full pt-5">
+              <p className="whitespace-nowrap text-2xl font-bold text-white sm:text-3xl md:text-4xl lg:text-5xl">
+                {textContent.subtitle}
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-2 pt-4 sm:gap-4 lg:flex-row">
+              <Link
+                href={redirectTo ?? '#billingButtons'}
+                className="flex w-max items-center rounded-lg bg-white px-3 py-2 text-base font-medium text-gray-100 sm:px-5 sm:py-3 sm:text-lg lg:hover:bg-gray-5"
+              >
+                {textContent.cta}
+              </Link>
+            </div>
+            <div className="flex flex-row items-center space-y-1 text-gray-100 sm:space-x-2">
+              <CheckCircle size={20} className="text-primary" />
+              <p className="whitespace-nowrap text-base font-medium text-gray-30 sm:text-lg">{textContent.guarantee}</p>
+            </div>
+            <p className="whitespace-nowrap text-sm font-medium text-gray-30 sm:text-sm">{textContent.lastCta}</p>
           </div>
-          <div className="flex w-full">
-            <p className="whitespace-nowrap text-2xl font-bold text-white sm:text-3xl md:text-4xl lg:text-5xl">
-              {textContent.subtitle}
-            </p>
+          <div className="min-width-[521px], min-height-[320px] flex items-center justify-center">
+            <Image
+              src={getImage('/images/christmas/internxt_christmas_pricing.webp')}
+              width={521}
+              height={320}
+              alt="Internxt Cloud Storage Pricing"
+              className=""
+            />
           </div>
-          <div className="flex flex-col items-center gap-2 sm:gap-4 lg:flex-row">
-            <Link
-              href={redirectTo ?? '#billingButtons'}
-              className="flex w-max items-center rounded-lg bg-white px-3 py-2 text-base font-medium text-gray-100 sm:px-5 sm:py-3 sm:text-lg lg:hover:bg-gray-5"
-            >
-              {textContent.cta}
-            </Link>
-          </div>
-          <div className="flex flex-row items-center space-x-1 text-gray-100 sm:space-x-2">
-            <CheckCircle size={20} className="text-primary" />
-            <p className="whitespace-nowrap text-base font-medium text-gray-30 sm:text-lg">{textContent.guarantee}</p>
-          </div>
-          <p className="whitespace-nowrap text-sm font-medium text-gray-30 sm:text-sm">{textContent.lastCta}</p>
-        </div>
-        <div className="lg animate-pingpong-v hidden w-full items-center justify-center lg:mr-20 lg:flex">
-          <Image
-            src={getImage('/images/christmas/internxt_christmas_pricing.webp')}
-            width={521}
-            height={382}
-            draggable={false}
-            alt="Internxt Cloud Storage Pricing"
-          />
         </div>
       </div>
 
