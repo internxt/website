b216dc075f5e00665b8d2794cd1bfa2f8d631000
jaaaaavier <javipiagui13@gmail.com>
2024-12-19 11:42:03 +0100

    Update WhatCanWeDo.tsx

--- src/components/business/WhatCanWeDo.tsx
+++ src/components/business/WhatCanWeDo.tsx
@@ -1,7 +1,9 @@
 import { getImage } from '@/lib/getImage';
 import { Transition } from '@headlessui/react';
 import Image from 'next/image';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
+import RevealX from '../components/RevealX';
+import ReactMarkdown from 'react-markdown';
 
 interface WhatCanWeDoProps {
   textContent: any;
@@ -33,47 +35,46 @@ export const WhatCanWeDo = ({ textContent }: WhatCanWeDoProps): JSX.Element => {
           <h2 className="text-5xl font-semibold">{textContent.title}</h2>
           <h3 className="text-xl">{textContent.description}</h3>
         </div>
-
-        <div className="hidden h-full max-h-32 flex-row gap-4 border-b-4 border-b-white xl:flex">
-          {textContent.cards.map((feat, index) => (
-            <button
-              key={feat.selectorTab}
-              className={`flex ${
-                selectedTab === index ? 'border-primary' : 'border-transparent'
-              } w-full max-w-[300px] translate-y-1 flex-col items-center justify-center border-b-4 p-5 text-center`}
-              onClick={() => {
-                onTabSelectorButtonClicked(index);
-              }}
-            >
-              <p className="text-2xl font-medium text-white">{feat.selectorTab}</p>
-            </button>
-          ))}
-        </div>
-        <Transition
-          appear={true}
-          show={!isTransitioning}
-          enter="transition-opacity duration-200"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          className="hidden flex-col gap-20 xl:flex"
-        >
-          <div className="flex w-full max-w-[890px] flex-col">
-            <p className="text-center text-xl text-white">{textContent.cards[selectedTab].description}</p>
-          </div>
-
+        <div className="hidden max-w-[1310px] flex-row items-start py-20 lg:flex">
           <div className="flex flex-col">
-            <Image
-              src={getImage(`/images/business/features/${textContent.cards[selectedTab].imagePathname}.webp`)}
-              alt={textContent.cards[selectedTab].imagePathname}
-              width={896}
-              height={850}
-              draggable={false}
-            />
+            {textContent.cards.map((info, index) => (
+              <Fragment key={info.title}>
+                <button
+                  className={`flex ${
+                    selectedTab === index ? 'border-primary' : 'border-gray-10'
+                  } cursor-pointer flex-row items-center border-r-4 p-2 pr-8`}
+                  onClick={() => {
+                    onTabSelectorButtonClicked(index);
+                  }}
+                >
+                  <p className="text-2xl font-medium text-white  hover:text-primary">{info.selectorTab}</p>
+                </button>
+                <div className="h-8 border-r-4 border-gray-10 pr-8 last:hidden" />
+              </Fragment>
+            ))}
           </div>
-        </Transition>
+          <RevealX direction="left" className="flex flex-col justify-start">
+            <div className="flex w-auto justify-center px-6">
+              <div className="flex flex-col rounded-3xl pl-6">
+                <div className="flex  w-full max-w-[710px] flex-col space-y-6">
+                  <p className="text-4xl font-semibold text-white">{textContent.cards[selectedTab].selectorTab}</p>
+                  <ReactMarkdown className="font-regular text-xl text-white">
+                    {textContent.cards[selectedTab].description}
+                  </ReactMarkdown>
+                  <div className="flex flex-col">
+                    <Image
+                      src={getImage(`/images/business/features/${textContent.cards[selectedTab].imagePathname}.webp`)}
+                      alt={textContent.cards[selectedTab].imagePathname}
+                      width={896}
+                      height={850}
+                      draggable={false}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </RevealX>
+        </div>
 
         {/*Mobile/Tablet View*/}
         <div className="flex w-full snap-x snap-mandatory flex-row justify-start gap-6 overflow-scroll xl:hidden">
