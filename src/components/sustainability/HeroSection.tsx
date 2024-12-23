9384e6d50750a5fe0401ecb75969c16c78229f02
jaaaaavier <javipiagui13@gmail.com>
2024-12-23 11:48:15 +0100

    feat: new components design

--- src/components/sustainability/HeroSection.tsx
+++ src/components/sustainability/HeroSection.tsx
@@ -21,7 +21,7 @@ const FeatureSection = ({ textContent }: HeroSectionsProps) => {
         {/* Main title */}
         <div className="flex flex-col items-center justify-center space-y-10 px-6 text-center">
           <Header>
-            <span className="text-green">{textContent.title.greenText}</span>
+            <span className="text-green-1">{textContent.title.greenText}</span>
             {textContent.title.normalText}
           </Header>
           <h2 className="mb-8 w-full max-w-[850px] text-xl font-normal text-gray-80 sm:mb-10">
@@ -30,20 +30,42 @@ const FeatureSection = ({ textContent }: HeroSectionsProps) => {
         </div>
 
         {/* Display goals */}
-        <div className="relative grid grid-cols-2 justify-items-center pt-6">
-          {goals.map((goal, index) => {
-            const bgColor = index === 1 ? 'bg-green-25' : index === 4 ? 'bg-green-55' : 'bg-green-40';
+        <div className="grid grid-cols-3 pt-6">
+          {goals.slice(0, 3).map((goal, index) => {
+            const bgColor = index === 1 || index === 3 ? 'bg-green-25' : 'bg-green-40';
+
+            const isFirstOrThirdGoal = index === 0 || index === 2;
 
             return (
               <div
                 key={index}
-                className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${bgColor} text-center `}
+                className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${bgColor}  border-4 border-white text-center ${
+                  isFirstOrThirdGoal ? ' mt-8' : 'z-20'
+                }`}
               >
                 <goal.icon size={60} className="mb-2 text-gray-100" />
                 <p className="max-w-[200px] text-2xl font-medium text-gray-100">{goal.title}</p>
               </div>
             );
           })}
+
+          <div className="col-span-3 -mt-[105px] flex justify-center">
+            {goals.slice(3, 5).map((goal, index) => {
+              const bgColor = index === 1 ? 'bg-green-40' : 'bg-green-55';
+
+              return (
+                <div
+                  key={index}
+                  className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${bgColor} border-4 border-white text-center ${
+                    index === 0 ? 'z-10 mr-[-50px]' : 'z-0'
+                  }`}
+                >
+                  <goal.icon size={60} className="mb-2 text-gray-100" />
+                  <p className="max-w-[200px] text-2xl font-medium text-gray-100">{goal.title}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
