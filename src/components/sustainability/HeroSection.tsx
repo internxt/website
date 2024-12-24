98c948984110e4c5dd24981486a759bc65e827f8
jaaaaavier <javipiagui13@gmail.com>
2024-12-24 08:56:51 +0100

    update: header heroSection mobile version

--- src/components/sustainability/HeroSection.tsx
+++ src/components/sustainability/HeroSection.tsx
@@ -8,11 +8,51 @@ export interface HeroSectionsProps {
 
 const FeatureSection = ({ textContent }: HeroSectionsProps) => {
   const goals = [
-    { title: textContent.goals.element1, icon: Sun },
-    { title: textContent.goals.element2, icon: Leaf },
-    { title: textContent.goals.element3, icon: Database },
-    { title: textContent.goals.element4, icon: GlobeHemisphereWest },
-    { title: textContent.goals.element5, icon: Heart },
+    {
+      title: textContent.goals.element1,
+      icon: Sun,
+      bgColor: 'bg-green-40',
+      size: 60,
+      position: 'mt-8',
+      zIndex: 'z-0',
+      lgDisplay: true,
+    },
+    {
+      title: textContent.goals.element2,
+      icon: Leaf,
+      bgColor: 'bg-green-25',
+      size: 60,
+      position: '',
+      zIndex: 'z-20',
+      lgDisplay: true,
+    },
+    {
+      title: textContent.goals.element3,
+      icon: Database,
+      bgColor: 'bg-green-40',
+      size: 60,
+      position: 'mt-8',
+      zIndex: 'z-0',
+      lgDisplay: true,
+    },
+    {
+      title: textContent.goals.element4,
+      icon: GlobeHemisphereWest,
+      bgColor: 'bg-green-55',
+      size: 60,
+      position: ' mr-[-50px]',
+      zIndex: 'z-10',
+      lgDisplay: false,
+    },
+    {
+      title: textContent.goals.element5,
+      icon: Heart,
+      bgColor: 'bg-green-40',
+      size: 60,
+      position: '',
+      zIndex: 'z-5',
+      lgDisplay: false,
+    },
   ];
 
   return (
@@ -31,79 +71,65 @@ const FeatureSection = ({ textContent }: HeroSectionsProps) => {
 
         {/* Display goals */}
         <div className="grid hidden grid-cols-3 pt-6 lg:grid">
-          {goals.slice(0, 3).map((goal, index) => {
-            const bgColor = index === 1 || index === 3 ? 'bg-green-25' : 'bg-green-40';
-
-            const isEven = index % 2 === 0;
-
-            return (
-              <div
-                key={index}
-                className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${bgColor}  border-4 border-white text-center ${
-                  isEven ? ' mt-8' : 'z-20'
-                }`}
-              >
-                <goal.icon size={60} className="mb-2 text-gray-100" />
-                <p className="max-w-[200px] text-2xl font-medium text-gray-100">{goal.title}</p>
-              </div>
-            );
-          })}
-
-          <div className="col-span-3 -mt-[105px] flex justify-center">
-            {goals.slice(3, 5).map((goal, index) => {
-              const bgColor = index === 1 ? 'bg-green-40' : 'bg-green-55';
-
-              return (
+          {goals.map(
+            (goal, index) =>
+              goal.lgDisplay && (
                 <div
                   key={index}
-                  className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${bgColor} border-4 border-white text-center ${
-                    index === 0 ? 'z-10 mr-[-50px]' : 'z-0'
-                  }`}
+                  className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${goal.bgColor} border-4 border-white text-center ${goal.position} ${goal.zIndex}`}
                 >
-                  <goal.icon size={60} className="mb-2 text-gray-100" />
+                  <goal.icon size={goal.size} className="mb-2 text-gray-100" />
                   <p className="max-w-[200px] text-2xl font-medium text-gray-100">{goal.title}</p>
                 </div>
-              );
-            })}
+              ),
+          )}
+
+          <div className="col-span-3 -mt-[105px] flex justify-center">
+            {goals.map(
+              (goal, index) =>
+                !goal.lgDisplay && (
+                  <div
+                    key={index}
+                    className={`flex h-[300px] w-[300px] transform flex-col items-center justify-center rounded-full ${goal.bgColor} border-4 border-white text-center ${goal.position} ${goal.zIndex}`}
+                  >
+                    <goal.icon size={goal.size} className="mb-2 text-gray-100" />
+                    <p className="max-w-[200px] text-2xl font-medium text-gray-100">{goal.title}</p>
+                  </div>
+                ),
+            )}
           </div>
         </div>
+
         <div className="block grid grid-cols-3 items-center justify-center pt-4 lg:hidden">
           <div className="col-span-3 flex justify-center">
-            {goals.slice(0, 3).map((goal, index) => {
-              const bgColor = index === 1 || index === 3 ? 'bg-green-25' : 'bg-green-40';
-
-              const isEven = index % 2 === 0;
-
-              if (isEven) return null;
-
-              return (
-                <div
-                  key={index}
-                  className={`flex h-[200px] w-[200px] transform flex-col items-center justify-center rounded-full ${bgColor} z-40 border-4 border-white text-center`}
-                >
-                  <goal.icon size={50} className="mb-2 text-gray-100" />
-                  <p className="max-w-[150px] text-xl font-medium text-gray-100">{goal.title}</p>
-                </div>
-              );
-            })}
+            {goals.map(
+              (goal, index) =>
+                goal.lgDisplay &&
+                index % 2 !== 0 && (
+                  <div
+                    key={index}
+                    className={`flex h-[200px] w-[200px] transform flex-col items-center justify-center rounded-full ${goal.bgColor} z-40 border-4 border-white text-center`}
+                  >
+                    <goal.icon size={50} className="mb-2 text-gray-100" />
+                    <p className="max-w-[150px] text-xl font-medium text-gray-100">{goal.title}</p>
+                  </div>
+                ),
+            )}
           </div>
 
           <div className="col-span-3 -mt-[100px] flex justify-center">
-            {goals.slice(3, 5).map((goal, index) => {
-              const bgColor = index === 1 ? 'bg-green-40' : 'bg-green-55';
-
-              return (
-                <div
-                  key={index}
-                  className={`flex h-[200px] w-[200px] transform flex-col items-center justify-center rounded-full ${bgColor} mt-10 border-4 border-white text-center ${
-                    index === 0 ? 'z-10 mr-[-30px]' : 'z-0'
-                  }`}
-                >
-                  <goal.icon size={50} className="mb-2 text-gray-100" />
-                  <p className="max-w-[150px] text-xl font-medium text-gray-100">{goal.title}</p>
-                </div>
-              );
-            })}
+            {goals.map(
+              (goal, index) =>
+                !goal.lgDisplay && (
+                  <div
+                    key={index}
+                    className={`flex h-[200px] w-[200px] transform flex-col items-center justify-center rounded-full ${goal.bgColor} ml-[-20px] mr-[-20px]  mt-10 border-4 border-white text-center ${goal.zIndex}`}
+                  >
+                    <goal.icon size={50} className="mb-2 text-gray-100" />
+                    <p className="max-w-[150px] text-xl font-medium text-gray-100">{goal.title}</p>
+                  </div>
+                ),
+            )}
           </div>
         </div>
       </div>
