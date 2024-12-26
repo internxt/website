06f58a8457959668591bbc01b755c05f9fe66299
jaaaaavier <javipiagui13@gmail.com>
2024-12-26 13:34:43 +0100

    feat: antivirus page components

---
+++ src/components/antivirus/FeatureSectionV2.tsx
@@ -0,0 +1,56 @@
+import { AntivirusText } from '@/assets/types/antivirus';
+import { getImage } from '@/lib/getImage';
+import Image from 'next/image';
+export interface FeatureSectionV2Props {
+  textContent: AntivirusText['FeatureSectionV2'];
+}
+
+const FeatureSectionV2 = ({ textContent }: FeatureSectionV2Props) => {
+  const cards = [
+    {
+      image: '/images/antivirus/detect_malware.svg',
+      description: textContent.cards.element1.description,
+    },
+    {
+      image: '/images/antivirus/file_protection.svg',
+      description: textContent.cards.element2.description,
+    },
+    {
+      image: '/images/antivirus/file_scanning.svg',
+      description: textContent.cards.element3.description,
+    },
+  ];
+
+  return (
+    <section className="overflow-hidden px-5">
+      <div className="flex flex-col items-center justify-center space-y-16 bg-white py-10 sm:py-20">
+        <div className="flex w-full max-w-[90%] flex-col items-center justify-center space-y-6 text-center sm:max-w-[850px]">
+          <p className="text-3xl font-semibold text-gray-100 sm:text-5xl">{textContent.title}</p>
+          <p className="font-regular text-base text-gray-80 sm:text-xl">{textContent.description}</p>
+        </div>
+        <div className="grid grid-cols-1 justify-center gap-6 py-8 sm:gap-8 sm:py-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-12">
+          {cards.map((card, index) => (
+            <div
+              key={index}
+              className="flex min-h-[300px] w-full flex-col space-y-4 bg-gray-1 p-4  sm:max-w-[350px] sm:space-y-5 sm:p-6"
+            >
+              <Image
+                src={getImage(cards[index].image)}
+                alt="icon"
+                width={264}
+                height={164}
+                className="ml-5 text-primary"
+              />
+
+              <p className="mt-1 items-center text-center text-xl font-medium text-gray-80  sm:mt-2 sm:text-center sm:text-2xl">
+                {card.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeatureSectionV2;
---
+++ src/components/antivirus/HeroSection.tsx
@@ -0,0 +1,63 @@
+import { getImage } from '@/lib/getImage';
+import Header from '../shared/Header';
+import Image from 'next/image';
+
+const HeroSection = ({ textContent }) => (
+  <section className="flex w-full flex-col">
+    <div className="flex flex-col items-center pb-32 pt-32 lg:pt-40">
+      <div className="flex flex-col items-center justify-center space-y-6 px-5 text-center">
+        <div className="flex w-max items-center justify-center rounded-lg bg-gray-5 px-4 py-2">
+          <h2 className="text-xl font-medium text-gray-80">{textContent.eyeBrow}</h2>
+        </div>
+      </div>
+      <Header maxWidth="max-w-max" className="pt-5 text-center  text-gray-100">
+        <span className="text-primary">{textContent.title}</span> <br className="hidden sm:flex" />
+        {textContent.blueText}
+      </Header>
+
+      <p className="font-regular max-w-[800px] px-5 pt-5 text-center text-xl text-gray-80 sm:text-xl lg:mb-20">
+        {textContent.description}
+      </p>
+
+      <div className="relative flex h-full flex-col items-center">
+        <Image
+          src={getImage('/images/antivirus/internxt_antivirus_1.webp')}
+          width={88}
+          height={88}
+          alt="Internxt Antivirus checklist"
+          draggable="false"
+          className="absolute top-20 -translate-x-1/2 transform object-cover"
+        />
+
+        <Image
+          src={getImage('/images/antivirus/internxt_antivirus_2.webp')}
+          width={90}
+          height={90}
+          alt="Internxt Antivirus shield"
+          draggable="false"
+          className="absolute left-1/2 top-0 -translate-x-1/2 transform object-cover"
+        />
+
+        <Image
+          src={getImage('/images/antivirus/internxt_antivirus_3.webp')}
+          width={172}
+          height={172}
+          alt="Internxt Antivirus person checking"
+          draggable="false"
+          className="fade-in-r-large absolute bottom-0 left-1/2 -translate-x-1/2 transform animate-[spinAround-5s-linear-infinite] object-cover"
+        />
+
+        {/* Imagen principal estática */}
+        <Image
+          src={getImage('/images/antivirus/Internxt_Antivirus_Header.png')}
+          width={600}
+          height={460}
+          alt="Internxt Antivirus"
+          draggable="false"
+        />
+      </div>
+    </div>
+  </section>
+);
+
+export default HeroSection;
