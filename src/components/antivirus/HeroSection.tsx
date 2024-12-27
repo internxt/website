e2bccedd0501605438465f6160e05b96797f0e93
jaaaaavier <javipiagui13@gmail.com>
2024-12-27 14:45:40 +0100

    Update HeroSection.tsx

--- src/components/antivirus/HeroSection.tsx
+++ src/components/antivirus/HeroSection.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const HeroSection = ({ textContent }) => (
   <section className="flex w-full flex-col">
-    <div className="flex flex-col items-center pb-32 pt-32 lg:pt-40">
+    <div className="flex flex-col items-center pt-32 ">
       <div className="flex flex-col items-center justify-center space-y-6 px-5 text-center">
         <div className="flex w-max items-center justify-center rounded-lg bg-gray-5 px-4 py-2">
           <h2 className="text-xl font-medium text-gray-80">{textContent.eyeBrow}</h2>
@@ -15,57 +15,52 @@ const HeroSection = ({ textContent }) => (
         {textContent.blueText}
       </Header>
 
-      <p className="font-regular max-w-[800px] px-5 pt-5 text-center text-xl text-gray-80 sm:text-xl lg:mb-20">
+      <p className="font-regular max-w-[800px] px-5 pt-5 text-center text-xl text-gray-80 sm:text-xl">
         {textContent.description}
       </p>
 
-      <div className="relative flex h-screen items-center justify-center">
-        {/* Contenedor 3D */}
-        <div className="perspective-3d relative h-[600px] w-[600px]">
-          {/* Imagen principal (Estática) */}
+     <div className="relative flex h-[50vh] md:h-screen items-center justify-center">   
+        <Image
+          src={getImage('/images/antivirus/Internxt_Antivirus_Header.png')}
+          width={600}
+          height={460}
+          alt="Internxt Antivirus"
+          draggable="false"
+          className="object-cover"
+        />
+      
+        <div className="absolute inset-0">
           <Image
-            src={getImage('/images/antivirus/Internxt_Antivirus_Header.png')}
-            width={600}
-            height={460}
-            alt="Internxt Antivirus"
+            src={getImage('/images/antivirus/internxt_antivirus_1.webp')}
+            width={88}
+            height={88}
+            alt="Internxt Antivirus checklist"
             draggable="false"
-            className="object-cover"
+            className="absolute top-[20%] right-[-20%] object-cover hidden md:block"
           />
 
-          {/* Orbita de imágenes pequeñas */}
-          <div className="absolute inset-0">
-            {/* Imagen 1 */}
-            <Image
-              src={getImage('/images/antivirus/internxt_antivirus_1.webp')}
-              width={88}
-              height={88}
-              alt="Internxt Antivirus checklist"
-              draggable="false"
-              className="absolute left-1/2 top-0 animate-orbit object-cover"
-            />
+          
+          <Image
+            src={getImage('/images/antivirus/internxt_antivirus_2.webp')}
+            width={90}
+            height={90}
+            alt="Internxt Antivirus shield"
+            draggable="false"
+            className="absolute top-[40%] left-[-20%] object-cover hidden md:block"
+          />
 
-            {/* Imagen 2 */}
-            <Image
-              src={getImage('/images/antivirus/internxt_antivirus_2.webp')}
-              width={90}
-              height={90}
-              alt="Internxt Antivirus shield"
-              draggable="false"
-              className="absolute left-0 top-1/2 object-cover animate-orbit-diagonal"
-            />
-
-            {/* Imagen 3 */}
-            <Image
-              src={getImage('/images/antivirus/internxt_antivirus_3.webp')}
-              width={172}
-              height={172}
-              alt="Internxt Antivirus person checking"
-              draggable="false"
-              className=" absolute bottom-1/2 left-1/2 object-cover animate-orbit-vertical"
-            />
-          </div>
-        </div>
+          <Image
+            src={getImage('/images/antivirus/internxt_antivirus_3.webp')}
+            width={172}
+            height={172}
+            alt="Internxt Antivirus person checking"
+            draggable="false"
+            className="absolute top-[50%] right-[-18%] object-cover hidden md:block"
+          />
+        
       </div>
+    </div>
+
     </div>
   </section>
 );
