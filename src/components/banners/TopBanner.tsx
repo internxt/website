fe3a2fad9c4d5a03f3a9a417ea614b7b161480d8
jaaaaavier <javipiagui13@gmail.com>
2025-01-13 11:39:42 +0100

    update: top banner mobile version bg-color

--- src/components/banners/TopBanner.tsx
+++ src/components/banners/TopBanner.tsx
@@ -41,7 +41,7 @@ const TopBanner = ({ isBannerFixed }: TopBannerProps) => {
       <Link
         href={'/pricing'}
         target="_blank"
-        className={`group fixed left-0 z-30 flex h-[65px] w-screen items-center justify-center overflow-hidden bg-red text-white lg:hidden`}
+        className={`group fixed left-0 z-30 flex h-[65px] w-screen items-center justify-center overflow-hidden bg-primary text-white lg:hidden`}
       >
         <div className="flex flex-col items-center justify-center px-2 py-2 text-center">
           <div className="flex flex-col items-center justify-center">
