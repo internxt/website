71ba33cfb4d741c2e18e5bdea7647e80c6c6a3a7
jaaaaavier <javipiagui13@gmail.com>
2025-01-13 12:13:22 +0100

    feat: pricing new pormocode

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -50,7 +50,7 @@ const Pricing = ({ metatagsDescriptions, navbarLang, footerLang, lang, textConte
     businessCoupon,
     lifetimeCoupons,
   } = usePricing({
-    couponCode: PromoCodeName.SoftSales,
+    couponCode: PromoCodeName.PrivacyWeek,
   });
 
   const [pageName, setPageName] = useState('Pricing Individuals Annually');
