0f9b3e97af855f00035b4f5e85e52ec97ef9d015
jaaaaavier <javipiagui13@gmail.com>
2025-01-14 12:08:53 +0100

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -99,12 +99,13 @@ export default function HeroSection({ textContent, lang, isHomePageV2 }: HeroSec
               }
             />
           </div>
-          <div className=" hidden h-screen max-h-[600px] w-full justify-center lg:flex">
+          <div className=" hidden h-screen max-h-[700px] w-full justify-center lg:flex">
             <Image
               src={getImage('/images/privacy_week/internxt_dataprivacyweek_header.webp')}
-              width={600}
-              height={540}
+              width={619}
+              height={576}
               alt="Header Logo"
+              className="object-contain"
             />
           </div>
           {/* Desktop animation/image
