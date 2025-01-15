4db0d3a440a0a9161ae59f3ca828d481f21c04b0
jaaaaavier <javipiagui13@gmail.com>
2025-01-15 10:06:44 +0100

    feat: add Google Ads conversion tracking to Ctas

--- src/components/pricing/CtaSection.tsx
+++ src/components/pricing/CtaSection.tsx
@@ -2,22 +2,43 @@ import React from 'react';
 import { goToSignUpURL } from '@/lib/auth';
 
 const CtaSection = ({ textContent, freePlan }: { textContent: any; freePlan?: boolean }) => {
+  // Google Ads conversion tracking
+  const handleConversion = (callback: () => void) => {
+    if (window.gtag) {
+      window.gtag('event', 'conversion', {
+        send_to: 'AW-728922855/-RgbCLv9z4caEOf1ydsC',
+        value: 1.0,
+        currency: 'EUR',
+        event_callback: callback,
+      });
+    } else {
+      callback();
+    }
+  };
+
   return (
     <section className="overflow-hidden bg-primary py-14">
       <div className="flex flex-col items-center justify-center space-y-8 px-5 text-center">
-        <div className="flex  flex-col items-center space-y-4 text-center text-white">
+        <div className="flex flex-col items-center space-y-4 text-center text-white">
           <p className="text-4xl font-semibold">{textContent.title}</p>
           <p className="w-full max-w-[570px] text-xl font-normal">{textContent.description}</p>
         </div>
         <button
           className="flex rounded-lg bg-white px-5 py-3 text-lg font-medium text-primary hover:bg-blue-10"
           onClick={() => {
-            freePlan
-              ? goToSignUpURL()
-              : window.scrollTo({
-                  top: document.getElementById('priceTable')?.offsetTop,
-                  behavior: 'smooth',
-                });
+            handleConversion(() => {
+              if (freePlan) {
+                goToSignUpURL();
+              } else {
+                const priceTable = document.getElementById('priceTable');
+                if (priceTable) {
+                  window.scrollTo({
+                    top: priceTable.offsetTop,
+                    behavior: 'smooth',
+                  });
+                }
+              }
+            });
           }}
         >
           {textContent.cta}
--- src/components/shared/CtaSection.tsx
+++ src/components/shared/CtaSection.tsx
@@ -8,36 +8,66 @@ const CtaSection = ({
   target,
   bgImage,
   onClick,
-  customDescription
+  customDescription,
 }: {
   textContent: any;
   url: string;
   maxWidth?: string;
   target?: string;
-  bgImage?: string
-  onClick?: ()=> void; 
+  bgImage?: string;
+  onClick?: () => void;
   customDescription?: React.ReactNode;
 }) => {
-  const defaultBgImage = getImage('/images/cyber-awareness/Background.svg'); 
+  const defaultBgImage = getImage('/images/cyber-awareness/Background.svg');
+
+  const handleClick = () => {
+    const callback = () => {
+      if (url) {
+        window.location.href = url;
+      }
+    };
+
+    // Google Ads conversion tracking
+    if (window.gtag) {
+      window.gtag('event', 'conversion', {
+        send_to: 'AW-728922855/-RgbCLv9z4caEOf1ydsC',
+        value: 1.0,
+        currency: 'EUR',
+        event_callback: callback,
+      });
+    } else {
+      callback();
+    }
+  };
+
   return (
     <section
-      style={{ 
-        backgroundImage: `url(${bgImage ? getImage(bgImage) : defaultBgImage})` 
+      style={{
+        backgroundImage: `url(${bgImage ? getImage(bgImage) : defaultBgImage})`,
       }}
       className="overflow-hidden bg-primary bg-cover px-5 py-14"
     >
       <div className="flex flex-col items-center justify-center space-y-8 text-center">
-        <div className={`flex  ${maxWidth} flex-col items-center space-y-4 text-center text-white`}>
+        <div className={`flex ${maxWidth} flex-col items-center space-y-4 text-center text-white`}>
           <p className="text-4xl font-semibold">{textContent.title}</p>
           {customDescription}
-            
         </div>
         <Link
           href={url}
           target={target}
+          onClick={
+            onClick
+              ? () => {
+                  onClick();
+                  handleClick();
+                }
+              : handleClick
+          } // Combina onClick existente con la lógica de conversión
           className={`flex rounded-lg px-5 py-3 text-lg font-medium ${
-          bgImage && bgImage !== defaultBgImage ? 'bg-primary text-white text-xl hover:bg-primary-dark' : 'bg-white text-primary hover:bg-blue-10'
-        }`}
+            bgImage && bgImage !== defaultBgImage
+              ? 'bg-primary text-xl text-white hover:bg-primary-dark'
+              : 'bg-white text-primary hover:bg-blue-10'
+          }`}
         >
           {textContent.cta}
         </Link>
