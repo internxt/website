0dc3e3418de8b289da47a4b742c9e488c1198e5c
jaaaaavier <javipiagui13@gmail.com>
2025-01-24 17:56:13 +0100

    Update twitter.js

--- public/js/twitter.js
+++ public/js/twitter.js
@@ -1,6 +1,18 @@
-!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
-a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-// Insert Twitter Pixel ID and Standard Event data below
-twq('init','nz3rh');
-twq('track','PageView');
+export const initializeTwitterPixel = () => {
+  !(function (e, t, n, s, u, a) {
+    e.twq ||
+      ((s = e.twq =
+        function () {
+          s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+        }),
+      (s.version = '1.1'),
+      (s.queue = []),
+      (u = t.createElement(n)),
+      (u.async = !0),
+      (u.src = 'https://static.ads-twitter.com/uwt.js'),
+      (a = t.getElementsByTagName(n)[0]),
+      a.parentNode.insertBefore(u, a));
+  })(window, document, 'script');
+  twq('init', 'nz3rh');
+  twq('track', 'PageView');
+};
