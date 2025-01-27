02952f17de3507e48bc392a080ed2f40e8752312
jaaaaavier <javipiagui13@gmail.com>
2025-01-27 12:26:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -224,7 +224,7 @@ module.exports = {
         'float-sock': 'float-sock 1.5s ease-in-out infinite',
         'move-vertical': 'move-vertical 5s linear infinite',
         fall: 'fall 5s linear infinite',
-        'fill-bar': 'fill 0.25s linear forwards',
+        'line-scan': 'lineMove 1s linear infinite',
       },
       translate: {
         25: '6.25rem',
@@ -272,6 +272,10 @@ module.exports = {
         fill: {
           '0%': { width: '0%' },
           '100%': { width: '100%' },
+        },
+        lineMove: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(550%)' },
         },
       },
 
