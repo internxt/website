9b67072b0f59309273024991d566ba7180af620b
jaaaaavier <javipiagui13@gmail.com>
2025-01-28 13:08:56 +0100

    Update HeroSection.tsx

--- src/components/annual-plans-for-affiliates/HeroSection.tsx
+++ src/components/annual-plans-for-affiliates/HeroSection.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import HeroSectionSafeArea from '../shared/HeroSectionSafeArea';
 import Button from '../shared/Button';
 import { Percent, X } from '@phosphor-icons/react';
+import { getImage } from '@/lib/getImage';
 
 interface HeroSectionProps {
   textContent: any;
@@ -26,6 +27,28 @@ const StartPageLabel = () => {
   );
 };
 
+const BraveLabel = () => {
+  return (
+    <div className="flex flex-row items-center space-x-3.5">
+      <Image
+        src={getImage('/images/partnerships/brave/Brave_Browser_logotype.svg')}
+        width={117}
+        height={27}
+        alt="Brave logo"
+      />
+      <X size={16} />
+      <Image
+        loading="lazy"
+        className="select-none"
+        src={`../../logos/internxt/cool-gray-90.svg`}
+        alt="Internxt logo"
+        width={130}
+        height={16}
+      />
+    </div>
+  );
+};
+
 const HeroSection: React.FC<HeroSectionProps> = ({ textContent, InfoTextComponent, isStartPage }) => {
   function redirectToPricingTable() {
     window.location.href = '#payment';
@@ -36,7 +59,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ textContent, InfoTextComponen
       <HeroSectionSafeArea>
         <div className="flex max-w-[550px] flex-col items-center justify-center space-y-8 text-center lg:items-start lg:text-left">
           <div className="flex flex-col space-y-4">
-            {isStartPage ? <StartPageLabel /> : null}
+            {isStartPage ? <StartPageLabel /> : <BraveLabel />}
             <h1 className="text-6xl font-bold text-gray-100">{textContent.title}</h1>
             <h2 className="text-4xl font-semibold text-primary">{textContent.subtitle}</h2>
           </div>
@@ -46,13 +69,23 @@ const HeroSection: React.FC<HeroSectionProps> = ({ textContent, InfoTextComponen
           </div>
           <Button onClick={redirectToPricingTable} text={textContent.cta} />
         </div>
-        <Image
-          src="/images/affiliates/Startpage_discount_collab.webp"
-          alt="Affiliates Hero Section"
-          draggable={false}
-          width={500}
-          height={500}
-        />
+        {isStartPage ? (
+          <Image
+            src="/images/affiliates/Startpage_discount_collab.webp"
+            alt="Affiliates Hero Section"
+            draggable={false}
+            width={500}
+            height={500}
+          />
+        ) : (
+          <Image
+            src={getImage('/images/partnerships/brave/internxt_brave_secure_cloud_storage.webp')}
+            alt="Affiliates Hero Section"
+            draggable={false}
+            width={500}
+            height={500}
+          />
+        )}
       </HeroSectionSafeArea>
     </section>
   );
