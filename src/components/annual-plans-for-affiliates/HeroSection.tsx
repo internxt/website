eb96bdfc5078c296499bd9e929a16e0cda4a6ae2
jaaaaavier <javipiagui13@gmail.com>
2025-02-04 12:00:28 +0100

    Update HeroSection.tsx

--- src/components/annual-plans-for-affiliates/HeroSection.tsx
+++ src/components/annual-plans-for-affiliates/HeroSection.tsx
@@ -8,6 +8,8 @@ interface HeroSectionProps {
   textContent: any;
   InfoTextComponent?: React.ReactNode;
   isStartPage?: boolean;
+  isBrave?: boolean;
+  isCloudWards?: boolean;
   redirect?: string;
 }
 
@@ -50,7 +52,31 @@ const BraveLabel = () => {
   );
 };
 
-const HeroSection: React.FC<HeroSectionProps> = ({ textContent, InfoTextComponent, isStartPage, redirect }) => {
+const CloudWardsLabel = () => {
+  return (
+    <div className="flex flex-row items-center justify-center space-x-3.5 lg:justify-start ">
+      <Image src={getImage('/images/partnerships/cloudwards/logo.svg')} width={117} height={27} alt="Brave logo" />
+      <X size={16} />
+      <Image
+        loading="lazy"
+        className="select-none"
+        src={`../../logos/internxt/cool-gray-90.svg`}
+        alt="Internxt logo"
+        width={130}
+        height={16}
+      />
+    </div>
+  );
+};
+
+const HeroSection: React.FC<HeroSectionProps> = ({
+  textContent,
+  InfoTextComponent,
+  isStartPage,
+  redirect,
+  isBrave,
+  isCloudWards,
+}) => {
   function redirectToPricingTable() {
     window.location.href = redirect ?? '#payment';
   }
@@ -60,7 +86,9 @@ const HeroSection: React.FC<HeroSectionProps> = ({ textContent, InfoTextComponen
       <HeroSectionSafeArea>
         <div className="flex max-w-[550px] flex-col items-center justify-center space-y-8 text-center text-center lg:items-start lg:text-left">
           <div className="flex flex-col space-y-4">
-            {isStartPage ? <StartPageLabel /> : <BraveLabel />}
+            {isStartPage ? <StartPageLabel /> : null}
+            {isCloudWards ? <CloudWardsLabel /> : null}
+            {isBrave ? <BraveLabel /> : null}
             <h1 className="text-6xl font-bold text-gray-100">{textContent.title}</h1>
             <h2 className="text-4xl font-semibold text-primary">{textContent.subtitle}</h2>
           </div>
