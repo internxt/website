e0d90a303e8243a0ccd98a1ea008fa3043ac44b9
jaaaaavier <javipiagui13@gmail.com>
2025-02-06 15:11:44 +0100

    Update index.ts

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -118,7 +118,7 @@ export enum PromoCodeName {
   PrivacyWeek = 'PRIVACY85',
   Brave = 'BRAVE85',
   SuperBowlCampaign = 'SUPERBOWL80',
-  ValentinesCampaign = 'MYVALENTINE80',
+  ValentinesCampaign = 'MYVALENTINE85',
 }
 
 export interface PromoCodeProps {
