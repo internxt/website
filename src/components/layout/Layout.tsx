f72d5f507f23e6f881ed6a7711228088d0ac77bd
jaaaaavier <javipiagui13@gmail.com>
2025-02-13 08:49:04 +0100

    remove: twitter ads tracking

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -186,30 +186,6 @@ LayoutProps) {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
 
         <script
-          id="twitter-pixel"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(e,t,n,s,u,a)
-            {e.twq ||
-              ((s = e.twq =
-                function () {
-                  s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
-                }),
-              (s.version = '1.1'),
-              (s.queue = []),
-              (u = t.createElement(n)),
-              (u.async = !0),
-              (u.src = '//static.ads-twitter.com/uwt.js'),
-              (a = t.getElementsByTagName(n)[0]),
-              a.parentNode.insertBefore(u, a))}
-            (window,document,'script');
-            twq('init','nz3rh');
-            twq('track','PageView');
-          `,
-          }}
-        />
-
-        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
