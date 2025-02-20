c4a9ece90599db86afa9ccd097c56be6012fe6cf
jaaaaavier <javipiagui13@gmail.com>
2025-02-20 16:51:11 +0100

    feat: new components design

--- src/components/shared/components/HeroSection.tsx
+++ src/components/shared/components/HeroSection.tsx
@@ -27,8 +27,8 @@ export const HeroSection = ({
     <section className={`overflow-hidden  py-20 ${background}`} style={style}>
       <HeroSectionSafeArea>
         <div className={`flex w-full flex-col items-center  gap-10 lg:flex-row lg:justify-between`}>
-          <div className="flex max-w-[550px] flex-col">{TextComponent}</div>
-          <div className="flex flex-col items-center">
+          <div className="flex max-h-[570px] min-h-[570px] flex-col">{TextComponent}</div>
+          <div className="flex max-h-[570px] flex-col items-center pt-20 md:min-h-[570px]">
             {ImageComponent}
             {imageProperties && (
               <Image
