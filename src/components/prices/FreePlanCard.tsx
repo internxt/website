98dccad1b774b4d5e019e2bbef94899db0fe55a5
jaaaaavier <javipiagui13@gmail.com>
2025-02-20 11:37:07 +0100

    feat: FreePlanCard new design

--- src/components/prices/FreePlanCard.tsx
+++ src/components/prices/FreePlanCard.tsx
@@ -1,5 +1,6 @@
 import { GlobalDialog, useGlobalDialog } from '@/contexts/GlobalUIManager';
-
+import { CheckSquare } from '@phosphor-icons/react';
+import { isMobile } from 'react-device-detect';
 interface FreePlanCardProps {
   textContent: any;
 }
@@ -7,29 +8,18 @@ interface FreePlanCardProps {
 const FreePlanCard = ({ textContent }: FreePlanCardProps): JSX.Element => {
   const { openDialog } = useGlobalDialog();
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-5 rounded-2xl border border-gray-10 px-5 py-9 md:flex-row md:justify-between">
-      <div className="flex flex-col space-y-5 text-center md:text-left">
-        <div className="flex flex-row items-center gap-2">
-          <div className="flex w-max flex-col items-center space-y-2 rounded-full bg-primary/7 px-5 py-2 text-center">
-            <p className="text-xl font-medium text-primary">{textContent.freeForever}</p>
-          </div>
-          <p className="text-2xl font-bold text-gray-100">{textContent.getStarted}</p>
+    <div className="flex min-h-[300px] w-full flex-col justify-center rounded-2xl border border-gray-10 md:max-h-[208px] md:min-h-[208px]">
+      <div className="bg-green flex flex-col items-center space-y-6">
+        <div className="flex  max-h-[28px] min-w-[154px] flex-col rounded-full bg-primary/7 text-center">
+          <p className="text-xl font-medium text-primary">{textContent.title}</p>
         </div>
-        <div className="flex flex-row space-x-2 md:items-center">
-          <img
-            loading="lazy"
-            className="mt-0.5 h-4 w-4 select-none"
-            src="/icons/checkPrimary.svg"
-            draggable="false"
-            alt="check icon"
-          />
-          <p className="text-gray-80">{textContent.enjoy10gb}</p>
+        <div className={`flex flex-row items-start justify-start ${isMobile ? 'px-20' : 'px-10'} text-center`}>
+          <CheckSquare className="min-h-[24px] min-w-[24px] pt-1 text-primary" />
+          <p className="font-regular text-lg text-gray-80">{textContent.getStarted}</p>
         </div>
-      </div>
 
-      <div className="flex">
         <button
-          className="flex w-full flex-col rounded-lg border border-primary px-20 py-2.5 font-medium text-primary hover:bg-gray-1 active:bg-gray-5"
+          className="flex min-h-[40px] min-w-[300px] flex-col items-center justify-center rounded-lg border border-primary text-center font-medium text-primary hover:bg-gray-1 active:bg-gray-5 md:min-h-[44px] md:min-w-[356px]"
           onClick={() => {
             openDialog(GlobalDialog.FreeSpaceCardBanner);
           }}
