80136b6c8ddf46d0ed7423d6b0a8d77ff1159e9b
jaaaaavier <javipiagui13@gmail.com>
2025-02-21 15:41:50 +0100

    update: mobile version for heroSection

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -22,8 +22,8 @@ export default function HeroSection({ textContent, lang, isHomePageV2 }: HeroSec
   const router = useRouter();
   const { dialogIsOpen } = useGlobalDialog();
   const shouldShowMobileBanner = dialogIsOpen(GlobalDialog.MobileBannerForHome);
-  const mobileImage = getImage('/images/campaigns/valentines/internxt_superbowl2025_mobile.webp');
-  const blurBgImage = getImage('/images/campaigns/euro/grass.webp');
+  const mobileImage = getImage('/images/home/image_mobile.webp');
+  const blurBgImage = getImage('/images/home/header/bg.svg');
   const componentsFlow = isHomePageV2 ? 'flex-col-reverse' : 'flex-col';
   const titleAndOnePlanText = isHomePageV2 ? textContent.TitleAndOnePlanV2 : textContent.TitleAndOnePlan;
   const handleOnClick = () => {
@@ -48,7 +48,7 @@ export default function HeroSection({ textContent, lang, isHomePageV2 }: HeroSec
                   width={600}
                   height={1000}
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  alt="Superbowl Mobile Image"
+                  alt="HeroSectiond Mobile Image"
                   onClick={handleOnClick}
                 />
               </div>
