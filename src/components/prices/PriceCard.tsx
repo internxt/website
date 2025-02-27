f65cdd75dbe090b6d9f6a15b558e796c10382b88
jaaaaavier <javipiagui13@gmail.com>
2025-02-27 18:16:28 +0100

    lifetimes: updates

--- src/components/prices/PriceCard.tsx
+++ src/components/prices/PriceCard.tsx
@@ -1,8 +1,22 @@
 import { PromoCodeProps } from '@/lib/types';
-import { Fire } from '@phosphor-icons/react';
+import {
+  ArrowsClockwise,
+  CodeBlock,
+  Database,
+  Envelope,
+  Fingerprint,
+  Fire,
+  Gauge,
+  Key,
+  LockSimple,
+  Password,
+  ShieldPlus,
+  VideoConference,
+} from '@phosphor-icons/react';
 import { Interval } from '../services/stripe.service';
 import { LifetimeMode } from '../lifetime/PaymentSection';
 import { checkout, checkoutForPcComponentes, goToSignUpURL } from '@/lib/auth';
+import React from 'react';
 
 export interface PriceCardProps {
   planType: string;
@@ -25,8 +39,10 @@ export interface PriceCardProps {
 }
 
 const STORAGE_LEVELS = {
+  '1TB': 'Essential ',
+  '3TB': 'Premium ',
   '2TB': 'Lite ',
-  '5TB': 'Pro ',
+  '5TB': 'Ultimate ',
   '10TB': 'Ultra ',
 };
 
@@ -56,6 +72,20 @@ export default function PriceCard({
   };
 
   const contentText = require(`@/assets/lang/${lang}/priceCard.json`);
+
+  const iconsFeatures = [
+    Database,
+    Key,
+    Gauge,
+    ShieldPlus,
+    ArrowsClockwise,
+    Password,
+    LockSimple,
+    Fingerprint,
+    CodeBlock,
+    VideoConference,
+    Envelope,
+  ];
 
   function onCheckoutButtonClicked() {
     if (lifetimeMode === 'redeem') return onButtonClicked?.();
@@ -107,9 +137,11 @@ export default function PriceCard({
 
   return (
     <div
-      className={`${'border-primary ring-[1px]'} flex max-w-xs flex-shrink-0 flex-grow-0 flex-col overflow-hidden rounded-2xl xs:w-72`}
+      className={`${'border-primary ring-[1px]'} flex max-h-[720px] min-h-[400px] min-w-[370px] max-w-xs flex-shrink-0 flex-grow-0 flex-col overflow-hidden rounded-2xl xs:w-72`}
     >
-      <div className={`info flex flex-col items-center justify-center space-y-6 rounded-t-2xl bg-white p-6 pt-6`}>
+      <div
+        className={`info flex max-h-[300px]  min-h-[300px] flex-col items-center justify-center space-y-6 rounded-t-2xl bg-white p-6 pt-6`}
+      >
         <div className="flex flex-col items-center justify-center space-y-4">
           {popular ? (
             <div className="flex flex-row items-center justify-center space-x-2 rounded-full bg-primary px-3 py-1">
@@ -191,17 +223,17 @@ export default function PriceCard({
           </>
         ) : null}
 
-        <div className="flex flex-col space-y-2 pt-6">
-          {contentText.productFeatures.individuals[storage].map((feature) => (
-            <div className="flex flex-row items-start space-x-2 px-6 last:font-semibold" key={feature}>
-              <img
-                loading="lazy"
-                className="translate-y-px select-none"
-                src="/icons/checkPrimary.svg"
-                draggable="false"
-                alt="check icon"
-              />
+        <div className="flex max-h-[500px] min-h-[500px] flex-col  space-y-2 pt-6">
+          {contentText.productFeatures.individuals[storage].map((feature, index) => (
+            <div className="flex flex-row items-start space-x-2 px-6 first:font-semibold" key={feature}>
+              {React.createElement(iconsFeatures[index % iconsFeatures.length], {
+                size: 24,
+                className: 'text-primary',
+              })}
               <span className="text-gray-80">{feature}</span>
+              {index > 8 ? (
+                <span className="rounded-lg bg-orange/10 px-1 text-orange">{contentText.commingSoon}</span>
+              ) : null}
             </div>
           ))}
         </div>
