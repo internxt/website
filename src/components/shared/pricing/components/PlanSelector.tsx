00d1a11fd36a79fa27efbf3e43ed2fc02fe8b735
jaaaaavier <javipiagui13@gmail.com>
2025-02-27 13:22:36 +0100

    Update PlanSelector.tsx

--- src/components/shared/pricing/components/PlanSelector.tsx
+++ src/components/shared/pricing/components/PlanSelector.tsx
@@ -64,7 +64,6 @@ export const PlanSelector = ({
       }`}
     >
       {textContent.billingFrequency.business}
-      <SealPercent size={24} className="hidden text-green-1 sm:flex" weight="fill" />
     </button>
   </div>
 );
