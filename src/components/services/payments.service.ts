ba2c5739b19979faad633425087aa2709e63a80c
jaaaaavier <javipiagui13@gmail.com>
2025-03-13 14:03:14 +0100

    Update payments.service.ts

--- src/components/services/payments.service.ts
+++ src/components/services/payments.service.ts
@@ -34,7 +34,7 @@ const getCustomerId = async (name: string, email: string, country?: string, comp
   return res.data;
 };
 
-const getPaymentIntent = async (customerId: string, plan: PlanData, token: string) => {
+const getPaymentIntent = async (customerId: string, plan: PlanData, token: string, couponCode?: string) => {
   const { id: priceId, amount, currency } = plan;
 
   const response = await axios.get(`${process.env.NEXT_PUBLIC_PAYMENTS_API}/payment-intent-for-object-storage`, {
@@ -44,6 +44,7 @@ const getPaymentIntent = async (customerId: string, plan: PlanData, token: strin
       amount,
       token,
       currency,
+      couponCode,
     },
   });
 
@@ -60,6 +61,7 @@ const createSubscription = async (
   token: string,
   companyName: string,
   vatId: string,
+  couponCode?: string,
 ) => {
   const { id: priceId, currency } = plan;
 
@@ -70,6 +72,7 @@ const createSubscription = async (
     currency,
     companyName,
     companyVatId: vatId,
+    couponCode,
   });
 
   const res = response.data;
