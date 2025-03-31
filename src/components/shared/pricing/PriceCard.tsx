9742d9ccbb4f5c162947d4b38a618a174ccb062e
jaaaaavier <javipiagui13@gmail.com>
2025-03-31 16:26:55 +0200

    Update PriceCard.tsx

--- src/components/shared/pricing/PriceCard.tsx
+++ src/components/shared/pricing/PriceCard.tsx
@@ -125,7 +125,7 @@ export const PriceCard = ({
       className={`${
         !darkMode && popular ? `border-${colorCard}/50 ring-[3px]` : darkMode ? '' : 'ring-1 ring-gray-10'
       } m-2 flex ${cardMaxWidth} ${
-        isBusiness ? `max-h-[820px] min-h-[700px]` : `max-h-[760px] min-h-[700px] `
+        isBusiness ? `max-h-[820px] min-h-[700px]` : `max-h-[780px] min-h-[700px] `
       } min-w-[380px] flex-shrink-0 flex-grow-0 flex-col  overflow-hidden rounded-2xl`}
     >
       <div
@@ -225,7 +225,7 @@ export const PriceCard = ({
       <div
         className={`featureList flex flex-col  ${
           darkMode ? 'bg-gray-100' : 'border-t border-neutral-20 bg-neutral-10'
-        } ${isBusiness ? `h-[480px] ` : `h-[415px]`} pb-6 text-sm`}
+        } ${isBusiness ? `h-[490px] ` : `h-[415px]`} pb-6 text-sm`}
       >
         <div className="flex flex-col space-y-2 pt-6">
           {contentText.productFeatures[productCardPlan][storage].map((feature, index) => (
