7b995658bd46d29d9ba6354a131462f34893bb32
NeoKlims <145866834+NeoKlims@users.noreply.github.com>
2025-04-24 15:37:34 +0300

    new page in footer

--- src/components/layout/footers/Footer.tsx
+++ src/components/layout/footers/Footer.tsx
@@ -494,6 +494,17 @@ export default function Footer({
                         {textContent.FooterSection.new}
                       </span>
                     </Link>
+                    <Link
+                      href="/cloud-storage-for-photos"
+                      locale={lang}
+                      passHref
+                      className="items-center hover:text-primary"
+                    >
+                      {textContent.FooterSection.sections.features.cloudPhotos}
+                      <span className="ml-2  h-max items-center justify-center rounded-full bg-primary bg-opacity-15 px-2 py-1 text-xs font-medium uppercase text-primary">
+                        {textContent.FooterSection.new}
+                      </span>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -962,6 +973,10 @@ export default function Footer({
 
                       <Link href="/gdpr-cloud-storage" locale={lang} passHref>
                         {textContent.FooterSection.sections.features.GDPRCloud}
+                      </Link>
+
+                      <Link href="/cloud-storage-for-photos" locale={lang} passHref>
+                        {textContent.FooterSection.sections.features.cloudPhotos}
                       </Link>
                     </Disclosure.Panel>
                   </Transition>
