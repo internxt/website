8bf3d207b4126b2547d903a37a49fa33ddd12173
NeoKlims <145866834+NeoKlims@users.noreply.github.com>
2025-05-23 12:34:29 +0300

    tools section icons

--- src/components/shared/sections/ToolsSection.tsx
+++ src/components/shared/sections/ToolsSection.tsx
@@ -62,19 +62,19 @@ export const ToolsSection = ({ textContent, lang, bgColor }: { textContent: any;
       pathname: textContent.toolsCard[6].pathname,
     },
     {
-      icon: Detective,
+      icon: Broom,
       title: textContent.toolsCard[7].title,
       cta: textContent.toolsCard[7].cta,
       pathname: textContent.toolsCard[7].pathname,
     },
     {
-      icon: Brain,
+      icon: Detective,
       title: textContent.toolsCard[8].title,
       cta: textContent.toolsCard[8].cta,
       pathname: textContent.toolsCard[8].pathname,
     },
     {
-      icon: Broom,
+      icon: Brain,
       title: textContent.toolsCard[9].title,
       cta: textContent.toolsCard[9].cta,
       pathname: textContent.toolsCard[9].pathname,
