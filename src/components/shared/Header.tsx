0e5707d3c63fde5e8eb5f7b6647c4e1aabd3e318
jaaaaavier <javipiagui13@gmail.com>
2025-06-26 16:25:00 +0200

    Update Header.tsx

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,4 +1,18 @@
-const Header = ({ children, className }: { children: React.ReactNode; className?: string }): JSX.Element => {
+const Header = ({
+  children,
+  maxWidth = 'max-w-[796px]',
+  className,
+  textHeightForDesk = 'sm:text-5xl',
+  withoutLeading,
+}: {
+  children: React.ReactNode;
+  maxWidth?: string;
+  className?: string;
+  withoutLeading?: boolean;
+  isToolsPage?: boolean;
+  textHeightForDesk?: string;
+}): JSX.Element => {
+  const leading = withoutLeading ? '' : 'sm:leading-tight';
   return (
     <h1
       className={`h-[72px] whitespace-normal text-3xl font-semibold leading-tight lg:text-5xl xl:h-[116px] xl:pt-2 ${className} `}
