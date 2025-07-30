70d22e093d8f9eb41f6b26646a1154738eebd4f3
jaaaaavier <javipiagui13@gmail.com>
2025-07-30 17:22:03 +0200

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -150,7 +150,7 @@ LayoutProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P7N7LW5G');`,
+            })(window,document,'script','dataLayer','GTM-5GWZKXKB');`,
           }}
         ></script>
 
