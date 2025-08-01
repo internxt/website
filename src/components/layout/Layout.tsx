159eb510034e418ea32c93d5cec4240a35fb5596
jaaaaavier <javipiagui13@gmail.com>
2025-08-01 08:22:12 +0200

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -150,7 +150,7 @@ LayoutProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5GWZKXKB');`,
+            })(window,document,'script','dataLayer','GTM-P7N7LW5');`,
           }}
         ></script>
 
@@ -158,9 +158,9 @@ LayoutProps) {
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer2'?'&l='+l:'';j.async=true;j.src=
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer2','GTM-5GWZKXKB');`,
+          })(window,document,'script','dataLayer','GTM-5GWZKXKB');`,
           }}
         />
 
