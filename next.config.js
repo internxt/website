f555897436843a077dc9255df3b82e77f8927a97
jaaaaavier <javipiagui13@gmail.com>
2025-09-10 10:44:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -227,6 +227,21 @@ module.exports = {
         destination: '/comparison/mega-alternative',
         permanent: false,
       },
+      {
+        source: '/pcloud-alternative',
+        destination: '/comparison/pcloud-alternative',
+        permanent: false,
+      },
+      {
+        source: '/dropbox-alternative',
+        destination: '/comparison/dropbox-alternative',
+        permanent: false,
+      },
+      {
+        source: '/mega-alternative',
+        destination: '/comparison/mega-alternative',
+        permanent: false,
+      },
     ];
   },
   images: {
