1a4d32d022a37f4ba7404ee971845d05aeb8df9c
jaaaaavier <javipiagui13@gmail.com>
2025-10-31 10:58:46 +0100

    Update Navbar.tsx

--- src/components/layout/navbars/Navbar.tsx
+++ src/components/layout/navbars/Navbar.tsx
@@ -95,7 +95,7 @@ export default function Navbar(props: Readonly<NavbarProps>) {
         menuState ? 'bg-opacity-100' : ''
       } z-40 border-b border-black`}
     >
-      <div className=" w-full lg:px-10 lg:pt-1 xl:px-32 3xl:px-80">
+      <div className=" w-full lg:px-10 lg:pt-1 xl:pl-32 3xl:pl-80">
         <div className="flex items-center justify-between">
           <div className="flex flex-row gap-12">
             <div className="flex flex-row items-center justify-start space-x-4 lg:space-x-0">
