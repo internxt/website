67bb685f28c0f9f73a962c3749dd4f24cb6cf51c
jaaaaavier <javipiagui13@gmail.com>
2025-11-03 10:54:24 +0100

    Update LanguageMobileBox.tsx

--- src/components/layout/components/LanguageMobileBox.tsx
+++ src/components/layout/components/LanguageMobileBox.tsx
@@ -3,7 +3,6 @@ import { CaretDown, CaretUp, Globe } from '@phosphor-icons/react';
 import { useRouter } from 'next/router';
 
 interface LanguageMobileBoxProps {
-  singlesDay?: boolean;
   darkMode?: boolean;
 }
 
@@ -18,15 +17,12 @@ const languages = [
   { text: '中国人 (TW)', lang: 'zh-tw' },
 ];
 
-export default function LanguageMobileBox({ darkMode, singlesDay }: LanguageMobileBoxProps) {
+export default function LanguageMobileBox({ darkMode }: LanguageMobileBoxProps) {
   const router = useRouter();
   const lang = router.locale || 'en';
   const selectedLanguage = languages.find((language) => language.lang === lang)?.text || 'Select Language';
 
-  // Filtramos idiomas si estamos en singlesDay
-  const filteredLanguages = singlesDay
-    ? languages.filter((language) => ['en', 'zh', 'zh-tw'].includes(language.lang))
-    : languages;
+  const filteredLanguages = languages;
 
   return (
     <div className="flex w-screen">
@@ -57,19 +53,24 @@ export default function LanguageMobileBox({ darkMode, singlesDay }: LanguageMobi
                   darkMode ? 'bg-gray-71 text-green-120' : 'text-gray-60'
                 } max-h-48 space-y-2 overflow-y-auto p-4`}
               >
-                {filteredLanguages.map((language) => (
-                  <a
-                    key={language.text}
-                    href={`${language.lang === 'en' ? '' : `/${language.lang}`}${router.pathname}`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      router.push(router.pathname, router.pathname, { locale: language.lang });
-                    }}
-                    className="block w-full py-2 text-center"
-                  >
-                    {language.text}
-                  </a>
-                ))}
+                {filteredLanguages.map((language) => {
+                  const langPrefix = language.lang === 'en' ? '' : `/${language.lang}`;
+                  const finalHref = `${langPrefix}${router.pathname}`;
+
+                  return (
+                    <a
+                      key={language.text}
+                      href={finalHref}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        router.push(router.pathname, router.pathname, { locale: language.lang });
+                      }}
+                      className="block w-full py-2 text-center"
+                    >
+                      {language.text}
+                    </a>
+                  );
+                })}
               </Disclosure.Panel>
             </Transition>
           </>
