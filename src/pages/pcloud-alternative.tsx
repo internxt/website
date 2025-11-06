9a39367f2d1f0fc3084ba76e1234f8885ef4d675
jaaaaavier <javipiagui13@gmail.com>
2025-11-06 16:55:55 +0100

    feat: JSON and HeroSection update for dropbox, pcloud and mega LPs

--- src/pages/pcloud-alternative.tsx
+++ src/pages/pcloud-alternative.tsx
@@ -1,5 +1,4 @@
-import { HeroSection } from '@/components/comparison/pCloud-alternative/HeroSection';
-import { TablesSection } from '@/components/comparison/pCloud-alternative/TablesSection';
+import { TablesSection } from '@/components/comparison/TablesSection';
 import Layout from '@/components/layout/Layout';
 import { MinimalFooter } from '@/components/layout/footers/MinimalFooter';
 import Navbar from '@/components/layout/navbars/Navbar';
@@ -13,6 +12,7 @@ import FAQSection from '@/components/shared/sections/FaqSection';
 import HorizontalScrollableSection from '@/components/comparison/HorizontalScrollableSection';
 import FloatingCtaSectionv2 from '@/components/shared/FloatingCtaSectionV2';
 import { ComparisonTable } from '@/components/comparison/ComparisonTable';
+import { HeroSection } from '@/components/comparison/HeroSection';
 
 const PCloudComparison = ({ metatagsDescriptions, langJson, lang, navbarLang, footerLang }): JSX.Element => {
   const metatags = metatagsDescriptions.filter((desc) => desc.id === 'pcloud-alternative');
@@ -45,12 +45,7 @@ const PCloudComparison = ({ metatagsDescriptions, langJson, lang, navbarLang, fo
     <Layout title={metatags[0].title} description={metatags[0].description} segmentName="pCloud Comparison" lang={lang}>
       <Navbar textContent={navbarLang} lang={lang} cta={['priceTable']} fixed />
 
-      <HeroSection
-        textContent={langJson.HeroSection}
-        redirectUrl={'/pricing'}
-        percentage={percentageDiscount}
-        competitor={'pCloud'}
-      />
+      <HeroSection textContent={langJson.HeroSection} percentage={percentageDiscount} competitor={'pCloud'} />
 
       <ComparisonTable textContent={langJson.HeaderSection} competitor="pCloud" />
 
@@ -79,21 +74,6 @@ const PCloudComparison = ({ metatagsDescriptions, langJson, lang, navbarLang, fo
       <HorizontalScrollableSection
         textContent={langJson.HorizontalScrollableSection}
         bgGradient="linear-gradient(180deg, #FFFFFF 0%, #FFCECC 50%, #FFFFFF 100%)"
-      />
-
-      <FloatingCtaSectionv2
-        textContent={langJson.CtaSection}
-        url={'/pricing'}
-        customText={
-          <>
-            <div className="flex flex-col gap-4 px-10 text-center lg:px-0">
-              <p className="text-2xl font-semibold text-gray-95 lg:text-4xl">{langJson.CtaSection.title}</p>
-              <p className="text-base font-normal text-gray-55 lg:text-xl">{langJson.CtaSection.description}</p>
-            </div>
-          </>
-        }
-        containerDetails="shadow-lg backdrop-blur-[55px] bg-white"
-        bgGradientContainerColor="linear-gradient(115.95deg, rgba(244, 248, 255, 0.75) 10.92%, rgba(255, 255, 255, 0.08) 96.4%)"
       />
 
       <HorizontalScrollableSection
