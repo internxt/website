fb1d6ad44398f391a76cdd02d449edef5113df2f
jaaaaavier <javipiagui13@gmail.com>
2025-11-07 11:35:09 +0100

    Update footer.ts

--- src/assets/types/layout/footer.ts
+++ src/assets/types/layout/footer.ts
@@ -87,6 +87,7 @@ export interface Resources {
   pCloudAlternative: string;
   dropboxAlternative: string;
   megaAlternative: string;
+  icedriveAlternative: string;
   googleDriveAlternative: string;
   directoryOfPrivacyOrganizations: string;
   cyberAwareness: string;
