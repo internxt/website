51628d2e46e4d72ddba071ef7c75fe18582563b6
jaaaaavier <javipiagui13@gmail.com>
2025-11-07 16:18:45 +0100

    Update footer.ts

--- src/assets/types/layout/footer.ts
+++ src/assets/types/layout/footer.ts
@@ -16,7 +16,7 @@ export interface FooterSection {
   comingSoon: string;
   new: string;
   sections: Sections;
-  financialProject: string;
+  financialProject?: string;
 }
 
 export interface Copyright {
