3a73080b93ef13dd4ddc7beed41859c898c90abe
jaaaaavier <javipiagui13@gmail.com>
2026-01-07 10:35:56 +0100

    Update FeatureSection.tsx

--- src/components/about/FeatureSection.tsx
+++ src/components/about/FeatureSection.tsx
@@ -11,30 +11,31 @@ const FeatureSection = ({ textContent }) => {
       <section className="flex flex-col-reverse items-center justify-center gap-8 bg-white py-10 lg:flex-row lg:px-10 lg:py-20 xl:px-32 3xl:px-80">
         <div className="absolute left-8 right-8 top-0 h-[1px] bg-neutral-35 lg:left-32 lg:right-32 " />
         <RevealX direction="right" className="hidden lg:flex">
-          <Image
-            src={getImage('/images/about/photos/Internxt-gift.webp')}
+          <video
             width={500}
             height={520}
-            quality={100}
-            layout="intrinsic"
             className="rounded-2xl"
-            draggable={false}
-            alt="Internxt gift"
-            loading="eager"
-          />
+            autoPlay
+            loop
+            playsInline
+            style={{ objectFit: 'cover' }}
+          >
+            <source src={getImage('/videos/about/INTRNXT_ANIM_MAIN_INGLES_1-1.mp4')} type="video/mp4" />
+          </video>
         </RevealX>
         <RevealX direction="right" className="flex lg:hidden">
-          <Image
-            src={getImage('/images/about/photos/Internxt-gift.webp')}
+          <video
             width={345}
             height={324}
-            quality={100}
-            layout="intrinsic"
             className="rounded-2xl"
-            draggable={false}
-            alt="Internxt gift"
-            loading="eager"
-          />
+            autoPlay
+            loop
+            muted
+            playsInline
+            style={{ objectFit: 'cover' }}
+          >
+            <source src={getImage('/videos/about/INTRNXT_ANIM_MAIN_INGLES_1-1.mp4')} type="video/mp4" />
+          </video>
         </RevealX>
         <div className="flex w-[554px] flex-col items-center justify-center space-y-6 md:items-start">
           <p className="w-[345px] text-30 font-bold leading-tight text-gray-100 lg:w-[480px] lg:text-5xl">
