e12f196ecd622e0171830c365098e0af47797643
jaaaaavier <javipiagui13@gmail.com>
2026-02-13 09:38:43 +0100

    uopdate PricingCARDS

--- src/components/shared/pricing/PricingSection.tsx
+++ src/components/shared/pricing/PricingSection.tsx
@@ -187,7 +187,7 @@ export const PricingSection = ({
         enterTo="scale-100 translate-y-0 opacity-100"
         className="flex flex-col gap-4"
       >
-        <div className="content flex w-full flex-wrap items-stretch justify-center gap-6">
+        <div className="content flex w-full flex-nowrap items-stretch justify-start gap-6 overflow-x-auto pb-8 md:justify-center">
           {products?.individuals
             ? products.individuals[billingFrequency]
                 .filter((product) => {
