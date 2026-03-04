1cf4dfd6d241536cb233ad6643d75a48f9a01856
jaaaaavier <javipiagui13@gmail.com>
2026-03-04 18:31:03 +0100

    Update lifetime.tsx

--- src/pages/lifetime.tsx
+++ src/pages/lifetime.tsx
@@ -98,6 +98,7 @@ function LifetimeSpecial({
       segmentName="Lifetime"
       lang={lang}
       specialOffer={`https://internxt.com/images/previewLink/LifetimePreviewLink.png`}
+      robots="noindex, follow"
     >
       <Navbar textContent={navbarLang} lang={lang} cta={[navbarCta]} fixed />
 
