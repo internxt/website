4ffd0e7aea1676bcd9b04ef014347777098cbeb3
jaaaaavier <javipiagui13@gmail.com>
2026-03-06 11:56:06 +0100

    aws alternative LP

---
+++ src/pages/alternative-to-aws.tsx
@@ -0,0 +1,31 @@
+import { ComparisonPage } from '@/components/templates/comparisonPageTemplate';
+import cookies from '@/lib/cookies';
+import { PromoCodeName } from '@/lib/types';
+import { GetServerSidePropsContext } from 'next';
+
+const AwsComparison = (props: any) => (
+  <ComparisonPage
+    {...props}
+    competitor="aws"
+    metaTagId="aws-alternative"
+    segmentName="AWS Comparison"
+    logo="/images/comparison/aws-Letters.webp"
+    isS3Alternative
+  />
+);
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const lang = ctx.locale;
+  const metatagsDescriptions = require(`@/assets/lang/${lang}/metatags-descriptions.json`);
+  const langJson = require(`@/assets/lang/${lang}/aws-alternative.json`);
+  const navbarLang = require(`@/assets/lang/${lang}/navbar.json`);
+  const footerLang = require(`@/assets/lang/${lang}/footer.json`);
+
+  cookies.setReferralCookie(ctx);
+
+  return {
+    props: { lang, metatagsDescriptions, langJson, navbarLang, footerLang },
+  };
+}
+
+export default AwsComparison;
