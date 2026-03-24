8ef4bbce4a487b06cbcf2f6bfbde5e3ce79ad9bb
jaaaaavier <javipiagui13@gmail.com>
2026-03-24 11:51:39 +0100

    Update useSpecialOfferConfig.ts

--- src/hooks/useSpecialOfferConfig.ts
+++ src/hooks/useSpecialOfferConfig.ts
@@ -50,7 +50,7 @@ export const ALLOWED_PATHS = [
   'heise'
 ];
 export const ALTERNATE_RECOMENDATED_PLAN_PATHS = new Set<string>([]);
-export const DARK_MODE_PATHS = new Set<string>(['baity', 'xavier', 'oscar', 'rimembah', 'believemy', 'ghareeb']);
+export const DARK_MODE_PATHS = new Set<string>(['baity', 'oscar', 'rimembah', 'believemy', 'ghareeb']);
 export const ALTERNATIVE_IMAGES_PATHS = new Set<string>(['baity']);
 export const ONLY_ULTIMATE_PLANS_PATHS = new Set<string>(['ultimate']);
 export const ULTIMATE_PREMIUM_PLANS_PATHS = new Set<string>([]);
