b097c5885c8b2e7fb9222afb89ef9354ceb415f1
Francis Terrero <terrero0611@gmail.com>
2026-03-24 01:33:15 -0400

    twst

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { PASSWORD_REGEX } from '@/components/cloud-object-storage/integrated-che
 
 export const IFRAME_AUTH_ENABLED = false;
 export const REDIRECT_AUTH_ENABLED = true;
-const AUTH_FLOW_URL = 'https://dos-therapist-hanging-calls.trycloudflare.com';
+const AUTH_FLOW_URL = 'https://subsidiary-diversity-footwear-crew.trycloudflare.com';
 const OBJECT_STORAGE_USER_ACTIVATION_URL = process.env.NEXT_PUBLIC_OBJECT_STORAGE_USER_ACTIVATION_URL as string;
 
 export const openAuthDialog = (view: 'login' | 'signup' | 'recover'): void => {
