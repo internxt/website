b41b82c3a6a302d61b805cf5ea7f5a61af780529
internxt-yu <yu@internxt.com>
2026-08-06 10:28:59 +0200

    changed reference photo to photos in EN version

--- src/pages/photos.tsx
+++ src/pages/photos.tsx
@@ -156,7 +156,7 @@ export function getServerSideProps(ctx: GetServerSidePropsContext) {
     const locale = ctx.locale as string;
     const metatagsDescription = require(`@/assets/lang/${locale}/metatags-descriptions.json`);
     const navbarText = require(`@/assets/lang/${locale}/navbar.json`);
-    const textContent = require(`@/assets/lang/${locale}/photo.json`);
+    const textContent = require(`@/assets/lang/${locale}/photos.json`);
     const footerText = require(`@/assets/lang/${locale}/footer.json`);
     const relationalLinksText = require(`@/assets/lang/${locale}/relational-links.json`);
 
