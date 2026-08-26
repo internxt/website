80d1ca28e03776581a05ff42719d4e89cdc48f86
jaaaaavier <javipiagui13@gmail.com>
2026-08-26 15:28:38 +0200

    Update cloud-storage-for-documents.ts

--- src/assets/types/cloud-storage-for-documents.ts
+++ src/assets/types/cloud-storage-for-documents.ts
@@ -1,75 +1,75 @@
 export interface CloudStorageForDocumentsText {
-  HeroSection:             HeroSection;
-  FeaturesSection:         FeaturesSection;
-  CtaSection:              CtaSection;
-  CoreFeatures:            CoreFeatures;
-  SecureAndManage:         SecureAndManage;
+  HeroSection: HeroSection;
+  FeaturesSection: FeaturesSection;
+  CtaSection: CtaSection;
+  CoreFeatures: CoreFeatures;
+  SecureAndManage: SecureAndManage;
   FeaturesInternxtSection: FeaturesInternxtSection;
-  CtaSectionV2:            CtaSection;
-  SemanticAccordion:       SemanticAccordion;
-  tableSection:            TableSection;
+  CtaSectionV2: CtaSection;
+  SemanticAccordion: SemanticAccordion;
+  tableSection: TableSection;
 }
 
 interface CtaSection {
-  title:       string;
+  title: string;
   description: string;
-  cta:         string;
+  cta: string;
 }
 
 interface CoreFeatures {
-  title:          string;
-  description:    string;
+  title: string;
+  description: string;
   accordionCards: AccordionCards;
 }
 
 interface AccordionCards {
-  titles:       string[];
+  titles: string[];
   descriptions: Paragraphs[];
 }
 
 interface Paragraphs {
-  contents: string[]
+  contents: string[];
 }
 
 interface FeaturesInternxtSection {
-  title:             string;
-  description:       string;
+  title: string;
+  description: string;
   scrollableSection: AccordionCards;
 }
 
 export interface FeaturesSection {
-  intro:    string;
+  intro: string;
   features: Features;
 }
 
-interface Features {
+export interface Features {
   feature1: Feature;
   feature2: Feature;
   feature3: Feature;
 }
 
 interface Feature {
-  title:       string;
+  title: string;
   description: string[];
 }
 
 interface HeroSection {
-  title:    string;
+  title: string;
   features?: string[];
   subtitle: string;
-  cta:      string;
+  cta: string;
 }
 
 interface SecureAndManage {
-  title:       string;
+  title: string;
   description: string;
-  cards:       Card[];
+  cards: Card[];
 }
 
 interface Card {
-  title:       string;
+  title: string;
   description: string[];
-  cta:         string;
+  cta: string;
 }
 
 interface SemanticAccordion {
@@ -79,7 +79,7 @@ interface SemanticAccordion {
 
 interface Item {
   question: string;
-  answer:   string[];
+  answer: string[];
 }
 
 export interface TableSection {
