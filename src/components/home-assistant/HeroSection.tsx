d408e1b233bf0c1891dfed2cfb1972b5aad1b28f
internxt-yu <yu@internxt.com>
2026-08-27 11:40:25 +0200

    added and modified components and ts file

--- src/components/home-assistant/HeroSection.tsx
+++ src/components/home-assistant/HeroSection.tsx
@@ -1,95 +1,42 @@
 import Image from 'next/image';
 import { getImage } from '@/lib/getImage';
-
-import {
-  Brain,
-  CellTower,
-  CloudArrowUp,
-  Envelope,
-  ShieldPlus,
-  Sparkle,
-  VideoConference,
-} from '@phosphor-icons/react';
 import Link from 'next/link';
 import { HighlightText } from '../components/HighlightText';
 
 interface HeroSectionForPartnerProps {
   textContent: any;
-  percentOff: string;
   darkMode?: boolean;
+  isValentinesMode?: boolean;
 }
 
 export default function HeroSection({
   textContent,
-  percentOff,
   darkMode = false,
 }: Readonly<HeroSectionForPartnerProps>): JSX.Element {
 
-  const products = [
-    { icon: CloudArrowUp, text: textContent.products.drive },
-    { icon: ShieldPlus, text: textContent.products.antivirus },
-    { icon: Sparkle, text: textContent.products.cleaner },
-    { icon: CellTower, text: textContent.products.vpn },
-    { icon: VideoConference, text: textContent.products.meet },
-    { icon: Envelope, text: textContent.products.mail },
-    { icon: Brain, text: textContent.products.ai },
-  ];
-
-  const parsePercentText = (text: string) => {
-    return typeof text === 'string' ? text.replace(/{{discount}}/g, percentOff) : text;
-  };
-
   return (
     <section
-      className={`mt-20 flex h-min w-full flex-row items-center justify-center gap-12 overflow-hidden py-10 lg:mt-16 lg:h-[700px] lg:justify-between lg:gap-16 lg:pl-10 lg:pr-4 xl:pl-16 xl:pr-8 2.5xl:pl-32 2.5xl:pr-16 3xl:pl-80 3xl:pr-40`}
+      className={`mt-20 flex h-min w-full flex-row items-center justify-center overflow-hidden py-10 lg:mt-16 lg:h-[657px] lg:justify-between lg:pl-10 lg:pr-4 xl:pl-32 xl:pr-16 3xl:pl-80 3xl:pr-60`}
       style={{
         background: darkMode
           ? 'linear-gradient(180deg, #082D66 0%, #1C1C1C 100%)'
           : 'linear-gradient(180deg, #E5EFFF 0%, #FFFFFF 100%)',
       }}
     >
-      <div className="flex h-min w-[345px] flex-col justify-center gap-6 lg:h-[392px] lg:w-[600px] lg:justify-between">
-        <div className="flex w-full flex-wrap items-start justify-start gap-2 lg:flex-nowrap lg:justify-between">
-          {products.map((feature) => (
-            <div
-              key={feature.text}
-              className={`flex h-6 w-min flex-row items-center justify-center gap-1 rounded ${darkMode ? 'bg-white/10' : 'bg-white/50'
-                }  px-1 py-0.5 shadow-sm lg:h-8 lg:px-2 lg:py-1`}
-            >
-              <feature.icon className="h-5 w-5 text-primary lg:h-6 lg:w-6" />
-              <p
-                className={`whitespace-nowrap text-sm font-medium leading-tight lg:pt-[2px] ${darkMode ? 'text-neutral-37' : 'text-gray-80'
-                  }`}
-              >
-                {feature.text}
-              </p>
-            </div>
-          ))}
-        </div>
+      <div className="flex h-min w-[345px] flex-col justify-center gap-6 lg:h-[392px] lg:w-[606px] lg:justify-between">
         <h1
-          className={`text-[34px] font-semibold leading-tight ${darkMode ? 'text-white-95' : 'text-gray-100'
-            } lg:text-[38px] xl:text-[40px] 2xl:text-[42px] 3xl:text[44px]`}
+          className={`text-30 font-semibold leading-tight ${darkMode ? 'text-white-95' : 'text-gray-100'} lg:text-5xl`}
         >
-          <HighlightText text={textContent.title} />
+            <HighlightText text={textContent.title} />
         </h1>
-        <div className="flex w-full flex-col justify-center gap-4 lg:gap-8">
-          <div className="flex flex-col justify-center gap-4">
-            {percentOff !== '0' && (
-              <span
-                className={`flex w-min flex-row items-center gap-1 whitespace-nowrap rounded-2 px-1 py-0.5 text-base font-semibold leading-tight lg:text-xl ${darkMode ? 'bg-purple-100 text-purple-8' : 'bg-neutral-37 text-primary'
-                  }`}
-              >
-                <span>{parsePercentText(textContent.subtitle)}</span>
-              </span>
-            )}
-            <p
-              className={`font-regular ${darkMode ? 'text-white-95' : 'text-gray-100'
-                } text-lg leading-tight lg:text-xl`}
+
+        <p
+              className={`font-regular ${
+                darkMode ? 'text-white-95' : 'text-gray-100'
+              } text-lg leading-tight lg:text-xl`}
             >
-              {parsePercentText(textContent.description)}
+              {textContent.description}
             </p>
-          </div>
-        </div>
 
         <div className="flex h-min w-full flex-col justify-center gap-4">
           <div className="flex w-full flex-row items-start gap-4">
@@ -103,13 +50,15 @@ export default function HeroSection({
         </div>
       </div>
 
-      <div className={'hidden justify-center lg:flex'}>
+      <div
+        className={'h-[500px] w-[562px] hidden justify-center lg:flex'}
+      >
         <Image
-          src={getImage('/images/home-assistant/hero.webp')}
+          src={getImage(`/images/influencers/internxt-private-cloud.webp`)}
           alt="Internxt Partners HeroSection Image"
-          width={1200}
-          height={727}
-          className="h-auto rounded"
+          height={529}
+          width={562}
+          quality={100}
         />
       </div>
     </section>
