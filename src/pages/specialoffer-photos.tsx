161c38a85262935f52a998c8ab502dfe51becc04
internxt-yu <yu@internxt.com>
2026-08-31 13:01:17 +0200

    added specialoffer-photos LP

---
+++ src/pages/specialoffer-photos.tsx
@@ -0,0 +1,196 @@
+import { useEffect, useState } from 'react';
+import Layout from '@/components/layout/Layout';
+import Script from 'next/script';
+
+import { PromoCodeName } from '@/lib/types';
+import { saveCelloFirstVisit, isCelloExpired } from '@/lib/cookies';
+import Footer from '@/components/layout/footers/Footer';
+import usePricing from '@/hooks/usePricing';
+
+import Navbar from '@/components/layout/navbars/Navbar';
+import { FooterText, MetatagsDescription, NavigationBarText } from '@/assets/types/layout/types';
+import HeroSection from '@/components/partnersTemplate/HeroSection';
+import TrustedSection from '@/components/home/TrustedSection';
+import ReviewsSection from '@/components/home/ReviewsSection';
+import HorizontalScrollableSection from '@/components/home/HorizontalScrollableSection';
+import FloatingCtaSectionv2 from '@/components/shared/FloatingCtaSectionV2';
+import { PricingSectionWrapper } from '@/components/shared/pricing/PricingSectionWrapper';
+import { stripeService } from '@/services/stripe.service';
+import { SpecialOfferText } from '@/assets/types/specialOfferTemplate';
+
+interface SpecialOfferProps {
+  metatagsDescriptions: MetatagsDescription[];
+  navbarLang: NavigationBarText;
+  langJson: SpecialOfferText;
+  footerLang: FooterText;
+  lang: string;
+}
+
+function SpecialOffer({
+  langJson,
+  lang,
+  metatagsDescriptions,
+  footerLang,
+  navbarLang,
+}: SpecialOfferProps): JSX.Element {
+  const [isCelloAttributionExpired, setIsCelloAttributionExpired] = useState(true);
+
+  useEffect(() => {
+    saveCelloFirstVisit();
+    setIsCelloAttributionExpired(isCelloExpired());
+  }, []);
+
+  const metatags = metatagsDescriptions.filter((desc) => desc.id === 'special-offer');
+
+  const {
+    products,
+    loadingCards,
+    currencyValue,
+    coupon: individualCoupon,
+    lifetimeCoupon: lifetimeCoupon,
+    lifetimeCoupons,
+  } = usePricing({ couponCode: PromoCodeName.emailphotos, couponCodeForLifetime: PromoCodeName.emailphotos });
+
+  const percentOff = lifetimeCoupon?.percentOff === undefined ? '0' : String(lifetimeCoupon.percentOff);
+  const parsePercentText = (text: string) => {
+    if (!percentOff || percentOff === '0') {
+      return <div className="bg-gray-200 h-4 w-16 animate-pulse rounded"></div>;
+    }
+    return typeof text === 'string' ? text.replace(/{{discount}}/g, percentOff) : text;
+  };
+  const decimalDiscountForLifetime = lifetimeCoupon?.percentOff && 100 - lifetimeCoupon.percentOff;
+  const decimalDiscount = individualCoupon?.percentOff && 100 - individualCoupon.percentOff;
+
+  const onCheckoutButtonClicked = async (
+    priceId: string,
+    isCheckoutForLifetime: boolean,
+    interval: string,
+    storage: string,
+  ) => {
+    const couponCodeForCheckout = isCheckoutForLifetime ? lifetimeCoupon : individualCoupon;
+
+    const finalPrice = await stripeService.calculateFinalPrice(
+      priceId,
+      interval,
+      currencyValue,
+      'individuals',
+      couponCodeForCheckout,
+    );
+
+    stripeService.redirectToCheckout(
+      priceId,
+      finalPrice,
+      currencyValue,
+      'individual',
+      isCheckoutForLifetime,
+      interval,
+      storage,
+      couponCodeForCheckout?.name,
+    );
+  };
+
+  return (
+    <Layout title={metatags[0].title} description={metatags[0].description} segmentName="Partners" lang={lang} robots="noindex,follow">
+      {!isCelloAttributionExpired && (
+        <Script
+          src="https://assets.cello.so/attribution/latest/cello-attribution.js"
+          type="module"
+          strategy="afterInteractive"
+        />
+      )}
+      <Navbar lang={lang} textContent={navbarLang} cta={['payment']} isLinksHidden hideCTA />
+
+      <HeroSection
+        textContent={langJson.HeroSection}
+        percentOff={percentOff}
+        image={'internxt-private-cloud'}
+        specialOffer
+      />
+
+      <ReviewsSection textContent={langJson.ReviewSection} />
+
+      <PricingSectionWrapper
+        textContent={langJson.tableSection}
+        decimalDiscount={{
+          individuals: decimalDiscount,
+          lifetime: decimalDiscountForLifetime,
+        }}
+        lifetimeCoupons={lifetimeCoupons}
+        lang={lang}
+        products={products}
+        loadingCards={loadingCards}
+        onCheckoutButtonClicked={onCheckoutButtonClicked}
+        hideBusinessCards
+        hideBusinessSelector
+        popularPlanBySize="5TB"
+        startFromStorage='Ultimate'
+        sectionDetails="bg-white lg:py-20"
+        hideFreeCard
+      />
+
+      <FloatingCtaSectionv2
+        textContent={langJson.ctaSection}
+        url={'#billingButtons'}
+        customText={
+          <div className="flex flex-col items-center gap-4 px-10 text-center lg:px-0">
+            <p className="text-2xl font-semibold leading-tight text-gray-95 lg:text-4xl">
+              {parsePercentText(langJson.ctaSection.title)}
+            </p>
+            <p className="text-base font-normal leading-tight text-gray-55 lg:w-[698px] lg:text-center lg:text-xl">
+              {parsePercentText(langJson.ctaSection.description)}
+            </p>
+          </div>
+        }
+        bgGradientContainerColor="linear-gradient(115.95deg, rgba(244, 248, 255, 0.75) 10.92%, rgba(255, 255, 255, 0.08) 96.4%)"
+        containerDetails="shadow-lg backdrop-blur-[55px]"
+        bgPadding="lg:pY-20 pb-20"
+        bgGradientColor="linear-gradient(0deg, #F4F8FF 0%, #FFFFFF 100%)"
+      />
+
+      <HorizontalScrollableSection textContent={langJson.NextGenSection} />
+
+      <TrustedSection textContent={langJson.TrustedBySection} bottomBar={false} bgColor="bg-neutral-17" />
+
+      <FloatingCtaSectionv2
+        textContent={langJson.ctaSection2}
+        url={'#billingButtons'}
+        customText={
+          <div className="flex flex-col items-center gap-4 px-10 text-center lg:px-20">
+            <p className="text-2xl font-semibold leading-tight text-gray-95 lg:text-4xl">
+              {parsePercentText(langJson.ctaSection2.title)}
+            </p>
+            <p className="text-base font-normal leading-tight text-gray-55  lg:text-center lg:text-xl">
+              {parsePercentText(langJson.ctaSection2.description)}
+            </p>
+          </div>
+        }
+        bgGradientContainerColor="linear-gradient(115.95deg, rgba(244, 248, 255, 0.75) 10.92%, rgba(255, 255, 255, 0.08) 96.4%)"
+        containerDetails="shadow-lg backdrop-blur-[55px] px-10"
+        bgPadding="lg:pb-20 pb-10"
+        bgGradientColor="linear-gradient(180deg, #F4F8FF 0%, #FFFFFF 100%)"
+      />
+
+      <Footer textContent={footerLang} lang={lang} />
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  const lang = ctx.locale;
+  const metatagsDescriptions = require(`@/assets/lang/${lang}/metatags-descriptions.json`);
+  const navbarLang = require(`@/assets/lang/${lang}/navbar.json`);
+  const langJson = require(`@/assets/lang/${lang}/specialOfferTemplate.json`);
+  const footerLang = require(`@/assets/lang/${lang}/footer.json`);
+
+  return {
+    props: {
+      lang,
+      metatagsDescriptions,
+      navbarLang,
+      langJson,
+      footerLang,
+    },
+  };
+}
+
+export default SpecialOffer;
