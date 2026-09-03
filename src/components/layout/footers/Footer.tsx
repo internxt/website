af4e5988d2729978c9ac09599f5076061e74f643
internxt-yu <yu@internxt.com>
2026-09-03 15:58:23 +0200

    resize the banner image

--- src/components/layout/footers/Footer.tsx
+++ src/components/layout/footers/Footer.tsx
@@ -85,8 +85,8 @@ export default function Footer({
           <Image
             src={getImage('/images/footer/proyecto.webp')}
             alt="Proyecto Financiado por la generalitat"
-            width={800}
-            height={350}
+            width={200}
+            height={90}
           />
         </div>
       )}
