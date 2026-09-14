ebc518e23c55e8597841b6a63fe04425777a3861
Javi <javipiagui13@gmail.com>
2026-09-14 15:00:16 +0200

    chore: remove phantom cypress coverage and fully-commented specs

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('@cypress/code-coverage/task')(on, config);
 
       // It's IMPORTANT to return the config object
       // with any changed environment variables
