5315aefbbf908782b94fd868842271e223103b5c
internxt-yu <yu@internxt.com>
2026-09-16 11:58:56 +0200

    modifications in bf LPs

--- src/pages/deals/black-friday-internxt/bf-vpn.tsx
+++ src/pages/deals/black-friday-internxt/bf-vpn.tsx
@@ -1,31 +1,32 @@
 import { GetServerSidePropsContext } from 'next';
 import { PromoCodeName } from '@/lib/types';
-import DealsTemplate from '@/components/templates/dealsTemplate';
+import BlackFridayDealsTemplate from '../../../components/templates/blackFridayDealsTemplate';
 import { getDealsProps } from '@/lib/helpers/deals';
 
-const DealsPage = (props) => {
-  return <DealsTemplate {...props} />;
+const BlackFridayPage = (props) => {
+  return <BlackFridayDealsTemplate {...props} />;
 };
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   return getDealsProps(
     ctx,
     {
-      heroImage: '/images/deals/cloud-storage-lifetime.webp',
-      metatagsId: 'free-cloud-storage-lifetime-deals',
+      heroImage: '/images/black-friday/bf-cloud-storage.webp',
+      metatagsId: 'black-friday-vpn',
       couponCodeForLifetime: PromoCodeName.FreePlanUpsell,
       popularPlanSize: '5TB',
       hideBusinessCards: true,
       hideBusinessSelector: true,
+
       moreDealsUrls: {
         card1: '/deals/black-friday-internxt',
-        card2: '/deals/black-friday-internxt/bf-personal-cloud-storage-deals',
+        card2: 'bf-personal-cloud-storage-deals',
       },
     },
     {
-      jsonFileName: 'cloud-storage-lifetime',
+      jsonFileName: 'black-friday-vpn',
     },
   );
 }
 
-export default DealsPage;
+export default BlackFridayPage;
