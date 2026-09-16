6dbdbc48fd61e2c48de27f3a609a2591524097c6
internxt-yu <yu@internxt.com>
2026-09-16 10:36:11 +0200

    changes link in cloud-storage-lifetime

--- src/pages/deals/cloud-storage-lifetime.tsx
+++ src/pages/deals/cloud-storage-lifetime.tsx
@@ -18,8 +18,8 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
       hideBusinessCards: true,
       hideBusinessSelector: true,
       moreDealsUrls: {
-        card1: '/deals',
-        card2: 'free-cloud-storage-deals',
+        card1: '/deals/black-friday-internxt',
+        card2: '/deals/black-friday-internxt/bf-personal-cloud-storage-deals',
       },
     },
     {
